88d9724efc085e293098fc2b56c25c76da103958
David Harper <adh@sanger.ac.uk>
2002-07-31 11:18:12 +0000

    Corrected line-break error in script.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@96 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/utils/flush-all-logs.sh
+++ arcturus/utils/flush-all-logs.sh
@@ -2,5 +2,4 @@
 
 admintool=/nfs/pathsoft/external/mysql-3.23.51/bin/mysqladmin
 
-${admintool} --defaults-file=/nfs/pathdb/arcturus/init.d/flushlogs.
-cnf -h babel -P 14643 flush-logs
+${admintool} --defaults-file=/nfs/pathdb/arcturus/init.d/flushlogs.cnf -h babel -P 14643 flush-logs
