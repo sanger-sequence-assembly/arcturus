d91a00ebdf8567512abc88d2b02db7563cd37eeb
Ed Zuiderwijk <ejz@sanger.ac.uk>
2003-02-27 11:41:02 +0000

    Change to perl 5.6 to run SmallProf
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@202 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/cgi-bin/recover.sh
+++ arcturus/cgi-bin/recover.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/csh
 
-if ($?LD_LIBRARY_PATH) then
-    setenv LD_LIBRARY_PATH /usr/local/badger/distrib-1999.0/lib/alpha-binaries:${LD_LIBRARY_PATH}
-else
-    setenv LD_LIBRARY_PATH /usr/local/badger/distrib-1999.0/lib/alpha-binaries
-endif
+# echo $LD_LIBRARY_PATH
 
 set task = $1
 
 set data = $2
 
-$task $data | grep -E '(dye|DYE)'
-
-#set yuk = `$task $data | grep -E '(dye|DYE)'`
-#echo "$yuk[1]"
+$task $data
 
 exit 0
