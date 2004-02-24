a327f25e776f3041ba1aae723103360521e35052
David Harper <adh@sanger.ac.uk>
2004-02-24 13:07:08 +0000

    This module is a simplified Perl version of the Java JDBC DataSource class.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@418 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/lib/DataSource.pm
@@ -0,0 +1,150 @@
+package DataSource;
+
+use strict;
+
+use Net::LDAP;
+use DBI;
+
+sub new {
+    my $type = shift;
+
+    my $this = {};
+    bless $this, $type;
+
+    my ($url, $base, $instance, $organism);
+
+    while (my $nextword = shift) {
+	$nextword =~ s/^\-//;
+
+	$url = shift if ($nextword eq 'url');
+
+	$base = shift if ($nextword eq 'base');
+
+	$instance = shift if ($nextword eq 'instance');
+
+	$organism = shift if ($nextword eq 'organism');
+    }
+
+    $url = 'ldap.internal.sanger.ac.uk' unless defined($url);
+
+    $instance = 'prod' unless defined($instance);
+
+    $base = "cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk" unless defined($base);
+
+    $base = "cn=$instance," . $base if defined($instance);
+
+    my $filter = "&(objectClass=javaNamingReference)";
+
+    $filter .= "(cn=$organism)" if defined($organism);
+
+    my $ldap = Net::LDAP->new($url) or die "$@";
+
+    # An anonymous bind
+    my $mesg = $ldap->bind;
+
+    $mesg->code && die $mesg->error;
+
+    # Perform the search
+    $mesg = $ldap->search(base   => $base,
+			  scope => 'sub',
+			  filter => "($filter)"
+			  );
+
+    $mesg->code && die $mesg->error;
+ 
+    foreach my $entry ($mesg->all_entries) {
+	my $dn = $entry->dn;
+	my @items = $entry->get_value('javaClassName');
+	my $classname = shift @items;
+	@items = $entry->get_value('javaFactory');
+	my $factory = shift @items;
+	@items = $entry->get_value('javaReferenceAddress');
+
+	my @datasourcelist = split(/\./, $classname);
+
+	my $datasourcetype = pop @datasourcelist;
+
+	my $datasource = {};
+
+	while (my $item = shift @items) {
+	    my ($id,$key,$value) = $item =~ /\#(\d+)\#(\w+)\#(\w+)/;
+	    $datasource->{$key} = $value;
+	}
+	
+	my ($url, $username, $password) = &buildUrl($datasourcetype, $datasource);
+
+	$this->{URL} = $url;
+	$this->{Username} = $username;
+	$this->{Password} = $password;
+    }
+
+    # Close LDAP session
+    $mesg = $ldap->unbind;
+
+    return $this;
+}
+
+# Private function for use in the constructor
+sub buildUrl {
+    my ($dstype, $dshash, $junk) = @_;
+
+    return buildMySQLUrl($dshash) if ($dstype =~ /mysqldatasource/i);
+
+    return buildOracleUrl($dshash) if ($dstype =~ /oracledatasource/i);
+
+    return undef;
+}
+
+# Private function for use in the constructor
+sub buildMySQLUrl {
+    my $dshash = shift;
+
+    my $username = $dshash->{'user'};
+    my $password = $dshash->{'password'};
+
+    my $hostname = $dshash->{'serverName'};
+    my $portnumber = $dshash->{'port'};
+
+    my $database = $dshash->{'databaseName'};
+
+    return ("DBI:mysql:$database:$hostname:$portnumber", $username, $password);
+}
+
+# Private function for use in the constructor
+sub buildOracleUrl {
+    my $dshash = shift;
+
+    my $username = $dshash->{'userName'};
+    my $password = $dshash->{'passWord'};
+
+    my $hostname = $dshash->{'serverName'};
+    my $portnumber = $dshash->{'portNumber'};
+
+    my $database = $dshash->{'databaseName'};
+
+    my $url = "DBI:Oracle:host=$hostname;port=$portnumber;sid=$database";
+
+    return ($url, $username, $password);
+}
+
+sub getConnection {
+    my $this = shift;
+
+    my $url      = $this->{URL};
+    my $username = $this->{Username};
+    my $password = $this->{Password};
+
+    my $dbh = DBI->connect($url, $username, $password, {RaiseError => 0, PrintError => 0});
+
+    return $dbh;
+}
+
+sub getURL {
+    my $this = shift;
+
+    my $url      = $this->{URL};
+
+    return $url;
+}
+
+1;
