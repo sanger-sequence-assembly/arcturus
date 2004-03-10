6bb0ad6cacf90f0f2eb1471c0d08f3640f339eb6
David Harper <adh@sanger.ac.uk>
2004-03-10 15:16:18 +0000

    Modified test program to reflect changes to DataSource module.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@453 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/test/testDataSource.pl
+++ arcturus/test/testDataSource.pl
@@ -7,6 +7,10 @@ while ($nextword = shift @ARGV) {
     $instance = shift @ARGV if ($nextword eq '-instance');
 
     $organism = shift @ARGV if ($nextword eq '-organism');
+
+    $username = shift @ARGV if ($nextword eq '-username');
+
+    $password = shift @ARGV if ($nextword eq '-password');
 }
 
 die "You must specify the instance" unless defined($instance);
@@ -18,12 +22,19 @@ $ds = new DataSource(-instance => $instance,
 $url = $ds->getURL();
 print "The URL is $url\n";
 
-$dbh = $ds->getConnection();
+if (defined($username) && defined($password)) {
+    $dbh = $ds->getConnection(username => $username, password => $password);
+} else {
+    $dbh = $ds->getConnection();
+}
 
 if (defined($dbh)) {
     print "        CONNECT OK\n";
 } else {
-    print "        CONNECT FAILED: : $DBI::errstr\n";
+    print "        CONNECT FAILED:";
+    print " (with username=\"$username\" and password=\"$password\")"
+	if (defined($username) && defined($password));
+    print " : $DBI::errstr\n";
 }
 
 $dbh->disconnect if defined($dbh);
