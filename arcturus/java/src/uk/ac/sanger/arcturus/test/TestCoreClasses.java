c5b4f0373beaa8ce704e91f8be31e4c4511afce4
David Harper <adh@sanger.ac.uk>
2004-10-28 15:54:48 +0000

    Created test programs for the manager classes and updated the core class test program to reflect the changes to the core classes.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@721 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/test/TestCoreClasses.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/TestCoreClasses.java
@@ -5,21 +5,14 @@ public class TestCoreClasses {
     public static void main(String args[]) {
 	ArcturusDatabase adb = null;
 
-	Read read = new Read("bav10101a01.p1k", 1, adb);
-
-	Template template = new Template("bav10101a01", 1, adb);
-
-	read.setTemplate(template);
-
-	Ligation ligation = new Ligation("bav10101", 1, adb);
-
-	template.setLigation(ligation);
-
-	ligation.setInsertSizeRange(1500, 3000);
-
 	Clone clone = new Clone("bav1", 1, adb);
 
-	ligation.setClone(clone);
+	Ligation ligation = new Ligation("bav10101", 1, clone, 1500, 3000, adb);
+
+	Template template = new Template("bav10101a01", 1, ligation, adb);
+
+	Read read = new Read("bav10101a01.p1k", 1, template, new java.sql.Date(System.currentTimeMillis()),
+			     Read.FORWARD, Read.UNIVERSAL_PRIMER, Read.DYE_TERMINATOR, adb);
 
 	System.out.println("Read:\n" + read);
 
---
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/TestLigationManager.java
@@ -0,0 +1,112 @@
+import uk.ac.sanger.arcturus.*;
+import uk.ac.sanger.arcturus.database.*;
+import uk.ac.sanger.arcturus.data.*;
+
+import java.io.*;
+import java.util.*;
+import javax.naming.Context;
+
+public class TestLigationManager {
+    public static void main(String args[]) {
+	System.out.println("TestLigationManager");
+	System.out.println("===================");
+	System.out.println();
+
+	String ldapURL = "ldap://ldap.internal.sanger.ac.uk/cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk";
+
+	String separator = "                    --------------------                    ";
+
+	Properties props = new Properties();
+
+	Properties env = System.getProperties();
+
+	props.put(Context.INITIAL_CONTEXT_FACTORY, env.get(Context.INITIAL_CONTEXT_FACTORY));
+	props.put(Context.PROVIDER_URL, ldapURL);
+
+	String instance = null;
+	String organism = null;
+	String objectname = null;
+
+	switch (args.length) {
+	case 0:
+	case 1:
+	    System.out.println("Argument(s) missing: [instance] organism objectname");
+	    System.exit(1);
+	    break;
+
+	case 2:
+	    instance = "dev";
+	    organism = args[0];
+	    objectname = args[1];
+	    break;
+
+	default:
+	    instance = args[0];
+	    organism = args[1];
+	    objectname = args[2];
+	    break;
+	}
+
+	try {
+	    System.out.println("Creating an ArcturusInstance for " + instance);
+	    System.out.println();
+
+	    ArcturusInstance ai = new ArcturusInstance(props, instance);
+
+	    System.out.println("Creating an ArcturusDatabase for " + organism);
+	    System.out.println();
+
+	    ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+
+	    System.out.println("FInding a CloneManager");
+	    System.out.println();
+
+	    CloneManager cmgr = adb.getCloneManager();
+
+	    System.out.println("Pre-loading all clones");
+	    System.out.println();
+
+	    cmgr.preloadAllClones();
+
+	    System.out.println("Finding a LigationManager");
+	    System.out.println();
+
+	    LigationManager lmgr = adb.getLigationManager();
+
+	    System.out.println("Pre-loading all ligations");
+	    System.out.println();
+
+	    lmgr.preloadAllLigations();
+
+	    System.out.println("Looking up ligation " + objectname + " by name");
+	    System.out.println();
+
+	    Ligation ligation = lmgr.getLigationByName(objectname);
+
+	    if (ligation == null)
+		System.out.println("*** FAILED ***");
+	    else {
+		System.out.println(ligation);
+		System.out.println("    " + ligation.getClone());
+	    }
+
+	    System.out.println();
+
+	    System.out.println("Looking up ligation 6 by ID");
+	    System.out.println();
+
+	    ligation = lmgr.getLigationByID(6);
+
+	    if (ligation == null)
+		System.out.println("*** FAILED ***");
+	    else {
+		System.out.println(ligation);
+		System.out.println("    " + ligation.getClone());
+	    }
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	    System.exit(1);
+	}
+    }
+}
