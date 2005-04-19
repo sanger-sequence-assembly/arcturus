abb13fa668edf3bc310523141d64084ee2b57827
David Harper <adh@sanger.ac.uk>
2005-04-19 13:25:57 +0000

    Added a directory to store useful SQL snippets. The first two examples list the current contigs and the free reads respectively.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@850 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/sql/freereads.sql
@@ -0,0 +1,16 @@
+create temporary table CURCTG as
+  select CONTIG.contig_id from CONTIG left join C2CMAPPING
+  on CONTIG.contig_id = C2CMAPPING.parent_id
+  where C2CMAPPING.parent_id is null;
+
+create temporary table CURSEQ
+  (seq_id integer not null, contig_id integer not null, key (contig_id)) as
+  select seq_id,CURCTG.contig_id from CURCTG left join MAPPING using(contig_id);
+
+create temporary table CURREAD
+  (read_id integer not null, seq_id integer not null, contig_id integer not null, key (read_id)) as
+  select read_id,SEQ2READ.seq_id,contig_id from CURSEQ left join SEQ2READ using(seq_id);
+
+create temporary table FREEREAD as
+  select READS.read_id from READS left join CURREAD using(read_id)
+  where seq_id is null;
