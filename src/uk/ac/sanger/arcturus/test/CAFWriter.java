4249cdacd91590df395743780a9447b4a2efc5e4
David Harper <adh@sanger.ac.uk>
2005-05-24 14:53:29 +0000

    Added two new test classes.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@882 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/CAFWriter.java
@@ -0,0 +1,131 @@
+import java.io.*;
+
+import uk.ac.sanger.arcturus.data.*;
+
+public class CAFWriter {
+    protected PrintStream ps = null;
+
+    public CAFWriter(PrintStream ps) {
+	this.ps = ps;
+    }
+
+    public void writeContig(Contig contig) {
+	writeContigSequence(contig);
+
+	if (contig.getDNA() != null) {
+	    ps.println();
+	    
+	    writeContigDNA(contig);
+	}
+
+	if (contig.getQuality() != null) {
+	    ps.println();
+
+	    writeContigQuality(contig);
+	}
+
+	ps.println();
+
+	Mapping[] mappings = contig.getMappings();
+
+	for (int i = 0; i < mappings.length; i++)
+	    writeRead(mappings[i].getSequence());
+    }
+
+    private void writeContigSequence(Contig contig) {
+	ps.println("Sequence : Contig" + contig.getID());
+	ps.println("Is_contig");
+	ps.println("Unpadded");
+
+	Mapping[] mappings = contig.getMappings();
+
+	for (int i = 0; i < mappings.length; i++)
+	    writeAssembledFrom(mappings[i]);
+    }
+
+    private void writeAssembledFrom(Mapping mapping) {
+	Segment[] segments = mapping.getSegments();
+	Sequence sequence = mapping.getSequence();
+	Read read = sequence.getRead();
+	String readname = read.getName();
+
+	for (int i = 0; i < segments.length; i++) {
+	    int cstart = segments[i].getContigStart();
+	    int rstart = segments[i].getReadStart();
+	    int length = segments[i].getLength();
+
+	    int cfinish = cstart + length - 1;
+
+	    int rfinish;
+
+	    if (mapping.isForward())
+		rfinish = rstart + length - 1;
+	    else
+		rfinish = rstart - (length - 1);
+
+	    ps.println("Assembled_from " + readname + " " +
+		       cstart + " " + cfinish + " " +
+		       rstart + " " + rfinish);
+	}
+    }
+
+    private void writeContigDNA(Contig contig) {
+	ps.println("DNA : Contig" + contig.getID());
+	writeDNA(contig.getDNA());
+    }
+
+    private void writeContigQuality(Contig contig) {
+	ps.println("BaseQuality : Contig" + contig.getID());
+	writeQuality(contig.getQuality());
+    }
+
+    private void writeRead(Sequence sequence) {
+	ps.println("Sequence : " + sequence.getRead().getName());
+	ps.println("Is_read");
+	ps.println("Unpadded");
+
+	ps.println();
+
+	writeSequenceDNA(sequence);
+
+	ps.println();
+
+	writeSequenceQuality(sequence);
+
+	ps.println();
+    }
+
+    private void writeSequenceDNA(Sequence sequence) {
+	ps.println("DNA : " + sequence.getRead().getName());
+	writeDNA(sequence.getDNA());
+    }
+
+ 
+    private void writeSequenceQuality(Sequence sequence) {
+	ps.println("BaseQuality : " + sequence.getRead().getName());
+	writeQuality(sequence.getQuality());
+    }
+
+    private void writeDNA(byte[] dna) {
+	for (int i = 0; i < dna.length; i += 50) {
+	    int sublen = (i + 50 < dna.length) ? 50 : dna.length - i;
+	    ps.write(dna, i, sublen);
+	    ps.print('\n');
+	}
+    }
+
+    private void writeQuality(byte[] quality) {
+	for (int i = 0; i < quality.length; i++) {
+	    int qual = (int)quality[i];
+	    ps.print(qual);
+
+	    if ((i % 25) < 24)
+		ps.print(' ');
+	    else
+		ps.print('\n');
+	}
+
+	if ((quality.length % 25) != 0)
+	    ps.print('\n');
+    }
+}
---
+++ src/uk/ac/sanger/arcturus/test/TestContigManager3.java
@@ -0,0 +1,167 @@
+import uk.ac.sanger.arcturus.*;
+import uk.ac.sanger.arcturus.database.*;
+import uk.ac.sanger.arcturus.data.*;
+
+import java.util.*;
+import java.io.*;
+
+import javax.naming.Context;
+
+public class TestContigManager3 {
+    private static long lasttime;
+    private static Runtime runtime = Runtime.getRuntime();
+
+    public static void main(String args[]) {
+	lasttime = System.currentTimeMillis();
+
+	System.err.println("TestContigManager3");
+	System.err.println("==================");
+	System.err.println();
+
+	String ldapURL = "ldap://ldap.internal.sanger.ac.uk/cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk";
+
+	String separator = "                    --------------------                    ";
+
+	Properties props = new Properties();
+
+	Properties env = System.getProperties();
+
+	props.put(Context.INITIAL_CONTEXT_FACTORY, env.get(Context.INITIAL_CONTEXT_FACTORY));
+	props.put(Context.PROVIDER_URL, ldapURL);
+
+	String instance = null;
+	String organism = null;
+
+	String objectname = null;
+
+	for (int i = 0; i < args.length; i++) {
+	    if (args[i].equalsIgnoreCase("-instance"))
+		instance = args[++i];
+
+	    if (args[i].equalsIgnoreCase("-organism"))
+		organism = args[++i];
+	}
+
+	if (instance == null || organism == null) {
+	    printUsage(System.err);
+	    System.exit(1);
+	}
+
+	try {
+	    System.out.println("Creating an ArcturusInstance for " + instance);
+	    System.out.println();
+
+	    ArcturusInstance ai = new ArcturusInstance(props, instance);
+
+	    System.out.println("Creating an ArcturusDatabase for " + organism);
+	    System.out.println();
+
+	    ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+
+	    java.sql.Connection conn = adb.getConnection();
+
+	    Manager manager = new Manager(conn);
+
+	    MyListener listener = new MyListener();
+
+	    manager.addManagerEventListener(listener);
+
+	    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+	    String line = null;
+	    Contig contig = null;
+	    CAFWriter cafWriter = new CAFWriter(System.out);
+
+	    while (true) {
+		System.err.print(">");
+
+		line = stdin.readLine();
+
+		if (line == null)
+		    System.exit(0);
+
+		String[] words = tokenise(line);
+		
+		for (int i = 0; i < words.length; i++) {
+		    if (words[i].equalsIgnoreCase("quit") || words[i].equalsIgnoreCase("exit"))
+			System.exit(0);
+
+		    if (words[i].equalsIgnoreCase("caf")) {
+			if (contig != null)
+			    cafWriter.writeContig(contig);
+		    } else {
+			int contig_id = Integer.parseInt(words[i]);
+
+			long clockStart = System.currentTimeMillis();
+
+			contig = manager.loadContigByID(contig_id);
+
+			long clockStop = System.currentTimeMillis() - clockStart;
+			System.err.println("TOTAL TIME: " + clockStop + " ms");
+			System.err.println();
+		    }
+		}
+	    }
+	}
+	catch (Exception e) {
+	    e.printStackTrace();
+	    System.exit(1);
+	}
+    }
+
+    public static String[] tokenise(String str) {
+	StringTokenizer tok = new StringTokenizer(str);
+
+	int ntokens = tok.countTokens();
+
+	String[] tokens = new String[ntokens];
+
+	for (int i = 0; i < ntokens && tok.hasMoreTokens(); i++)
+	    tokens[i] = tok.nextToken();
+
+	return tokens;
+    }
+
+    public static void report() {
+	long timenow = System.currentTimeMillis();
+
+	System.out.println("******************** REPORT ********************");
+	System.out.println("Time: " + (timenow - lasttime));
+
+	System.out.println("Memory (kb): (free/total) " + runtime.freeMemory()/1024 + "/" + runtime.totalMemory()/1024);
+	System.out.println("************************************************");
+	System.out.println();
+    }
+
+    public static void printUsage(PrintStream ps) {
+	ps.println("MANDATORY PARAMETERS:");
+	ps.println("\t-instance\tName of instance");
+	ps.println("\t-organism\tName of organism");
+    }
+}
+
+class MyListener implements ManagerEventListener {
+    private long clock;
+    private Runtime runtime = Runtime.getRuntime();
+
+    public void managerUpdate(ManagerEvent event) {
+	switch (event.getState()) {
+	case ManagerEvent.START:
+	    System.err.println("START -- " + event.getMessage());
+	    clock = System.currentTimeMillis();
+	    break;
+
+	case ManagerEvent.WORKING:
+	    //System.err.print('.');
+	    break;
+
+	case ManagerEvent.END:
+	    //System.err.println();
+	    clock = System.currentTimeMillis() - clock;
+	    System.err.println("END   -- " + clock + " ms");
+	    System.err.println("MEM      FREE=" + runtime.freeMemory()/1024 + ", TOTAL=" + runtime.totalMemory()/1024);
+	    System.err.println();
+	    break;
+	}
+    }
+}
