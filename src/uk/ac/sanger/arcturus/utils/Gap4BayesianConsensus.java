9dd61a6e43e56438495e60bc4bbaff7ff5512509
David Harper <adh@sanger.ac.uk>
2005-06-07 12:23:01 +0000

    Added the classes (originally from the Minerva CVS repository) for the calculation of contig consensus.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@899 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/utils/Gap4BayesianConsensus.java
@@ -0,0 +1,308 @@
+package uk.ac.sanger.arcturus.utils;
+
+import java.io.PrintStream;
+import java.text.DecimalFormat;
+
+// This class implements the Gap4 Bayesian consensus algorithm, or something which
+// closely approximates it.
+//
+// See the function process_frags in src/gap4/qual.c in the Staden package for the
+// definitive version.
+
+public class Gap4BayesianConsensus implements ConsensusAlgorithm {
+    private static int[] dependent_table = {0, 0, 8, 16, 24, 31, 38, 44, 48, 51, 52};
+    private static String basecodes = "ACGT*-";
+
+    private boolean pad_present = false;
+    private boolean best_is_current = true;
+    private int qhighest[][] = new int[6][4];
+    private int qcount[][] = new int[6][4];
+    private int depth = 0;
+    private double bayesian[][] = new double[5][30];
+    private int bestbase = -1;
+    private double probs[] = new double[6];
+    private int scores[] = new int[6];
+    private int nEvents = 0;
+    private PrintStream debugps = null;
+    private DecimalFormat decimal = null;
+
+    private final static double log10 = Math.log(10.0);
+
+    public void setDebugPrintStream(PrintStream debugps) {
+	this.debugps = debugps;
+	if (debugps != null && decimal == null) {
+	    decimal = new DecimalFormat("0.000000");
+	}
+    }
+
+    public PrintStream getDebugPrintStream() { return debugps; }
+
+    public boolean reset() {
+	if (debugps != null) {
+	    debugps.println();
+	    debugps.println("Gap4BayesianConsensus::reset");
+	}
+
+	bestbase = -1;
+	depth = 0;
+
+	pad_present = false;
+	best_is_current = true;
+
+	for (int i = 0; i < 6; i++)
+	    for (int j = 0; j < 4; j++)
+		qhighest[i][j] = qcount[i][j] = 0;
+
+	for (int j = 0; j < 6; j++)
+	    scores[j] = 0;
+
+	nEvents = 0;
+
+	return true;
+    }
+
+    public boolean addBase(char base, int quality, int strand, int chemistry) {
+	if (debugps != null) {
+	    debugps.println();
+	    debugps.println("Gap4BayesianConsensus::addBase(" + base + ", " + quality + ", " + strand + ", " + chemistry + ")");
+	}
+
+	if (strand == ConsensusAlgorithm.UNKNOWN || chemistry == ConsensusAlgorithm.UNKNOWN)
+	    return false;
+
+	// iBase is the first index into the arrays "qhighest" and "qcount"
+	//
+	// 0 -> A
+	// 1 -> C
+	// 2 -> G
+	// 3 -> T
+	// 4 -> *
+	// 5 -> -
+
+	int iBase = -1;
+
+	switch (base) {
+	case 'a': case 'A': iBase = 0; break;
+	case 'c': case 'C': iBase = 1; break;
+	case 'g': case 'G': iBase = 2; break;
+	case 't': case 'T': iBase = 3; break;
+	case '*':           iBase = 4; break;
+	case '-':           iBase = 5; break;
+	default: return false;
+	}
+
+	if (debugps != null)
+	    debugps.println("  iBase = " + iBase);
+
+	// iStrandAndChemistry is the second index into the arrays "qhighest" and "qcount"
+	//
+	// 0 -> Forward strand, Primer
+	// 1 -> Reverse strand, Primer
+	// 2 -> Forward strand, Terminator
+	// 3 -> Reverse strand, Terminator
+
+	int iStrandAndChemistry = 0;
+	iStrandAndChemistry |= (strand == ConsensusAlgorithm.FORWARD ? 0 : 1);
+	iStrandAndChemistry |= (chemistry == ConsensusAlgorithm.PRIMER ? 0 : 1) << 1;
+
+	if (debugps != null)
+	    debugps.println("  iStrandAndChemistry = " + iStrandAndChemistry);
+
+	// FROM qual.c:
+	//
+	// qual == 1 implies less likely to be the called base. We map this to 2, as
+	// this is undesirable.
+
+	if (quality == 1)
+	    quality = 2;
+
+	// FROM qual.c:
+	//
+	// qual == 0 implies "ignore this base". We do this by changing the type to dash,
+	// to force even spread of probability. Otherwise, we'd actually be negatively
+	// weighting this base type.
+
+	if (quality == 0)
+	    iBase = 5;
+
+	// Check the quality against the current maximum value for this base and strand/chemistry
+
+	if (quality > qhighest[iBase][iStrandAndChemistry])
+	    qhighest[iBase][iStrandAndChemistry] = quality;
+
+	qcount[iBase][iStrandAndChemistry]++;
+
+	depth++;
+
+	if (debugps != null) {
+	    debugps.println("  qhighest[" + iBase + "][" + iStrandAndChemistry + "] = " + qhighest[iBase][iStrandAndChemistry]);
+	    debugps.println("  qCount[" + iBase + "][" + iStrandAndChemistry + "] = " + qcount[iBase][iStrandAndChemistry]);
+	}
+
+	if (iBase == 4)
+	    pad_present = true;
+
+	best_is_current = false;
+
+	return true;
+    }
+
+    private void findBestBase() {
+	if (best_is_current)
+	    return;
+
+	if (debugps != null) {
+	    debugps.println();
+	    debugps.println("Gap4BayesianConsensus::findBestBase()");
+	}
+
+	int nbase_types = pad_present ? 5 : 4;
+	int nevents = 0;
+
+	if (debugps != null)
+	    debugps.println("  nbase_types = " + nbase_types);
+
+	for (int k = 0; k < 4; k++) {
+	    if (qhighest[5][k] > 0) {
+		double tmp = (double)(qhighest[5][k] + dependent_table[Math.min(qcount[5][k], 10)]);
+		double prob = Math.exp(-log10 * tmp/10.0);
+		
+		double sharedprob = prob/(double)nbase_types;
+
+		for (int i = 0; i < 5; i++)
+		    bayesian[i][nEvents] = sharedprob;
+
+		if (debugps != null) {
+		    debugps.println();
+		    debugps.println("  qhighest[5][" + k + "] = " + qhighest[5][k]);
+		    debugps.println("  qcount[5][" + k + "] = " + qcount[5][k]);
+		    debugps.println("  score = " + tmp);
+		    debugps.println("  Sharing probability " + decimal.format(sharedprob) + " amongst " + nbase_types + " base types");
+		}
+
+		nEvents++;
+	    }
+	}
+	    
+	for (int j = 0; j < nbase_types; j++) {
+	    for (int k = 0; k < 4; k++) {
+		if (qhighest[j][k] > 0) {
+		    double tmp = (double)(qhighest[j][k] + dependent_table[Math.min(qcount[j][k], 10)]);
+		    double prob = Math.exp(-log10 * tmp/10.0);
+
+		    double sharedprob = prob/(double)(nbase_types - 1);
+
+		    prob = 1.0 - prob;
+
+		    for (int i = 0; i < 5; i++)
+			bayesian[i][nEvents] = sharedprob;
+		    
+		    bayesian[j][nEvents] = prob;
+
+		    if (debugps != null) {
+			debugps.println();
+			debugps.println("  qhighest[" + j + "][" + k + "] = " + qhighest[j][k]);
+			debugps.println("  qcount[" + j + "][" + k + "] = " + qcount[j][k]);
+			debugps.println("  score = " + tmp);
+			debugps.println("  Assigning probability " + decimal.format(prob) + " to base " + j);
+			debugps.println("  Sharing probability " + decimal.format(sharedprob) + " amongst other bases");
+		    }
+			
+		    nEvents++;
+		}
+	    }
+	}
+
+	double qnorm = 0.0;
+	double highest_product = 0.0;
+	bestbase = 5;
+
+	if (debugps != null) {
+	    debugps.println();
+	    switch (nEvents) {
+	    case 0:
+		debugps.print("There are NO independent events");
+		break;
+	    case 1:
+		debugps.print("There is one independent event");
+		break;
+	    default:
+		debugps.print("There are " + nEvents + " independent events");
+		break;
+	    }
+
+	    debugps.println(" for the Bayesian calculation");
+	}
+
+	if (nEvents > 0) {
+	    if (debugps != null)
+		debugps.println();
+
+	    for (int j = 0; j < nbase_types; j++) {
+		double product = 1.0;
+
+		for (int k = 0; k < nEvents; k++)
+		    product *= bayesian[j][k];
+
+		qnorm += product;
+
+		probs[j] = product;
+
+		if (product > highest_product) {
+		    highest_product = product;
+		    bestbase = j;
+		}
+	    }
+
+	    for (int j = 0; j < nbase_types; j++) {
+		probs[j] /= qnorm;
+		if (probs[j] < 1.0) {
+		    double log10probs = Math.log(1.0 - probs[j]) / log10;
+		    scores[j] = (int)Math.round(-10.0 * log10probs);
+		    if (scores[j] > 99)
+			scores[j] = 99;
+		} else
+		    scores[j] = 99;
+
+		if (debugps != null) {
+		    debugps.println("  Normalised probability and score for base " + j + " = " + 
+				    decimal.format(probs[j]) + ", " + scores[j]);
+		}
+	    }
+	}
+
+	best_is_current = true;
+    }
+
+    public char getBestBase() {
+	findBestBase();
+
+	if (bestbase < 0)
+	    return '?';
+	else
+	    return basecodes.charAt(bestbase);
+    }
+
+    public int getBestScore() {
+	findBestBase();
+ 
+	if (bestbase < 0)
+	    return -1;
+	else
+	    return scores[bestbase];
+    }
+
+    public int getScoreForBase(char base) {
+	findBestBase();
+
+	switch (base) {
+	case 'a': case 'A': return scores[0];
+	case 'c': case 'C': return scores[1];
+	case 'g': case 'G': return scores[2];
+	case 't': case 'T': return scores[3];
+	case '*':           return scores[4];
+	case '-':           return scores[5];
+	default:            return -1;
+	}
+    }
+}
---
+++ src/uk/ac/sanger/arcturus/utils/WeightedMajorityVote.java
@@ -0,0 +1,82 @@
+package uk.ac.sanger.arcturus.utils;
+
+public class WeightedMajorityVote implements ConsensusAlgorithm {
+    private int totalA, totalC, totalG, totalT, totalX;
+    private char bestbase;
+    private int bestscore;
+
+    public boolean reset() {
+	totalA = totalC = totalG = totalT = totalX = 0;
+	bestbase = '?';
+	bestscore = -1;
+
+	return true;
+    }
+
+    public boolean addBase(char base, int quality, int strand, int chemistry) {
+	switch (base) {
+	case 'a': case 'A': totalA += quality; break;
+	case 'c': case 'C': totalC += quality; break;
+	case 'g': case 'G': totalG += quality; break;
+	case 't': case 'T': totalT += quality; break;
+	case '*':           totalX += quality; break;
+	}
+
+	return true;
+    }
+
+    private void findBestBase() {
+	if (bestscore < 0) {
+	    bestbase = 'A';
+	    bestscore = totalA;
+
+	    if (totalC > bestscore) {
+		bestbase = 'C';
+		bestscore = totalC;
+	    }
+
+	    if (totalG > bestscore) {
+		bestbase = 'G';
+		bestscore = totalG;
+	    }
+
+	    if (totalT > bestscore) {
+		bestbase = 'T';
+		bestscore = totalT;
+	    }
+
+	    if (totalX > bestscore) {
+		bestbase = '*';
+		bestscore = totalX;
+	    }
+	}
+    }
+
+    public char getBestBase() {
+	findBestBase();
+	return bestbase;
+    }
+
+    public int getBestScore() {
+	findBestBase();
+	return bestscore;
+    }
+
+    public int getScoreForBase(char base) {
+	int value = 0;
+
+	switch (base) {
+	case 'a': case 'A': value = totalA;
+	case 'c': case 'C': value = totalC;
+	case 'g': case 'G': value = totalG;
+	case 't': case 'T': value = totalT;
+	case '*':           value = totalX;
+	default:            value = 0;
+	}
+
+	if (value > 99)
+	    return 99;
+	else
+	    return value;
+    }
+}
