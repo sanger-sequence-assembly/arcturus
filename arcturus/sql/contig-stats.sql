1ad55d7bfeeb5bb77bfe1bcf36536f77729c1de6
David Harper <adh@sanger.ac.uk>
2005-06-20 10:56:18 +0000

    Added SQL script to display statistics on current contigs.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@928 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/sql/contig-stats.sql
@@ -0,0 +1,52 @@
+create temporary table currentcontigs as
+select CONTIG.contig_id,nreads,ncntgs,length,updated,project_id
+  from CONTIG left join C2CMAPPING
+  on CONTIG.contig_id = C2CMAPPING.parent_id
+  where C2CMAPPING.parent_id is null;
+
+select 'ALL CONTIGS';
+
+select PROJECT.name,count(*) as contigs,
+  sum(nreads) as reads,
+  sum(length) as length,
+  round(avg(length)) as avglen,
+  round(std(length)) as stdlen,
+  max(length) as maxlen
+  from currentcontigs left join PROJECT using(project_id)
+  group by currentcontigs.project_id order by name asc;
+
+select 'CONTIGS 5kb OR MORE';
+
+select PROJECT.name,count(*) as contigs,
+  sum(nreads) as reads,
+  sum(length) as length,
+  round(avg(length)) as avglen,
+  round(std(length)) as stdlen,
+  max(length) as maxlen
+  from currentcontigs left join PROJECT using(project_id)
+  where length >= 5000
+  group by currentcontigs.project_id order by name asc;
+
+select 'CONTIGS 10kb OR MORE';
+
+select PROJECT.name,count(*) as contigs,
+  sum(nreads) as reads,
+  sum(length) as length,
+  round(avg(length)) as avglen,
+  round(std(length)) as stdlen,
+  max(length) as maxlen
+  from currentcontigs left join PROJECT using(project_id)
+  where length >= 1-000
+  group by currentcontigs.project_id order by name asc;
+
+select 'CONTIGS 100kb OR MORE';
+
+select PROJECT.name,count(*) as contigs,
+  sum(nreads) as reads,
+  sum(length) as length,
+  round(avg(length)) as avglen,
+  round(std(length)) as stdlen,
+  max(length) as maxlen
+  from currentcontigs left join PROJECT using(project_id)
+  where length >= 100000
+  group by currentcontigs.project_id order by name asc;
