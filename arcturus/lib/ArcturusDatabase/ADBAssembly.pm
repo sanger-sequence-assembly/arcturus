ec7b8af81082ba1cb201ada5fd1333977b750038
Ed Zuiderwijk <ejz@sanger.ac.uk>
2005-06-27 16:01:57 +0000

    Added method returning assembly data for read
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@938 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/lib/ArcturusDatabase/ADBAssembly.pm
+++ arcturus/lib/ArcturusDatabase/ADBAssembly.pm
@@ -20,8 +20,41 @@ sub new {
     return $this;
 }
 
+sub getAssemblyDataforReadName { # TO BE TESTED
+# return project data keyed on contig_id for input readname
+    my $this = shift;
+    my $readname = shift;
+
+    my $contig_items = "CONTIG.contig_id,gap4name,CONTIG.created";
+    my $projectitems = "PROJECT.name,PROJECT.owner,assembly_id";
+
+    my $query = "select distinct $contig_items,$projectitems"
+              . "  from READS,SEQ2READ,MAPPING,CONTIG,PROJECT"
+              . " where CONTIG.project_id = PROJECT.project_id"
+              . "   and CONTIG.contig_id = MAPPING.contig_id"
+              . "   and MAPPING.seq_id = SEQ2READ.seq_id"
+              . "   and SEQ2READ.read_id = READS.read_id"
+	      . "   and READS.readname = ?"
+	      . " order by contig_id DESC";
+
+    my $dbh = $this->getConnection();
+
+    my $sth = $dbh->prepare_cached($query);
+
+    $sth->execute($readname) || &queryFailed($query,$readname);
+
+    my $resultlist = {};
+    while (my ($contig_id,@items) = $sth->fetchrow_array()) {
+        $resultlist->{$contig_id} = [@items];
+    }
+
+    $sth->finish();
+
+    return $resultlist;
+}
+
 #------------------------------------------------------------------------------
-# methods dealing with Projects
+# methods dealing with Projects 
 #------------------------------------------------------------------------------
 
 sub aborttest {
