c93933a190a02ec00b285d4f24fd8a1eee00094f
David Harper <adh@sanger.ac.uk>
2005-07-19 15:37:00 +0000

    Split all of the classes into their own files, to make it easier to experiment with different ways to present the data.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@973 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/scaffolding/Gap.java
@@ -0,0 +1,21 @@
+package scaffolding;
+
+
+class Gap extends Core {
+    private int size = -1;
+
+    public Gap(int size) {
+	this.size = size;
+    }
+
+    public int getSize() { return size; }
+
+    public String toString() {
+	int nBridges = children.size();
+
+	if (nBridges > 0)
+	    return "Gap[size=" + size + ", " + nBridges + " bridges]";
+	else
+	    return "Gap[size=" + size + ", no bridges]";
+    }
+}
