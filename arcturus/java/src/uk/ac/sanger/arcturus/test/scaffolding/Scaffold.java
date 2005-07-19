c6302849e652dda5ad581acf617ef324d521194c
David Harper <adh@sanger.ac.uk>
2005-07-19 15:37:00 +0000

    Split all of the classes into their own files, to make it easier to experiment with different ways to present the data.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@973 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/scaffolding/Scaffold.java
@@ -0,0 +1,21 @@
+package scaffolding;
+
+
+class Scaffold extends Core {
+    private int id = -1;
+    private boolean forward = true;
+
+    public Scaffold(int id, boolean forward) {
+	this.id = id;
+	this.forward = forward;
+    }
+
+    public int getId() { return id; }
+
+    public boolean isForward() { return forward; }
+
+    public String toString() {
+	return "Scaffold[id=" + id + ", sense=" +
+	    (forward ? "F" : "R") + "]";
+    }
+}
---
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/scaffolding/SuperScaffold.java
@@ -0,0 +1,19 @@
+package scaffolding;
+
+class SuperScaffold extends Core {
+    private int id = -1;
+    private int size = 0;
+
+    public SuperScaffold(int id, int size) {
+	this.id = id;
+	this.size = size;
+    }
+
+    public int getId() { return id; }
+
+    public int getSize() { return size; }
+
+    public String toString() {
+	return "SuperScaffold[id=" + id + ", size=" + size + "]";
+    }
+}
