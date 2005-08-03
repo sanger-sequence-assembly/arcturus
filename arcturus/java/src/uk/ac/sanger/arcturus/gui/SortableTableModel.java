dbdd6e8f6e5e7c3d7f66fa7007c52687a917c448
David Harper <adh@sanger.ac.uk>
2005-08-03 13:18:36 +0000

    Added classes to support sortable tables and ISO 8601 formatting of dates.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1005 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/java/src/uk/ac/sanger/arcturus/gui/SortableTableModel.java
@@ -0,0 +1,6 @@
+package uk.ac.sanger.arcturus.gui;
+
+public interface SortableTableModel extends javax.swing.table.TableModel {
+    public boolean isColumnSortable(int column);
+    public void sortOnColumn(int column, boolean ascending);
+}
