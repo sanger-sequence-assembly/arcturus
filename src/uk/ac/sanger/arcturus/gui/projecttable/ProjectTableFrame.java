9df5d3f1f8d9c2b165db6c3314c4ba9b41e0ee85
David Harper <adh@sanger.ac.uk>
2005-08-04 08:06:22 +0000

    Rationalised the methods for displaying the selected projects. This can now be done by double-clicking in the table, right-clicking in the table or via the Project menu.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1013 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/gui/projecttable/ProjectTableFrame.java
+++ src/uk/ac/sanger/arcturus/gui/projecttable/ProjectTableFrame.java
@@ -5,24 +5,44 @@ import uk.ac.sanger.arcturus.gui.*;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 
 public class ProjectTableFrame extends MinervaFrame {
+    protected ProjectTable table = null;
+    protected JMenu projectMenu = null;
+
     public ProjectTableFrame(Minerva minerva) {
 	super(minerva, "Project List : " +  minerva.getArcturusDatabase().getName());
 
 	ProjectTableModel model = new ProjectTableModel( minerva.getArcturusDatabase());
 
-	ProjectTable table = new ProjectTable(model);
+	table = new ProjectTable(model);
 
 	JScrollPane scrollpane = new JScrollPane(table);
 
 	JPanel panel = new JPanel(new BorderLayout());
 
 	panel.add(scrollpane, BorderLayout.CENTER);
-	panel.setPreferredSize(new Dimension(600, 530));
+	panel.setPreferredSize(new Dimension(700, 530));
 
 	setContentPane(panel);
+
+	projectMenu = new JMenu("Project");
+	menubar.add(projectMenu);
+
+	projectMenu.add(new ViewProjectAction("View selected project(s)")); 
+
 	pack();
 	setVisible(true);
+    }
+
+    class ViewProjectAction extends AbstractAction {
+	public ViewProjectAction(String name) {
+	    super(name);
+	}
+
+	public void actionPerformed(ActionEvent event) {
+	    table.displaySelectedProjects();
+	}
     }
 }
