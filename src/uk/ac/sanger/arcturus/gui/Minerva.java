ae1aca2fc2c10f341d942468cb42a739c709b264
David Harper <adh@sanger.ac.uk>
2005-08-24 13:13:56 +0000

    Added a utility class which allows the user to change the colour associated with a specified project.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1026 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/gui/Minerva.java
+++ src/uk/ac/sanger/arcturus/gui/Minerva.java
@@ -8,6 +8,8 @@ import java.io.*;
 import javax.naming.NamingException;
 import javax.naming.Context;
 import java.sql.SQLException;
+import java.awt.Color;
+import java.util.prefs.*;
 
 import uk.ac.sanger.arcturus.ArcturusInstance;
 import uk.ac.sanger.arcturus.database.*;
@@ -16,217 +18,75 @@ import uk.ac.sanger.arcturus.gui.projecttable.ProjectTableFrame;
 
 /**
  * This class is the main class for all GUI applications.
- * It handles user preferences and manages the shared ArcturusDatabase
- * objects. It also maintains a list of all active frames.
+ * It manages the shared ArcturusDatabase objects and maintains
+ * a list of all active frames.
  */
 
 public class Minerva implements WindowListener {
-    public static final String ARCTURUS_USER_DIRECTORY = ".arcturus";
-    public static final String MINERVA_PREFERENCES_FILE = "minerva.prefs";
-
     private static Minerva instance = null;
 
-    protected Properties userProperties = new Properties();
-    protected Properties siteProperties = new Properties();
     protected Vector activeFrames = new Vector();
-    protected ArcturusDatabase adb = null;
+    protected Properties ldapProps = new Properties();
+    protected HashMap databases = new HashMap();
+    protected HashMap instances = new HashMap();
 
-    public static Minerva getInstance(String[] args) {
-	if (instance == null)
-	    instance = new Minerva(args);
-
-	return instance;
-    }
+    protected Preferences appPrefs = Preferences.userNodeForPackage(getClass());
 
     public static Minerva getInstance() {
+	if (instance == null)
+	    instance = new Minerva();
+
 	return instance;
     }
 
-    private Minerva(String[] args) {
-	loadUserProperties();
-	loadSiteProperties();
+    private Minerva() {
+	Properties env = System.getProperties();
 
-	String instance = getStringParameter(args, "-instance");
-	String organism = getStringParameter(args, "-organism");
-
-	if (instance == null) {
-	    System.err.println("No instance specified");
-	    System.exit(1);
-	}
-
-	if (organism == null) {
-	    System.err.println("No organism specified");
-	    System.exit(1);
-	}
-
-	try {
-	    adb = createArcturusDatabase(instance ,organism);
-	}
-	catch (Exception e) {
-	    System.err.println("Failed to create an ArcturusDatabase  for instance \"" + instance +
-			       "\", organism \"" + organism + "\" : " + e.getMessage());
-	    System.exit(1);
-	}
-
-	if (adb == null) {
-	    System.err.println("ArcturusDatabase object for instance \"" + instance +
-			       "\", organism \"" + organism + "\" was null");
-	    System.exit(1);
-	}
-
-	//restoreOldSessions();
-
-	//createNewSessions(args);
-    }
-
-    private void loadUserProperties() {
-	String userHome = System.getProperty("user.home");
-
-	File file = new File(userHome);
-
-	file = new File(file, ARCTURUS_USER_DIRECTORY);
-
-	if (!file.exists()) {
-	    System.err.println("Cannot load user preferences: directory " + file +
-			       " does not exist");
-	    return;
-	}
-
-	if (!file.isDirectory()) {
-	    System.err.println("Cannot load user preferences: " + file +
-			       " exists but is not a directory");
-	}
-
-	file = new File(file, MINERVA_PREFERENCES_FILE);
-
-	loadPropertiesFromFile(userProperties, file);
-    }
-
-    private void loadSiteProperties() {
-	String siteHome = System.getProperty("arcturus.site.home");
-
-	if (siteHome == null) {
-	    System.err.println("Cannot load site preferences: site home directory is not defined");
-	    return;
-	}
-
-	File file = new File(siteHome);
-
-	if (!file.exists()) {
-	    System.err.println("Cannot load site preferences: directory " + file + " does not exist");
-	    return;
-	}
-
-	if (!file.isDirectory()) {
-	    System.err.println("Cannot load site preferences: " + file + " exists but is not a directory");
-	    return;
-	}
-
-	if (!file.canRead()) {
-	    System.err.println("Cannot load site preferences: directory " + file + " exists but is not readable");
-	    return;
-	}
-
-	file = new File(file, MINERVA_PREFERENCES_FILE);
-
-	loadPropertiesFromFile(siteProperties, file);
-    }
-
-    private void loadPropertiesFromFile(Properties props, File file) {
-	String preftype = (props == userProperties) ? "user" : (props == siteProperties) ? "site" : "(unknown)";
-
-	String warning = "Cannot load " + preftype + " preferences:";
-
-	if (!file.exists()) {
-	    System.err.println(warning + file + " does not exist");
-	    return;
-	}
-
-	if (!file.isFile()) {
-	    System.err.println(warning + file + " is not a file");
-	    return;
-	}
-
-	if (!file.canRead()) {
-	    System.err.println(warning + file + " is not readable");
-	    return;
-	}
-
-	try {
-	    FileInputStream fis = new FileInputStream(file);
-	    props.load(fis);
-	    fis.close();
-	}
-	catch (IOException ioe) {
-	    System.err.println(warning + " an IOException occurred when attempting to read " + file);
-	    System.err.println("The error message is: " + ioe.getMessage());
-	}
-    }
-
-    private String getStringParameter(String[] args, String key) {
-	if (args == null || key == null)
-	    return null;
-
-	for (int i = 0; i < args.length - 1; i++)
-	    if (args[i].equalsIgnoreCase(key) && !args[i+1].startsWith("-"))
-		return args[i+1];
-
-	return null;
-    }
-
-    private boolean getBooleanParameter(String[] args, String key) {
-	if (args == null || key == null)
-	    return false;
-
-	String notkey = "-no" + key.substring(1);
-
-	boolean value = false;
-
-	for (int i = 0; i < args.length; i++) {
-	    if (args[i].equalsIgnoreCase(key))
-		value = true;
-
-	    if (args[i].equalsIgnoreCase(notkey))
-		value = false;
-	}
-
-	return value;
+	ldapProps.put(Context.INITIAL_CONTEXT_FACTORY, env.get(Context.INITIAL_CONTEXT_FACTORY));
+	ldapProps.put(Context.PROVIDER_URL, env.get(Context.PROVIDER_URL));
     }
 
     private ArcturusDatabase createArcturusDatabase(String instance, String organism)
 	throws NamingException, SQLException {
-	Properties props = new Properties();
+	ArcturusInstance ai = (ArcturusInstance)instances.get(instance);
 
-	Properties env = System.getProperties();
+	if (ai == null) {
+	    ai = new ArcturusInstance(ldapProps, instance);
+	    instances.put(instance, ai);
+	}
 
-	props.put(Context.INITIAL_CONTEXT_FACTORY, env.get(Context.INITIAL_CONTEXT_FACTORY));
-	props.put(Context.PROVIDER_URL, env.get(Context.PROVIDER_URL));
+	if (ai != null) {
+	    ArcturusDatabase adb = ai.findArcturusDatabase(organism);
 
-	ArcturusInstance ai = new ArcturusInstance(props, instance);
+	    databases.put(organism, adb);
 
-	ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+	    return adb;
+	} else
+	    return null;
+    }
+
+    public ArcturusDatabase getArcturusDatabase(String instance, String organism)
+	throws NamingException, SQLException {
+	String key = instance + "/" + organism;
+	ArcturusDatabase adb = (ArcturusDatabase)databases.get(organism);
+
+	if (adb == null) {
+	    adb = createArcturusDatabase(instance, organism);
+
+	    if (adb != null)
+		databases.put(organism, adb);
+	}
 
 	return adb;
     }
 
-    public ArcturusDatabase getArcturusDatabase() { return adb; }
+    public Color getColourForProject(String assembly, String project) {
+	Preferences prefs = appPrefs.node(assembly);
+	prefs = prefs.node(project);
 
-    public String getUserProperty(String key) {
-	return userProperties.getProperty(key);
-    }
+	int icol = prefs.getInt("colour", -1);
 
-    public Object setUserProperty(String key, String value) {
-	return userProperties.setProperty(key, value);
-    }
-
-    public String getSiteProperty(String key) {
-	return siteProperties.getProperty(key);
-    }
-
-    public String getProperty(String key) {
-	String value = siteProperties.getProperty(key);
-
-	return (value != null) ? value : userProperties.getProperty(key);
+	return (icol < 0) ? null : new Color(icol);
     }
     
     /**
@@ -291,18 +151,67 @@ public class Minerva implements WindowListener {
 	frame.show();
     }
 
-    public void run() {
+    public void run(final String[] args) {
 	SwingUtilities.invokeLater(new Runnable() {
 		public void run() {
-		    MinervaFrame frame = new ProjectTableFrame(Minerva.this);
-		    displayNewFrame(frame);
+		    restoreOldSessions();
+		    createNewSessions(args);
 		}
 	    });
     }
 
-    public static void main(String[] args) {
-	Minerva minerva = Minerva.getInstance(args);
+    private void restoreOldSessions() {}
 
-	minerva.run();
+    private void createNewSessions(String[] args) {
+	String instance = getStringParameter(args, "-instance");
+	String organism = getStringParameter(args, "-organism");
+
+	if (instance != null && organism != null) {
+	    try {
+		ArcturusDatabase adb = getArcturusDatabase(instance, organism);
+		if (adb != null) {
+		    MinervaFrame frame = new ProjectTableFrame(this, adb);
+		    displayNewFrame(frame);
+		}
+	    }
+	    catch (Exception e) {
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    private String getStringParameter(String[] args, String key) {
+	if (args == null || key == null)
+	    return null;
+
+	for (int i = 0; i < args.length - 1; i++)
+	    if (args[i].equalsIgnoreCase(key) && !args[i+1].startsWith("-"))
+		return args[i+1];
+
+	return null;
+    }
+
+    private boolean getBooleanParameter(String[] args, String key) {
+	if (args == null || key == null)
+	    return false;
+
+	String notkey = "-no" + key.substring(1);
+
+	boolean value = false;
+
+	for (int i = 0; i < args.length; i++) {
+	    if (args[i].equalsIgnoreCase(key))
+		value = true;
+
+	    if (args[i].equalsIgnoreCase(notkey))
+		value = false;
+	}
+
+	return value;
+    }
+
+    public static void main(String[] args) {
+	Minerva minerva = Minerva.getInstance();
+	minerva.run(args);
     }
 }
