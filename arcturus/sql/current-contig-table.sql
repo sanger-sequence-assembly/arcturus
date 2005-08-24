1549a159442abec69f6e1de7c55d75b5286790dd
David Harper <adh@sanger.ac.uk>
2005-08-24 15:07:48 +0000

    Added an SQL script which searches for pUC bridges and lists all templates having two or more "hook" reads at the ends of contigs.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1030 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/sql/current-contig-table.sql
+++ arcturus/sql/current-contig-table.sql
@@ -1,4 +1,17 @@
-create temporary table currentcontigs as
+create temporary table currentcontigs (
+  contig_id mediumint unsigned not null,
+  gap4name binary(32) not null,
+  nreads mediumint unsigned not null,
+  ncntgs mediumint unsigned not null,
+  length mediumint unsigned not null,
+  created datetime not null,
+  updated datetime not null,
+  project_id int unsigned not null,
+  primary key (contig_id),
+  unique key (gap4name)
+);
+
+insert into currentcontigs(contig_id,gap4name,nreads,ncntgs,length,created,updated,project_id)
 select CONTIG.contig_id,gap4name,nreads,ncntgs,length,created,updated,project_id
   from CONTIG left join C2CMAPPING
   on CONTIG.contig_id = C2CMAPPING.parent_id
---
+++ arcturus/sql/puchooks.sql
@@ -0,0 +1,51 @@
+create temporary table currentcontigs (
+	contig_id mediumint unsigned not null,
+	gap4name binary(32) not null,
+	nreads mediumint unsigned not null,
+	ncntgs mediumint unsigned not null,
+	length mediumint unsigned not null,
+	created datetime not null,
+	updated datetime not null,
+	project_id int unsigned not null,
+	primary key (contig_id),
+	unique key (gap4name)
+);
+
+insert into currentcontigs(contig_id,gap4name,nreads,ncntgs,length,created,updated,project_id)
+	select CONTIG.contig_id,gap4name,nreads,ncntgs,length,created,updated,project_id
+	from CONTIG left join C2CMAPPING
+	on CONTIG.contig_id = C2CMAPPING.parent_id
+	where C2CMAPPING.parent_id is null;
+
+create temporary table hooks1 as
+	select currentcontigs.contig_id,seq_id,cfinish as pos,'L' as end
+	from currentcontigs left join MAPPING using(contig_id)
+	where cfinish < 4000 and direction = 'Reverse';
+
+insert into hooks1(contig_id,seq_id,pos,end)
+	select currentcontigs.contig_id,seq_id,length - cstart,'R'
+	from currentcontigs left join MAPPING using(contig_id)
+	where cstart > length - 4000 and direction = 'Forward';
+
+create temporary table hooks2 as
+	select contig_id,read_id,pos,end
+	from hooks1 left join SEQ2READ using(seq_id);
+
+drop temporary table hooks1;
+
+create temporary table hooks as
+	select contig_id,hooks2.read_id,strand,READS.template_id,sihigh,pos,end
+	from hooks2,READS,TEMPLATE,LIGATION
+	where hooks2.read_id = READS.read_id
+		and READS.template_id = TEMPLATE.template_id
+		and TEMPLATE.ligation_id = LIGATION.ligation_id;
+
+drop temporary table hooks2;
+
+delete from hooks where pos > sihigh;
+
+create temporary table pucbridges as
+	select template_id,count(*) as hits
+	from hooks
+	group by template_id
+	having hits > 1;
