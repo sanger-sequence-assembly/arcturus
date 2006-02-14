9c74759b5785cf665690db19ee5c5f0e920d8d70
David Harper <adh@sanger.ac.uk>
2006-02-14 17:03:59 +0000

    Added a class to represetn an organism.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1221 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/java/src/uk/ac/sanger/arcturus/data/Organism.java
@@ -0,0 +1,28 @@
+package uk.ac.sanger.arcturus.data;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class Organism {
+    protected String name;
+    protected String description;
+    protected DataSource datasource;
+
+    public Organism(String name, String description, DataSource datasource) {
+	this.name = name;
+	this.description = description;
+	this.datasource = datasource;
+    }
+
+    public String getName() { return name; }
+
+    public String getDescription() { return description; }
+
+    public DataSource getDataSource() { return datasource; }
+
+    public String toString() {
+	return "Organism[name=" + name +
+	    ", description=" + description +
+	    ", datasource=" + datasource + "]";
+    }
+}
