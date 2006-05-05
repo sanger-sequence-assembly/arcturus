76c0c0aea7ab15f03f8a825717bb7a8342301d6b
David Harper <adh@sanger.ac.uk>
2006-05-05 12:39:50 +0000

    Created a package to test the generic display package.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1346 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/contigdisplay/ContigFeature.java
@@ -0,0 +1,68 @@
+package contigdisplay;
+
+import java.awt.Point;
+import java.awt.Dimension;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+import uk.ac.sanger.arcturus.gui.genericdisplay.*;
+
+public class ContigFeature implements Feature {
+    protected Contig contig;
+    protected Point position;
+    protected Dimension size;
+    protected boolean forward;
+    protected Set bridgeSet = new HashSet();
+    protected DrawableFeature parent;
+
+    public ContigFeature(Contig contig, Point position, boolean forward) {
+	this.contig = contig;
+	this.position = position;
+	this.forward = forward;
+	this.size = new Dimension(contig.getLength(), 20);
+    }
+
+    public Point getPosition() { return position; }
+
+    public void setPosition(Point position) {
+	this.position = position;
+	recalculateBridgeFeatures();
+    }
+
+    public Dimension getSize() { return size; }
+
+    public Object getClientObject() { return contig; }
+
+    public boolean isForward() { return forward; }
+
+    public String toString() {
+	return "ContigFeature[contig=" + contig + ", position=" + position +
+	    ", size=" + size + ", forward=" + forward + "]";
+    }
+
+    public void addBridgeFeature(BridgeFeature bf) {
+	bridgeSet.add(bf);
+    }
+
+    private void recalculateBridgeFeatures() {
+	for (Iterator iter = bridgeSet.iterator(); iter.hasNext();) {
+	    BridgeFeature bf = (BridgeFeature)iter.next();
+
+	    bf.getParent().calculateBoundingShape();
+	}
+    }
+
+    public void setParent(DrawableFeature parent) {
+	this.parent = parent;
+    }
+
+    public DrawableFeature getParent() { return parent; }
+
+    public Point getLeftEnd() {
+	return new Point(position.x, position.y + size.height/2);
+    }
+
+    public Point getRightEnd() {
+	return new Point(position.x + size.width, position.y + size.height/2);
+    }
+}
