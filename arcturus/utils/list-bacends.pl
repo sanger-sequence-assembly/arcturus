c2ee0b3eaf1fb1ac06d1a0c4cde02daae13900f1
David Harper <adh@sanger.ac.uk>
2006-05-15 15:39:39 +0000

    Added a script which lists the consistent BAC end or fosmid end read pairs in one or more contigs.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1367 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/utils/list-bacends.pl
@@ -0,0 +1,136 @@
+#!/usr/local/bin/perl
+
+use ArcturusDatabase;
+use Read;
+
+use FileHandle;
+
+use strict;
+
+my $nextword;
+my $instance;
+my $organism;
+my $contigid;
+my $clonename;
+my $minlen;
+
+while ($nextword = shift @ARGV) {
+    $instance = shift @ARGV if ($nextword eq '-instance');
+    $organism = shift @ARGV if ($nextword eq '-organism');
+
+    $contigid = shift @ARGV if ($nextword eq '-contig');
+
+    $clonename = shift @ARGV if ($nextword eq '-clone');
+
+    $minlen = shift @ARGV if ($nextword eq '-minlen');
+}
+
+unless (defined($instance) &&
+	defined($organism) &&
+	defined($clonename)) {
+    &showUsage();
+    exit(0);
+}
+
+my $adb;
+
+$adb = new ArcturusDatabase(-instance => $instance,
+			    -organism => $organism);
+
+die "Failed to create ArcturusDatabase" unless $adb;
+
+my $dbh = $adb->getConnection();
+
+my ($query, $stmt);
+
+my @contigs;
+
+if (defined($contigid)) {
+    push @contigs, $contigid;
+} else {
+    $query =  "select CONTIG.contig_id from CONTIG left join C2CMAPPING" .
+	" on CONTIG.contig_id = C2CMAPPING.parent_id" .
+	" where C2CMAPPING.parent_id is null";
+
+    $query .= " and length > $minlen" if defined($minlen);
+
+    $query .= " order by length desc";
+
+    $stmt = $dbh->prepare($query);
+    &db_die("Failed to create query \"$query\"");
+
+    $stmt->execute();
+    &db_die("Failed to execute query \"$query\"");
+
+    while (($contigid) = $stmt->fetchrow_array()) {
+	push @contigs, $contigid;
+    }
+
+    $stmt->finish();
+}
+
+my @conditions = ("MAPPING.seq_id = SEQ2READ.seq_id",
+		  "SEQ2READ.read_id = READS.read_id",
+		  "READS.template_id = TEMPLATE.template_id",
+		  "TEMPLATE.ligation_id = LIGATION.ligation_id",
+		  "LIGATION.clone_id = CLONE.clone_id");
+
+my $joins = join(' and ', @conditions);
+
+$query = "select TEMPLATE.template_id,READS.read_id,readname,cstart,cfinish,direction,LIGATION.sihigh" .
+    " from MAPPING,SEQ2READ,READS,TEMPLATE,LIGATION,CLONE" .
+    " where contig_id = ?" .
+    " and $joins and CLONE.name=? order by template_id asc,cstart asc";
+
+$stmt = $dbh->prepare($query);
+&db_die("Failed to create query \"$query\"");
+
+foreach $contigid (@contigs) {
+    $stmt->execute($contigid, $clonename);
+    &db_die("Failed to execute query \"$query\" for contig $contigid");
+    
+    my $last_templateid = -1;
+    my ($last_readname, $last_cstart, $last_cfinish, $last_direction);
+
+    while (my ($templateid, $readid, $readname, $cstart, $cfinish, $direction, $sihigh) =
+	   $stmt->fetchrow_array()) {
+	if ($templateid == $last_templateid) {
+	    if ($last_direction eq 'Forward' && $direction eq 'Reverse') {
+		printf "%8d %8d  %8d  %8d  %-30s  %-30s\n",$contigid,
+		$last_cstart, $cfinish, ($cfinish-$last_cstart-$sihigh),
+		$last_readname, $readname;
+	    } else {
+		print STDERR "Inconsistent: $contigid $last_readname $last_direction $last_cstart to $last_cfinish <-->" .
+		    " $readname $direction $cstart $cfinish\n";
+	    }
+	}
+	
+	($last_templateid, $last_readname, $last_cstart, $last_cfinish, $last_direction) =
+	    ($templateid, $readname, $cstart, $cfinish, $direction);
+    }
+}
+
+$stmt->finish();
+
+$dbh->disconnect();
+
+exit(0);
+
+sub db_die {
+    my $msg = shift;
+    return unless $DBI::err;
+    print STDERR "MySQL error: $msg $DBI::err ($DBI::errstr)\n\n";
+}
+
+sub showUsage {
+    print STDERR "MANDATORY PARAMETERS:\n";
+    print STDERR "\n";
+    print STDERR "  -instance\t\tName of instance\n";
+    print STDERR "  -organism\t\tName of organism\n";
+    print STDERR "  -contig\t\t\tID of contig to analyse\n";
+    print STDERR "  -clone\t\t\tName of clone for BAC/fosmid ends\n";
+    print STDERR "\n";
+    print STDERR "OPTIONAL PARAMETERS:\n";
+    print STDERR "  -contig\t\t\tID of contig to analyse\n";
+    print STDERR "  -minlen\t\t\tMinimum length for contig scan\n";
+}
--- arcturus/utils/repeatfixer.pl
+++ arcturus/utils/repeatfixer.pl
@@ -4,6 +4,7 @@ use ArcturusDatabase;
 use Read;
 
 use FileHandle;
+use Compress::Zlib;
 
 use strict;
 
@@ -81,17 +82,34 @@ while (my ($contigid,$repstart, $repfinish, $repcomment) = $stmt->fetchrow_array
 $stmt->finish();
 
 my %queries = ( "repeatreads",
-	       "select cstart,cfinish,readname,READS.read_id,strand,template_id from MAPPING,SEQ2READ,READS" .
-	       "  where contig_id=? and cstart>=? and cfinish<=? and MAPPING.seq_id=SEQ2READ.seq_id" .
-	       "  and SEQ2READ.read_id=READS.read_id order by cstart asc",
+		"select MAPPING.seq_id,cstart,cfinish,readname,READS.read_id,strand,template_id" .
+		"  from MAPPING,SEQ2READ,READS" .
+		"  where contig_id=? and cstart>=? and cfinish<=? and MAPPING.seq_id=SEQ2READ.seq_id" .
+		"  and SEQ2READ.read_id=READS.read_id order by cstart asc",
 
-	       "partnerreads",
-	       "select readname,READS.read_id,seq_id from READS left join SEQ2READ using(read_id)" .
-	       "  where template_id=? and strand !=?",
+		"partnerreads",
+		"select readname,READS.read_id,seq_id from READS left join SEQ2READ using(read_id)" .
+		"  where template_id=? and strand !=?",
 
-	       "partnermappings",
-	       "select contig_id,cstart,cfinish from MAPPING where seq_id=?"
-	       );
+		"ligations",
+		"select sihigh from TEMPLATE left join LIGATION using(ligation_id)" .
+		"  where template_id=?",
+
+		"partnermappings",
+		"select contig_id,cstart,cfinish,direction from MAPPING where seq_id=?",
+
+		"consensus",
+		"select sequence from CONSENSUS where contig_id=?",
+
+		"readsequence",
+		"select sequence from SEQUENCE where seq_id=?",
+
+		"qualityclip",
+		"select qleft,qright from QUALITYCLIP where seq_id=?",
+
+		"vectorclip",
+		"select svleft,svright from SEQVEC where seq_id=?"
+		);
 
 my $statements;
 
@@ -103,6 +121,8 @@ foreach my $qkey (keys %queries) {
 foreach my $contigid (keys %{$repeats}) {
     my $ctglen = $contigdata->{$contigid}->[2];
 
+    my $consensus = &getConsensus($contigid, $statements->{'consensus'});
+
     print STDERR "\n\nProcessing contig $contigid ($ctglen bp)\n" if $verbose;
 
     foreach my $reprange (@{$repeats->{$contigid}}) {
@@ -112,9 +132,13 @@ foreach my $contigid (keys %{$repeats}) {
 
 	$statements->{"repeatreads"}->execute($contigid, $repstart, $repfinish);
 
-	while (my ($rstart, $rfinish, $readname, $readid, $strand, $templateid) =
+	while (my ($seqid,$rstart, $rfinish, $readname, $readid, $strand, $templateid) =
 	       $statements->{"repeatreads"}->fetchrow_array()) {
 	    print STDERR "\n\t\tREAD $rstart to $rfinish $readname $readid $templateid\n" if $verbose;
+
+	    $statements->{"ligations"}->execute($templateid);
+
+	    my ($sihigh) = $statements->{"ligations"}->fetchrow_array();
 
 	    $statements->{"partnerreads"}->execute($templateid, $strand);
 
@@ -126,18 +150,34 @@ foreach my $contigid (keys %{$repeats}) {
 
 		my $pfound = 0;
 
-		while (my ($pcontigid, $pstart, $pfinish) =
+		while (my ($pcontigid, $pstart, $pfinish, $pdirection) =
 		       $statements->{"partnermappings"}->fetchrow_array()) {
 		    next unless defined($contigdata->{$pcontigid});
 
 		    $pfound = 1;
 
 		    if ($pcontigid == $contigid) {
-			print STDERR " at $pstart to $pfinish" if $verbose;
+			print STDERR " at $pstart to $pfinish $pdirection" if $verbose;
 
 			my $inRepeat = &isInRepeat($pstart, $pfinish, $repeats->{$contigid});
 
 			print STDERR " IN REPEAT" if ($verbose && $inRepeat);
+
+			my ($newrstart, $newrfinish) = ($pdirection eq 'Forward') ?
+			    ($pstart, $pstart+$sihigh) : ($pfinish-$sihigh, $pfinish);
+
+			$inRepeat = &isInRepeat($newrstart, $newrfinish, $repeats->{$contigid});
+
+			print STDERR "\n\t\t\t\tSEARCH RANGE $newrstart $newrfinish" if $verbose;
+			print STDERR " IN REPEAT" if ($verbose && $inRepeat);
+
+			#my ($sequence,
+			#    $clipleft,
+			#    $clipright) = &getSequenceAndClipping($seqid,
+			#					  $statement->{'sequence'},
+			#					  $statement->{'qualityclip'},
+			#					  $statement->{'vectorclip'});
+
 		    } else {
 			print STDERR " in DIFFERENT contig ($pcontigid)" if $verbose;
 
@@ -186,4 +226,53 @@ sub isInRepeat {
     }
 
     return 0;
+}
+
+sub getSequenceAndClipping {
+    my ($seqid, $sth_sequence, $sth_qualityclip, $sth_vectorclip) = @_;
+
+    $sth_sequence->execute($seqid);
+
+    my ($sequence) = $sth_sequence->fetchrow_array();
+
+    $sth_sequence->finish();
+
+    return undef unless defined($sequence);
+
+    $sequence = uncompress($sequence);
+
+    $sth_qualityclip->execute($seqid);
+
+    my ($clipleft, $clipright) = $sth_qualityclip->fetchrow_array();
+
+    $sth_qualityclip->finish();
+
+    ($clipleft, $clipright) = (1, length($sequence))
+	unless (defined($clipleft) && defined($clipright));
+
+    $sth_vectorclip->execute($seqid);
+
+    while (my ($svleft, $svright) = $sth_vectorclip->fetchrow_array()) {
+	$clipleft = $svright if ($svleft == 1 && $svright > $clipleft);
+
+	$clipright = $svleft if ($svleft > 1 && $svleft < $clipright);
+    }
+
+    $sth_vectorclip->finish();
+
+    return ($sequence, $clipleft, $clipright);
+}
+
+sub getConsensus {
+    my ($contigid, $sth_consensus) = @_;
+
+    $sth_consensus->execute($contigid);
+
+    my ($sequence) = $sth_consensus->fetchrow_array();
+
+    $sth_consensus->finish();
+
+    $sequence = uncompress($sequence) if defined($sequence);
+
+    return $sequence;
 }
