b8aa2491ff1338593246df4b78ac40b9931c3811
David Harper <adh@sanger.ac.uk>
2006-07-28 10:02:34 +0000

    First import of source files for the new scaffold builder test program.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1465 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/scaffoldbuilder/GenericContigDisplayPanel.java
@@ -0,0 +1,45 @@
+package scaffoldbuilder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import uk.ac.sanger.arcturus.gui.genericdisplay.*;
+
+public class GenericContigDisplayPanel extends GenericDisplay
+    implements Transformer, PopupManager {
+    protected FeaturePainter contigPainter = new ContigFeaturePainter();
+    protected FeaturePainter bridgePainter = new BridgeFeaturePainter();
+    protected ContigInfoPanel cip;
+    protected BridgeInfoPanel bip;
+
+    public GenericContigDisplayPanel() {
+	super();
+
+	cip = new ContigInfoPanel(this);
+	bip = new BridgeInfoPanel(this);
+    }
+
+    public DrawableFeature addFeature(Feature f, int dragMode) {
+	if (f instanceof ContigFeature) {
+	    DrawableFeature df = new DrawableFeature(this, f, contigPainter, dragMode);
+	    addDrawableFeature(df, false);
+	    return df;
+	} else if (f instanceof BridgeFeature) {
+	    DrawableFeature df = new DrawableFeature(this, f, bridgePainter, dragMode);
+	    addDrawableFeature(df, false);
+	    return df;
+	} else
+	    return null;
+    }
+
+    public InfoPanel findInfoPanelForFeature(Feature f) {
+	if (f instanceof ContigFeature)
+	    return cip;
+
+	if (f instanceof BridgeFeature)
+	    return bip;
+
+	return null;
+    }
+}
