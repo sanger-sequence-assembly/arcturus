aea5e764bd2fb69cfa5496b210da050869f67780
David Harper <adh@sanger.ac.uk>
2006-10-23 12:31:39 +0000

    Added a test class to experiment with the behaviour of MySQL connections when used by multiple, parallel threads.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1593 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/TestConnection.java
@@ -0,0 +1,153 @@
+package uk.ac.sanger.arcturus.test;
+
+import javax.swing.*;
+import java.sql.*;
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.naming.Context;
+
+import uk.ac.sanger.arcturus.*;
+import uk.ac.sanger.arcturus.database.*;
+
+public class TestConnection implements ActionListener {
+	JProgressBar pbTask1 = new JProgressBar();
+	JProgressBar pbTask2 = new JProgressBar();
+	ArcturusDatabase adb;
+	String columns;
+	String tablename;
+	
+	public TestConnection(ArcturusDatabase adb, String columns, String tablename) {
+		this.adb = adb;
+		this.columns = columns;
+		this.tablename = tablename;
+	}
+	
+	public static void main(String[] args) {
+		if (args.length < 4) {
+			System.err.println("You must supply instance, organism, columns and table name");
+			System.exit(1);
+		}
+		
+		String instance = args[0];
+		String organism = args[1];
+		String columns = args[2];
+		String tablename = args[3];
+		
+		Properties props = new Properties();
+
+		Properties env = System.getProperties();
+
+		props.put(Context.INITIAL_CONTEXT_FACTORY,
+					env.get(Context.INITIAL_CONTEXT_FACTORY));
+		
+		props.put(Context.PROVIDER_URL,
+					env.get(Context.PROVIDER_URL));
+
+		try {
+			ArcturusInstance ai = new ArcturusInstance(props, instance);
+
+			System.err.println("Creating an ArcturusDatabase for " + organism);
+			System.err.println();
+
+			ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+	
+			TestConnection tc = new TestConnection(adb, columns, tablename);
+			tc.run();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void run() {
+		createUI();
+	}
+	
+	public void createUI() {
+		JFrame frame = new JFrame("TestConnection");
+		
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		
+		JPanel panel = new JPanel(new GridLayout(2, 1));
+		
+		panel.add(pbTask1);
+		panel.add(pbTask2);
+		
+		mainPanel.add(panel, BorderLayout.CENTER);
+		
+		JButton btnGo = new JButton("Go!");
+		mainPanel.add(btnGo, BorderLayout.SOUTH);
+		btnGo.addActionListener(this);
+		
+		frame.setContentPane(mainPanel);
+		
+		frame.pack();
+		
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		
+		frame.show();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		try {
+			Connection conn = adb.getUniqueConnection();
+		
+			Task task1 = new Task(conn, pbTask1, columns, tablename);
+			Thread thread1 = new Thread(task1);
+			thread1.start();
+			
+			if (Boolean.getBoolean("twoconn"))
+				conn = adb.getUniqueConnection();
+		
+			Task task2 = new Task(conn, pbTask2, columns, tablename);
+			Thread thread2 = new Thread(task2);
+			thread2.start();
+		}
+		catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+	}
+	
+	class Task implements Runnable {
+		JProgressBar pb;
+		Connection conn;
+		String columns;
+		String tablename;
+		Random rand = new Random();
+		
+		public Task(Connection conn, JProgressBar pb, String columns, String tablename) {
+			this.pb = pb;
+			this.conn = conn;
+			this.tablename = tablename;
+		}
+		
+		public void run() {
+			try {
+				String query = "select count(*) from " + tablename;
+
+				Statement stmt = conn.createStatement();
+			
+				ResultSet rs = stmt.executeQuery(query);
+				rs.next();
+				
+				pb.setMaximum(rs.getInt(1));
+			
+				query = "select " + columns + " from " + tablename;
+				
+				rs = stmt.executeQuery(query);
+				
+				int i = 0;
+				
+				while (rs.next())
+					pb.setValue(++i);
+				
+				stmt.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
