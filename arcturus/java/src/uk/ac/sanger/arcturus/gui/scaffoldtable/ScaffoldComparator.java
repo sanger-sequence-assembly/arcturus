18381e659cbc0885216b95f1886ddc0e58821175
David Harper <adh@sanger.ac.uk>
2006-11-21 09:39:53 +0000

    Added a utility class to represent a scaffold.
    Added a new package to contain classes for a tabular listing of a set of scaffolds.
    
    
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1651 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ arcturus/java/src/uk/ac/sanger/arcturus/gui/scaffoldtable/ScaffoldComparator.java
@@ -0,0 +1,69 @@
+package uk.ac.sanger.arcturus.gui.scaffoldtable;
+
+import java.util.Comparator;
+import uk.ac.sanger.arcturus.scaffold.Scaffold;
+
+public class ScaffoldComparator implements Comparator {
+	public static final int BY_LENGTH = 1;
+	public static final int BY_CONTIG_COUNT = 2;
+
+	protected boolean ascending;
+	protected int type;
+
+	public ScaffoldComparator() {
+		this(BY_LENGTH, true);
+	}
+
+	public ScaffoldComparator(int type, boolean ascending) {
+		this.type = type;
+		this.ascending = ascending;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setAscending(boolean ascending) {
+		this.ascending = ascending;
+	}
+
+	public boolean isAscending() {
+		return ascending;
+	}
+
+	public boolean equals(Object that) {
+		return (that instanceof ScaffoldComparator && (ScaffoldComparator) that == this);
+	}
+
+	public int compare(Object o1, Object o2) {
+		Scaffold scaffold1 = (Scaffold) o1;
+		Scaffold scaffold2 = (Scaffold) o2;
+		
+		switch (type) {
+			case BY_LENGTH:
+				return compareByLength(scaffold1, scaffold2);
+
+			case BY_CONTIG_COUNT:
+				return compareByContigCount(scaffold1, scaffold2);
+
+			default:
+				return compareByLength(scaffold1, scaffold2);
+		}
+	}
+
+	protected int compareByLength(Scaffold scaffold1, Scaffold scaffold2) {
+		int diff = scaffold1.getTotalLength() - scaffold2.getTotalLength();
+
+		return ascending ? diff : -diff;
+	}
+
+	protected int compareByContigCount(Scaffold scaffold1, Scaffold scaffold2) {
+		int diff = scaffold1.getContigCount() - scaffold2.getContigCount();
+
+		return ascending ? diff : -diff;
+	}
+}
