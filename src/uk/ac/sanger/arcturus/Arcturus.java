197ef2792ecf80260eb06ca376fcd4ad26e074f4
David Harper <adh@sanger.ac.uk>
2007-01-12 13:16:07 +0000

    Added code to static initialiser block in Arcturus to look for the .arcturus.props project-specific properties file and load the properties from it if it exists.
    Altered the Arcturus and ArcturusInstance classes to use the default instance and organism properties if they are defined.
    
    
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1688 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/Arcturus.java
+++ src/uk/ac/sanger/arcturus/Arcturus.java
@@ -2,12 +2,18 @@ package uk.ac.sanger.arcturus;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.File;
 import java.util.Properties;
 
 public class Arcturus {
+	protected static final String PROJECT_PROPERTIES_FILE = ".arcturus.props";
+	
 	protected static Properties arcturusProps = new Properties(System.getProperties());
 
 	static {
+		// Load the properties that are embedded in the JAR file
+		
 		InputStream is = Arcturus.class.getResourceAsStream("/resources/arcturus.props");
 		
 		if (is != null) {
@@ -20,6 +26,34 @@ public class Arcturus {
 			}
 		} else
 			System.err.println("Unable to open resource /resources/arcturus.props as stream");
+		
+		// Find the project-specific properties, if they exist, by walking up the
+		// directory tree from the application's current working directory, looking
+		// for a file named .arcturus.props
+		
+		String cwd = System.getProperty("user.dir");
+		
+		File dir = new File(cwd);
+		
+		boolean found = false;
+		
+		while (dir != null && !found) {
+			File file = new File(dir, PROJECT_PROPERTIES_FILE);
+			
+			if (file.exists() && file.canRead()) {
+				try {
+					FileInputStream fis = new FileInputStream(file);
+					arcturusProps = new Properties(arcturusProps);
+					arcturusProps.load(fis);
+					fis.close();
+					found = true;
+				}
+				catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+			} else
+				dir = dir.getParentFile();
+		}
 	}
 	
 	public static Properties getProperties() {
@@ -28,5 +62,14 @@ public class Arcturus {
 	
 	public static String getProperty(String key) {
 		return arcturusProps.getProperty(key);
+	}
+	
+	public static String getDefaultInstance() {
+		String instance = getProperty("arcturus.instance");
+		
+		if (instance != null)
+			return instance;
+		else
+			return getProperty("arcturus.default.instance");
 	}
 }
