afb5255110e66a27b97675f52b6fd3e09b070af2
David Harper <adh@sanger.ac.uk>
2007-02-01 14:45:39 +0000

    Added latest contig update date to result set, and unified the format of the result sets returned by the project summary scripts.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@1728 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/sql/procedures/project-summary-byname-like.sql
+++ arcturus/sql/procedures/project-summary-byname-like.sql
@@ -9,8 +9,10 @@ BEGIN
   select PROJECT.name, count(*) as contigs,
     sum(nreads) as nreads, sum(length) as length,
     round(avg(length)) as avglen, round(std(length)) as stdlen,
-    max(length) as maxlen, max(CURRENTCONTIGS.created) as newestcontig,
-    max(PROJECT.updated) as projectupdated
+    max(length) as maxlen,
+    max(CURRENTCONTIGS.created) as newestcontig,
+    max(CURRENTCONTIGS.updated) as lastcontigupdate,
+    PROJECT.updated as projectupdated
   from CURRENTCONTIGS left join PROJECT using(project_id)
   where length >= minContigSize and name REGEXP projectName
   group by CURRENTCONTIGS.project_id order by name asc;
--- arcturus/sql/procedures/project-summary-byname.sql
+++ arcturus/sql/procedures/project-summary-byname.sql
@@ -6,22 +6,16 @@ CREATE PROCEDURE procProjectSummaryByName(IN minContigSize INT, IN projectName V
   READS SQL DATA
   SQL SECURITY INVOKER
 BEGIN
-  DECLARE intProjectId INT;
-  DECLARE intProjectNotFound INT DEFAULT 0;
-
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET intProjectNotFound = 1;
-
-  select project_id into intProjectId from PROJECT where name=projectName;
-
-  IF (intProjectNotFound = 0) THEN
-    select projectName as name, count(*) as contigs,
-      sum(nreads) as nreads, sum(length) as length,
-      round(avg(length)) as avglen, round(std(length)) as stdlen,
-      max(length) as maxlen, max(CURRENTCONTIGS.created) as newestcontig,
-      max(PROJECT.updated) as projectupdated
-    from CURRENTCONTIGS
-    where project_id = intProjectId AND length >= minContigSize;
-  END IF;
+  select PROJECT.name, count(*) as contigs,
+    sum(nreads) as nreads, sum(length) as length,
+    round(avg(length)) as avglen, round(std(length)) as stdlen,
+    max(length) as maxlen,
+    max(CURRENTCONTIGS.created) as newestcontig,
+    max(CURRENTCONTIGS.updated) as lastcontigupdate,
+    PROJECT.updated as projectupdated
+  from CURRENTCONTIGS left join PROJECT using(project_id)
+  where length >= minContigSize and name = projectName
+  group by CURRENTCONTIGS.project_id order by name asc;
 END;$
 
 DELIMITER ;
