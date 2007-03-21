3be75f52417c16fa47b582aeff2cd24b1e7834b2
David Harper <adh@sanger.ac.uk>
2007-03-21 16:26:31 +0000

    Added the preliminary versions of a set of classes to represent and manipulate contig transfer requests.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1915 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/contigtransfer/ContigTransferRequest.java
@@ -0,0 +1,262 @@
+package uk.ac.sanger.arcturus.contigtransfer;
+
+import uk.ac.sanger.arcturus.data.*;
+import uk.ac.sanger.arcturus.people.Person;
+import java.util.Date;
+
+public class ContigTransferRequest {
+	public static final int UNKNOWN = -1;
+	public static final int PENDING = 0;
+	public static final int CANCELLED = 1;
+	public static final int DONE = 2;
+	public static final int FAILED = 3;
+	public static final int REFUSED = 4;
+	public static final int APPROVED = 5;
+
+	private int request_id;
+	private Contig contig;
+	private Project oldProject;
+	private Project newProject;
+
+	private Person requester;
+	private Date openedDate;
+	private String requesterComment;
+
+	private Person reviewer;
+	private String reviewerComment;
+	private Date reviewedDate;
+
+	private int status = PENDING;
+
+	private Date closedDate;
+	
+	private ContigTransferRequestEventListener listener;
+	private ContigTransferRequestEvent event;
+
+	public ContigTransferRequest(int request_id, Contig contig,
+			Project oldProject, Project newProject, Person requester,
+			String requesterComment) {
+		this.request_id = request_id;
+		this.contig = contig;
+		this.oldProject = oldProject;
+		this.newProject = newProject;
+		this.requester = requester;
+		this.requesterComment = requesterComment;
+	}
+
+	public ContigTransferRequest(Contig contig, Project oldProject,
+			Project newProject, Person requester, String requesterComment) {
+		this(-1, contig, oldProject, newProject, requester, requesterComment);
+	}
+
+	public ContigTransferRequest(Contig contig, Project oldProject,
+			Project newProject, Person requester) {
+		this(-1, contig, oldProject, newProject, requester, null);
+	}
+
+	public int getRequestID() {
+		return request_id;
+	}
+
+	public void setRequestID(int request_id) {
+		this.request_id = request_id;
+	}
+
+	public Contig getContig() {
+		return contig;
+	}
+
+	public Project getOldProject() {
+		return oldProject;
+	}
+
+	public Project getNewProject() {
+		return newProject;
+	}
+
+	public Person getRequester() {
+		return requester;
+	}
+	
+	public Date getOpenedDate() { return openedDate; }
+	
+	public void setOpenedDate(Date openedDate) {
+		this.openedDate = (openedDate == null) ? new Date() : openedDate;
+	}
+
+	public String getRequesterComment() {
+		return requesterComment;
+	}
+
+	public void setRequesterComment(String requesterComment) {
+		this.requesterComment = requesterComment;
+	}
+	
+	public Person getReviewer() {
+		return reviewer;
+	}
+	
+	public void setReviewer(Person reviewer) {
+		this.reviewer = reviewer;
+	}
+	
+	public Date getReviewed() {
+		return reviewedDate;
+	}
+	
+	public void setReviewedDate(Date reviewedDate) {
+		this.reviewedDate = (reviewedDate == null) ? new Date() : reviewedDate;
+	}
+	
+	public String getReviewerComment() {
+		return reviewerComment;
+	}
+	
+	public void setReviewerComment(String reviewerComment) {
+		this.reviewerComment = reviewerComment;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public String getStatusString() {
+		switch(status) {
+			case PENDING:
+				return "pending";
+				
+			case APPROVED:
+				return "approved";
+				
+			case CANCELLED:
+				return "cancelled";
+				
+			case FAILED:
+				return "failed";
+				
+			case REFUSED:
+				return "refused";
+				
+			case DONE:
+				return "done";
+				
+			default:
+				return "unknown";
+		}
+	}
+	
+	public void setStatusAsString(String str) {
+		int s = stringToStatus(str);
+		
+		if (s == UNKNOWN)
+			throw new IllegalArgumentException("Status is invalid: \"" + str + "\"");
+		
+		this.status = stringToStatus(str);
+	}
+	
+	private int stringToStatus(String str) {
+		if (str.equalsIgnoreCase("pending"))
+			return PENDING;
+		
+		if (str.equalsIgnoreCase("approved"))
+			return APPROVED;
+	
+		if (str.equalsIgnoreCase("cancelled"))
+			return CANCELLED;
+		
+		if (str.equalsIgnoreCase("failed"))
+			return FAILED;
+		
+		if (str.equalsIgnoreCase("refused"))
+			return REFUSED;
+		
+		if (str.equalsIgnoreCase("done"))
+			return DONE;
+		
+		return UNKNOWN;
+	}
+	
+	public void setStatus(int status) {
+		if (!isValidStatus(status))
+			throw new IllegalArgumentException("Status is invalid: " + status);
+		
+		this.status = status;
+	}
+	
+	public Date getClosedDate() {
+		return closedDate;
+	}
+	
+	public void setClosedDate(Date closedDate) {
+		this.closedDate = (closedDate == null) ? new Date() : closedDate;
+	}
+	
+	public boolean isClosed() {
+		return isClosedStatus(status);
+	}
+	
+	private boolean isClosedStatus(int s) {
+		return s == DONE || s == FAILED || s == REFUSED || s == CANCELLED;
+	}
+	
+	private boolean isValidStatus(int s) {
+		return s == PENDING || s == APPROVED || s == DONE ||
+			s == FAILED || s == REFUSED || s == CANCELLED;
+	}
+	
+	public void review(Person reviewer, Date reviewedDate, int status, String reviewerComment) {
+		if (!isValidStatus(status))
+			throw new IllegalArgumentException("Status is invalid: " + status);
+		
+		setReviewer(reviewer);
+		setReviewedDate(reviewedDate);
+		setReviewerComment(reviewerComment);
+		
+		if (isClosedStatus(status))
+			setClosedDate(reviewedDate);
+		
+		int oldStatus = status;
+		
+		setStatus(status);
+		
+		if (listener != null) {
+			event.setOldStatus(oldStatus);
+			listener.stateChanged(event);
+		}
+	}
+	
+	public void pending(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, PENDING, reviewerComment);
+	}
+
+	public void cancel(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, CANCELLED, reviewerComment);
+	}
+	
+	public void refuse(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, REFUSED, reviewerComment);
+	}
+	
+	public void fail(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, FAILED, reviewerComment);
+	}
+	
+	public void approve(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, APPROVED, reviewerComment);
+	}
+	
+	public void done(Person reviewer, Date reviewedDate, String reviewerComment) {
+		review(reviewer, reviewedDate, DONE, reviewerComment);
+	}
+	
+	public void setContigTransferRequestEventListener(ContigTransferRequestEventListener listener) {
+		this.listener = listener;
+		
+		if (event == null)
+			event = new ContigTransferRequestEvent(this, UNKNOWN);
+	}
+	
+	public ContigTransferRequestEventListener getContigTransferRequestEventListener() {
+		return listener;
+	}
+}
