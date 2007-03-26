fc35d6b8a0cead2e0817050ae5bf75055696e603
David Harper <adh@sanger.ac.uk>
2007-03-26 08:59:07 +0000

    Added class to test automatic updates.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1918 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/TestArcturusUpdater.java
@@ -0,0 +1,76 @@
+package uk.ac.sanger.arcturus.test;
+
+import uk.ac.sanger.arcturus.Arcturus;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.zip.*;
+
+public class TestArcturusUpdater {
+	private static final String JAR_FILE_NAME = "arcturus.jar";
+
+	public static void main(String[] args) {
+		TestArcturusUpdater tau = new TestArcturusUpdater();
+		tau.run();
+	}
+
+	public void run() {
+		String classpath = System.getProperty("java.class.path");
+		String pathsep = System.getProperty("path.separator");
+
+		String[] pathitems = classpath.split(pathsep);
+
+		Date jardate = null;
+
+		for (int i = 0; i < pathitems.length; i++) {
+			System.out.println(pathitems[i]);
+
+			if (pathitems[i].endsWith(JAR_FILE_NAME)) {
+				File jarfile = new File(pathitems[i]);
+
+				if (jarfile.exists() && jarfile.isFile() && jardate == null)
+					jardate = new Date(jarfile.lastModified());
+			}
+		}
+
+		if (jardate == null)
+			System.out.println("Unable to determine date of JAR file");
+		else
+			System.out.println("JAR file last modified at " + jardate);
+
+		String locator = Arcturus.getProperty("arcturus.update.url");
+
+		Date zipjardate = findZipJARDate(locator);
+		
+		if (zipjardate == null)
+			System.out.println("Unable to determine date of zip file JAR");
+		else
+			System.out.println("Zip JAR file last modified at " + zipjardate);
+	}
+
+	private Date findZipJARDate(String locator) {
+		try {
+			URL url = new URL(locator);
+			URLConnection conn = url.openConnection();
+			InputStream is = conn.getInputStream();
+			ZipInputStream zis = new ZipInputStream(is);
+
+			ZipEntry entry;
+
+			while ((entry = zis.getNextEntry()) != null) {
+				if (entry.getName().endsWith(JAR_FILE_NAME)) {
+					zis.close();
+					return new Date(entry.getTime());
+				}
+			}
+			
+			zis.close();
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		return null;
+	}
+}
