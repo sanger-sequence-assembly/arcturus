44e016323d322f998f8d1d9719148b25edd5c07f
David Harper <adh@sanger.ac.uk>
2007-03-26 09:11:36 +0000

    Added code to extract and display full JAR file path.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1921 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/test/TestArcturusUpdater.java
+++ src/uk/ac/sanger/arcturus/test/TestArcturusUpdater.java
@@ -22,6 +22,7 @@ public class TestArcturusUpdater {
 		String[] pathitems = classpath.split(pathsep);
 
 		Date jardate = null;
+		String jarname = null;
 
 		for (int i = 0; i < pathitems.length; i++) {
 			System.out.println(pathitems[i]);
@@ -29,15 +30,21 @@ public class TestArcturusUpdater {
 			if (pathitems[i].endsWith(JAR_FILE_NAME)) {
 				File jarfile = new File(pathitems[i]);
 
-				if (jarfile.exists() && jarfile.isFile() && jardate == null)
+				if (jarfile.exists() && jarfile.isFile() && jardate == null) {
+					try {
+						jarname = jarfile.getCanonicalPath();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					jardate = new Date(jarfile.lastModified());
+				}
 			}
 		}
 
 		if (jardate == null)
 			System.out.println("Unable to determine date of JAR file");
 		else
-			System.out.println("JAR file last modified at " + jardate);
+			System.out.println("JAR file " + jarname + " last modified at " + jardate);
 
 		String locator = Arcturus.getProperty("arcturus.update.url");
 
