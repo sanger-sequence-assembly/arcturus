d6ddb6f5b5e387ba0e452302b18ba2b240028766
David Harper <adh@sanger.ac.uk>
2007-03-28 13:42:40 +0000

    Extended read finder to return length, read count and updated date of the contig. Changed return code of findRead to indicate whether the read exists or not or whether it is in a contig.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1942 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/utils/ReadFinder.java
+++ src/uk/ac/sanger/arcturus/utils/ReadFinder.java
@@ -3,11 +3,20 @@ package uk.ac.sanger.arcturus.utils;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
 
 import java.sql.*;
+import java.text.*;
 
-public class ReadFinder {	
+public class ReadFinder {
+	public static final int READ_DOES_NOT_EXIST = 1;
+	public static final int READ_IS_FREE = 2;
+	public static final int READ_IS_IN_CONTIG = 3;
+	
 	private Connection conn;
 	private PreparedStatement pstmtReadToContig;
-
+	private PreparedStatement pstmtReadInfo;
+	
+	private final DateFormat formatter = new SimpleDateFormat(
+	"yyyy MMM dd HH:mm");
+	
 	public ReadFinder(ArcturusDatabase adb) throws SQLException {
 		conn = adb.getPooledConnection();
 
@@ -15,8 +24,12 @@ public class ReadFinder {
 	}
 
 	private void prepareStatements() throws SQLException {
+		pstmtReadInfo = conn.prepareStatement("select read_id from READINFO where readname = ?");
+		
 		pstmtReadToContig = conn
-				.prepareStatement("select CURRENTCONTIGS.contig_id,gap4name,PROJECT.name,"
+				.prepareStatement("select CURRENTCONTIGS.contig_id,gap4name,nreads,length,"
+						+ "CURRENTCONTIGS.updated,"
+						+ "PROJECT.name,"
 						+ "cstart,cfinish,direction"
 						+ " from READINFO,SEQ2READ,MAPPING,CURRENTCONTIGS,PROJECT"
 						+ " where READINFO.readname = ?"
@@ -42,31 +55,52 @@ public class ReadFinder {
 
 	private int contigid;
 	private String gap4name;
+	private int nreads;
+	private int ctglen;
+	private String updated;
 	private String projectName;
 	private int cstart;
 	private int cfinish;
 	private String direction;
 	private boolean isDataValid = false;
 	
-	public boolean findRead(String readname) throws SQLException {
+	public int findRead(String readname) throws SQLException {
+		int status = READ_DOES_NOT_EXIST;
+		
+		pstmtReadInfo.setString(1, readname);
+		
+		ResultSet rs = pstmtReadInfo.executeQuery();
+		
+		if (rs.next())
+			status = READ_IS_FREE;
+		
+		rs.close();
+		
+		if (status == READ_DOES_NOT_EXIST)
+			return status;
+		
 		pstmtReadToContig.setString(1, readname);
 		
-		ResultSet rs = pstmtReadToContig.executeQuery();
+		rs = pstmtReadToContig.executeQuery();
 		
 		if (rs.next()) {
+			status = READ_IS_IN_CONTIG;
 			contigid = rs.getInt(1);
 			gap4name = rs.getString(2);
-			projectName = rs.getString(3);
-			cstart = rs.getInt(4);
-			cfinish = rs.getInt(5);
-			direction = rs.getString(6);
+			nreads = rs.getInt(3);
+			ctglen = rs.getInt(4);
+			updated = formatter.format(rs.getTimestamp(5));
+			projectName = rs.getString(6);
+			cstart = rs.getInt(7);
+			cfinish = rs.getInt(8);
+			direction = rs.getString(9);
 			isDataValid = true;
 		} else
 			isDataValid = false;
 		
 		rs.close();
 		
-		return isDataValid;
+		return status;
 	}
 	
 	public int getContigID() {
@@ -75,6 +109,18 @@ public class ReadFinder {
 	
 	public String getGap4Name() {
 		return isDataValid ? gap4name : null;
+	}
+	
+	public int getReadCount() {
+		return isDataValid ? nreads : -1;
+	}
+	
+	public int getContigLength() {
+		return isDataValid ? ctglen : -1;
+	}
+	
+	public String getContigUpdated() {
+		return isDataValid ? updated : null;
 	}
 	
 	public String getProjectName() {
