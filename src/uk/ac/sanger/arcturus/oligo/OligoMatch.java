95d6591aa1223bc271dbaac2e5366c9bc0cb65e0
David Harper <adh@sanger.ac.uk>
2007-04-02 10:53:31 +0000

    Altered classes so that they can deal with oligo searches on reads as well as contigs.  Added new event types to distinguish read processing from contig processing.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@1954 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/oligo/OligoMatch.java
+++ src/uk/ac/sanger/arcturus/oligo/OligoMatch.java
@@ -1,23 +1,29 @@
 package uk.ac.sanger.arcturus.oligo;
 
-import uk.ac.sanger.arcturus.data.Contig;
+import uk.ac.sanger.arcturus.data.*;
 
 public class OligoMatch {
 	private Oligo oligo;
-	private Contig contig;
+	private DNASequence sequence;
 	private int offset;
 	private boolean forward;
 	
-	public OligoMatch(Oligo oligo, Contig contig, int offset, boolean forward) {
+	public OligoMatch(Oligo oligo, DNASequence sequence, int offset, boolean forward) {
 		this.oligo = oligo;
-		this.contig = contig;
+		this.sequence = sequence;
 		this.offset = offset;
 		this.forward = forward;
 	}
 	
 	public Oligo getOligo() { return oligo; }
 	
-	public Contig getContig() { return contig; }
+	public Contig getContig() {
+		return (sequence != null && sequence instanceof Contig) ? (Contig)sequence : null;
+	}
+	
+	public Read getRead() {
+		return (sequence != null && sequence instanceof Read) ? (Read)sequence : null;
+	}
 	
 	public int getOffset() { return offset; }
 	
