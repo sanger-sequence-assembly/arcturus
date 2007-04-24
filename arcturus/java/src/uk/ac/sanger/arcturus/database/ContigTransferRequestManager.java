a2bf79a86b25fcf4f541758cf6a88bdb8e4d0752
David Harper <adh@sanger.ac.uk>
2007-04-24 14:00:46 +0000

    Added methods to create and manage contig transfer requests.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2044 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/database/ContigTransferRequestManager.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/database/ContigTransferRequestManager.java
@@ -11,11 +11,16 @@ import java.util.*;
 import java.util.zip.DataFormatException;
 
 public class ContigTransferRequestManager {
-	private ArcturusDatabase adb;
-	private Connection conn;
+	protected ArcturusDatabase adb;
+	protected Connection conn;
 
 	protected PreparedStatement pstmtByRequester = null;
 	protected PreparedStatement pstmtByContigOwner = null;
+	protected PreparedStatement pstmtByID = null;
+
+	protected PreparedStatement pstmtCountActiveRequestsByContigId = null;
+
+	protected PreparedStatement pstmtInsertNewRequest = null;
 
 	/**
 	 * Creates a new ContigTransferRequestManager to provide contig transfer
@@ -31,7 +36,7 @@ public class ContigTransferRequestManager {
 		prepareStatements();
 	}
 
-	private void prepareStatements() throws SQLException {
+	protected void prepareStatements() throws SQLException {
 		String columns = "request_id,contig_id,old_project_id,new_project_id,requester,"
 				+ "requester_comment,opened,reviewer,reviewer_comment,reviewed,CONTIGTRANSFERREQUEST.status,closed";
 
@@ -50,6 +55,23 @@ public class ContigTransferRequestManager {
 				+ " where owner = ? and opened > date_sub(now(), interval 14 day)";
 
 		pstmtByContigOwner = conn.prepareStatement(query);
+
+		query = "select " + columns
+				+ " from CONTIGTRANSFERREQUEST left join PROJECT"
+				+ " where request_id = ?";
+
+		pstmtByID = conn.prepareStatement(query);
+
+		query = "select count(*) from CONTIGTRANSFERREQUEST"
+				+ " where contig_id = ? and (status = 'pending' or status = 'approved')";
+
+		pstmtCountActiveRequestsByContigId = conn.prepareStatement(query);
+
+		query = "insert into CONTIGTRANSFERREQUEST(contig_id,old_project_id,new_project_id,requester,opened"
+				+ " values(?,?,?,?,NOW())";
+
+		pstmtInsertNewRequest = conn.prepareStatement(query,
+				Statement.RETURN_GENERATED_KEYS);
 	}
 
 	public ContigTransferRequest[] getContigTransferRequestsByUser(Person user,
@@ -64,7 +86,7 @@ public class ContigTransferRequestManager {
 			pstmt = pstmtByContigOwner;
 			pstmt.setString(1, user.getUID());
 		}
-		
+
 		ResultSet rs = pstmt.executeQuery();
 
 		ContigTransferRequest[] transfers = getTransfersFromResultSet(rs);
@@ -74,7 +96,7 @@ public class ContigTransferRequestManager {
 		return transfers;
 	}
 
-	private ContigTransferRequest[] getTransfersFromResultSet(ResultSet rs)
+	protected ContigTransferRequest[] getTransfersFromResultSet(ResultSet rs)
 			throws SQLException {
 		Vector v = new Vector();
 
@@ -91,10 +113,10 @@ public class ContigTransferRequestManager {
 				Arcturus.logWarning("Failed to get contig " + contigId, dfe);
 				contig = null;
 			}
-			
+
 			if (contig == null) {
-				System.err.println("Contig transfer request " + requestId +
-						" refers to a non-existent contig " + contigId);
+				Arcturus.logWarning("Contig transfer request " + requestId
+						+ " refers to a non-existent contig " + contigId);
 				continue;
 			}
 
@@ -135,4 +157,216 @@ public class ContigTransferRequestManager {
 		return (ContigTransferRequest[]) (v
 				.toArray(new ContigTransferRequest[0]));
 	}
+
+	public ContigTransferRequest findContigTransferRequest(int requestId)
+			throws ContigTransferRequestException, SQLException {
+		pstmtByID.setInt(1, requestId);
+
+		ResultSet rs = pstmtByID.executeQuery();
+
+		ContigTransferRequest[] transfers = getTransfersFromResultSet(rs);
+
+		rs.close();
+
+		return (transfers == null || transfers.length == 0) ? null
+				: transfers[0];
+	}
+
+	public ContigTransferRequest createContigTransferRequest(Person requester,
+			int contigId, int toProjectId)
+			throws ContigTransferRequestException, SQLException {
+		checkUser(requester);
+		checkForExistingRequests(contigId);
+		checkIsCurrentContig(contigId);
+
+		Contig contig = null;
+
+		try {
+			contig = adb.getContigByID(contigId,
+					ArcturusDatabase.CONTIG_BASIC_DATA);
+		} catch (DataFormatException e) {
+			// This will never happen in the CONTIG_BASIC_DATA mode.
+		}
+
+		if (contig == null)
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.NO_SUCH_CONTIG);
+
+		Project fromProject = contig.getProject();
+
+		if (fromProject == null)
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.NO_SUCH_PROJECT,
+					"The contig does not belong to a valid project");
+
+		Project toProject = adb.getProjectByID(toProjectId);
+
+		if (toProject == null)
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.NO_SUCH_PROJECT,
+					"No such project with ID=" + toProjectId);
+
+		checkCanUserTransferBetweenProjects(requester, fromProject, toProject);
+
+		return realCreateContigTransferRequest(requester, contig, toProject);
+	}
+
+	protected ContigTransferRequest realCreateContigTransferRequest(
+			Person requester, Contig contig, Project toProject)
+			throws ContigTransferRequestException, SQLException {
+
+		pstmtInsertNewRequest.setInt(1, contig.getID());
+		pstmtInsertNewRequest.setInt(2, contig.getProject().getID());
+		pstmtInsertNewRequest.setInt(3, toProject.getID());
+		pstmtInsertNewRequest.setString(4, requester.getUID());
+
+		int rc = pstmtInsertNewRequest.executeUpdate();
+
+		if (rc == 1) {
+			ResultSet rs = pstmtInsertNewRequest.getGeneratedKeys();
+
+			int requestId = rs.next() ? rs.getInt(1) : -1;
+
+			rs.close();
+
+			return findContigTransferRequest(requestId);
+		} else
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.SQL_INSERT_FAILED);
+	}
+
+	protected void checkUser(Person user) throws ContigTransferRequestException {
+		if (user == null)
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.USER_IS_NULL);
+	}
+
+	protected void checkForExistingRequests(int contigId)
+			throws ContigTransferRequestException, SQLException {
+		int requestId = countActiveRequestsForContig(contigId);
+
+		if (requestId > 0)
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.CONTIG_ALREADY_REQUESTED);
+	}
+
+	protected int countActiveRequestsForContig(int contigId)
+			throws SQLException {
+		pstmtCountActiveRequestsByContigId.setInt(1, contigId);
+
+		ResultSet rs = pstmtCountActiveRequestsByContigId.executeQuery();
+
+		int count = rs.next() ? rs.getInt(1) : 0;
+
+		rs.close();
+
+		return count;
+	}
+
+	protected void checkIsCurrentContig(int contigId)
+			throws ContigTransferRequestException, SQLException {
+		if (!adb.isCurrentContig(contigId))
+			throw new ContigTransferRequestException(
+					ContigTransferRequestException.CONTIG_NOT_CURRENT);
+	}
+
+	protected void checkCanUserTransferBetweenProjects(Person requester,
+			Project fromProject, Project toProject)
+			throws ContigTransferRequestException, SQLException {
+		if (fromProject.getOwner().equals(requester) && toProject.isBin())
+			return;
+
+		if (toProject.getOwner().equals(requester))
+			return;
+
+		if (adb.hasPrivilege(requester, "move_any_contig"))
+			return;
+
+		if (isSuperUser(requester))
+			return;
+
+		throw new ContigTransferRequestException(
+				ContigTransferRequestException.USER_NOT_AUTHORISED);
+	}
+
+	protected boolean isSuperUser(Person person) throws SQLException {
+		String role = adb.getRoleForUser(person);
+
+		return role.equalsIgnoreCase("team leader")
+				|| role.equalsIgnoreCase("administrator")
+				|| role.equalsIgnoreCase("superuser");
+	}
+
+	public ContigTransferRequest createContigTransferRequest(int contigId,
+			int toProjectId) throws ContigTransferRequestException,
+			SQLException {
+		return createContigTransferRequest(PeopleManager.findMe(), contigId,
+				toProjectId);
+	}
+
+	public void reviewContigTransferRequest(ContigTransferRequest request,
+			Person reviewer, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+		checkUser(reviewer);
+		checkIsCurrentContig(request.getContig().getID());
+
+		checkCanUserAlterRequestStatus(request, reviewer, newStatus);
+
+		changeContigRequestStatus(request, reviewer, newStatus);
+	}
+
+	protected void checkCanUserAlterRequestStatus(
+			ContigTransferRequest request, Person reviewer, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+		if (newStatus == ContigTransferRequest.CANCELLED
+				&& reviewer.equals(request.getRequester()))
+			return;
+
+		if ((newStatus == ContigTransferRequest.REFUSED || newStatus == ContigTransferRequest.APPROVED)
+				&& reviewer.equals(request.getContigOwner()))
+			return;
+
+		if (isSuperUser(reviewer))
+			return;
+
+		throw new ContigTransferRequestException(
+				ContigTransferRequestException.USER_NOT_AUTHORISED);
+	}
+
+	protected void changeContigRequestStatus(ContigTransferRequest request,
+			Person reviewer, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+
+	}
+
+	public void reviewContigTransferRequest(int requestId, Person reviewer,
+			int newStatus) throws ContigTransferRequestException, SQLException {
+		ContigTransferRequest request = findContigTransferRequest(requestId);
+		reviewContigTransferRequest(request, reviewer, newStatus);
+	}
+
+	public void reviewContigTransferRequest(int requestId, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+		reviewContigTransferRequest(requestId, PeopleManager.findMe(),
+				newStatus);
+	}
+
+	public void executeContigTransferRequest(ContigTransferRequest request,
+			Person reviewer, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+		// XXX to be implemented
+	}
+
+	public void executeContigTransferRequest(int requestId, Person reviewer,
+			int newStatus) throws ContigTransferRequestException, SQLException {
+		ContigTransferRequest request = findContigTransferRequest(requestId);
+		executeContigTransferRequest(request, reviewer, newStatus);
+	}
+
+	public void executeContigTransferRequest(int requestId, int newStatus)
+			throws ContigTransferRequestException, SQLException {
+		executeContigTransferRequest(requestId, PeopleManager.findMe(),
+				newStatus);
+	}
+
 }
