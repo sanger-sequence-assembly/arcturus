38fab17b16868f94d20e1d91fb44cb26fcb047f0
David Harper <adh@sanger.ac.uk>
2007-06-06 15:30:00 +0000

    Tweaked the output slightly.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2165 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/test/CrossMatchFilter.java
+++ src/uk/ac/sanger/arcturus/test/CrossMatchFilter.java
@@ -19,7 +19,8 @@ public class CrossMatchFilter {
 
 		if (args.length == 0) {
 			JFileChooser chooser = new JFileChooser();
-			FileFilter filter = new FileNameExtensionFilter("CrossMatch file", "xma");
+			FileFilter filter = new FileNameExtensionFilter("CrossMatch files",
+					"xma");
 			chooser.addChoosableFileFilter(filter);
 
 			int rc = chooser.showOpenDialog(null);
@@ -33,16 +34,16 @@ public class CrossMatchFilter {
 			System.err.println("No file to read!");
 			System.exit(1);
 		}
-		
+
 		BufferedReader br = null;
-		
+
 		try {
 			br = new BufferedReader(new FileReader(file));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		Pattern pattern = Pattern
 				.compile("^\\s*\\d+\\s+\\d+\\.\\d+\\s+\\d+\\.\\d+\\s+\\d+\\.\\d+\\s+.*");
 
@@ -54,60 +55,68 @@ public class CrossMatchFilter {
 
 				if (matcher.matches()) {
 					String[] words = line.trim().split("\\s+");
-					
+
 					int i = 0;
-					
+
 					int score = Integer.parseInt(words[i++]);
-					
+
 					double subs = Double.parseDouble(words[i++]);
 					double dels = Double.parseDouble(words[i++]);
 					double inss = Double.parseDouble(words[i++]);
-					
+
 					String name1 = words[i++];
-					
+
 					int start1 = Integer.parseInt(words[i++]);
 					int finish1 = Integer.parseInt(words[i++]);
-					
+
 					String word = words[i++];
-					int tail1 = Integer.parseInt(word.substring(1, word.length()-1));
-					
+					int tail1 = Integer.parseInt(word.substring(1, word
+							.length() - 1));
+
 					String name2;
 					int start2;
 					int finish2;
 					int tail2;
 					boolean compl;
-					
+
 					if (words[i].equalsIgnoreCase("C")) {
 						compl = true;
 						i++;
-						
+
 						name2 = words[i++];
-						
+
 						word = words[i++];
-						tail2 = Integer.parseInt(word.substring(1, word.length()-1));
-						
+						tail2 = Integer.parseInt(word.substring(1, word
+								.length() - 1));
+
 						start2 = Integer.parseInt(words[i++]);
 						finish2 = Integer.parseInt(words[i++]);
 					} else {
 						compl = false;
-						
+
 						name2 = words[i++];
-						
+
 						start2 = Integer.parseInt(words[i++]);
 						finish2 = Integer.parseInt(words[i++]);
 
 						word = words[i++];
-						tail2 = Integer.parseInt(word.substring(1, word.length()-1));
+						tail2 = Integer.parseInt(word.substring(1, word
+								.length() - 1));
 					}
-					
-					System.out.println("Score: " + score);
-					System.out.println("Subs: " + subs + ", dels: " + dels + ", inss: " + inss);
-					System.out.println("Sequence #1: " + name1);
-					System.out.println("Alignment: " + start1 + " " + finish1 + " " + tail1);
-					System.out.println("Sequence #2: " + name2 + (compl ? " [REVERSED]":""));
-					System.out.println("Alignment: " + start2 + " " + finish2 + " " + tail2);
-					
-					System.out.println();
+
+					if (score >= 100) {
+						System.out.println("Score: " + score);
+						System.out.println("Subs: " + subs + ", dels: " + dels
+								+ ", inss: " + inss);
+						System.out.println("Sequence #1: " + name1);
+						System.out.println("Alignment: " + start1 + " "
+								+ finish1 + " " + tail1);
+						System.out.println("Sequence #2: " + name2
+								+ (compl ? " [REVERSED]" : ""));
+						System.out.println("Alignment: " + start2 + " "
+								+ finish2 + " " + tail2);
+						System.out.println();
+					}
 				}
 			}
 		} catch (IOException e) {
