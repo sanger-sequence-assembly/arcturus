d445ba308f6b2d6804a91f30bf1810139044f4f0
David Harper <adh@sanger.ac.uk>
2007-06-20 10:20:48 +0000

    Updated code to allow a complete project to be exported.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2232 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/utils/ContigCAFWriter.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/utils/ContigCAFWriter.java
@@ -5,6 +5,8 @@ import uk.ac.sanger.arcturus.database.ArcturusDatabase;
 
 import java.sql.*;
 import java.util.zip.*;
+import java.util.List;
+import java.util.Vector;
 import java.io.*;
 import java.text.*;
 
@@ -15,7 +17,7 @@ public class ContigCAFWriter {
 	public static final int READ_BASIC_DATA_NOT_FOUND = 3;
 	public static final int READ_CLONE_DATA_NOT_FOUND = 4;
 	public static final int NO_SEQUENCE_DATA = 5;
-	
+
 	private Connection conn;
 
 	private PreparedStatement pstmtContigData;
@@ -29,9 +31,11 @@ public class ContigCAFWriter {
 	private PreparedStatement pstmtCloningVector;
 	private PreparedStatement pstmtQualityClipping;
 	private PreparedStatement pstmtReadTag;
-	
+
+	private PreparedStatement pstmtContigsForProject;
+
 	private DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
-	
+
 	private DecimalFormat decimalformat = new DecimalFormat("00000000");
 
 	private Inflater decompresser = new Inflater();
@@ -96,125 +100,134 @@ public class ContigCAFWriter {
 		sql = "select tagtype,pstart,pfinal,comment from READTAG where seq_id = ? and deprecated is null";
 
 		pstmtReadTag = conn.prepareStatement(sql);
+
+		sql = "select contig_id from"
+				+ " CURRENTCONTIGS left join PROJECT using(project_id)"
+				+ " where nreads > 1 and name=?";
+		
+		pstmtContigsForProject = conn.prepareStatement(sql);
 	}
 
 	public int writeContigAsCAF(int contigid, PrintWriter pw)
-		throws SQLException, DataFormatException {
+			throws SQLException, DataFormatException {
 		pstmtContigData.setInt(1, contigid);
-		
+
 		ResultSet rs = pstmtContigData.executeQuery();
-		
+
 		if (!rs.next()) {
 			rs.close();
 			return CONTIG_NOT_FOUND;
 		}
-		
+
 		String gap4name = rs.getString(1);
 		int ctglen = rs.getInt(2);
 		int nreads = rs.getInt(3);
-		
+
 		String contigname = "Contig" + decimalformat.format(contigid);
-		
+
 		pw.println("Sequence : " + contigname);
 		pw.println("Is_contig");
 		pw.println("Unpadded");
-		
+
 		int[] readids = new int[nreads];
-		int[] seqids = new int[nreads]; 
-		
+		int[] seqids = new int[nreads];
+
 		pstmtMapping.setInt(1, contigid);
-		
+
 		rs = pstmtMapping.executeQuery();
-		
+
 		int i = 0;
-		
+
 		while (rs.next()) {
 			int mappingid = rs.getInt(1);
 			String direction = rs.getString(2);
 			int seqid = rs.getInt(3);
 			int readid = rs.getInt(4);
 			String readname = rs.getString(5);
-			
+
 			readids[i] = readid;
 			seqids[i] = seqid;
 			i++;
-			
+
 			boolean forward = direction.equalsIgnoreCase("Forward");
-			
+
 			pstmtSegment.setInt(1, mappingid);
-			
+
 			ResultSet rs2 = pstmtSegment.executeQuery();
-			
+
 			while (rs2.next()) {
 				int cstart = rs2.getInt(1);
 				int rstart = rs2.getInt(2);
 				int seglen = rs2.getInt(3);
-				
-				int rfinish = forward ? rstart + seglen - 1 : rstart - seglen + 1;
+
+				int rfinish = forward ? rstart + seglen - 1 : rstart - seglen
+						+ 1;
 				int cfinish = cstart + seglen - 1;
-				
+
 				if (forward)
-					pw.println("Assembled_from " + readname + " " + cstart + " " + cfinish + " " + rstart + " " + rfinish);
+					pw.println("Assembled_from " + readname + " " + cstart
+							+ " " + cfinish + " " + rstart + " " + rfinish);
 				else
-					pw.println("Assembled_from " + readname + " " + cfinish + " " + cstart + " " + rfinish + " " + rstart);
+					pw.println("Assembled_from " + readname + " " + cfinish
+							+ " " + cstart + " " + rfinish + " " + rstart);
 			}
-			
+
 			rs2.close();
 		}
-		
+
 		rs.close();
-			
+
 		pw.println();
-		
+
 		pstmtConsensus.setInt(1, contigid);
-		
+
 		rs = pstmtConsensus.executeQuery();
-		
+
 		if (!rs.next()) {
 			rs.close();
 			return NO_CONSENSUS;
 		}
-		
+
 		byte[] dna = rs.getBytes(1);
 		byte[] quality = rs.getBytes(2);
 		int seqlen = rs.getInt(3);
-		
+
 		dna = decodeCompressedData(dna, seqlen);
 		quality = decodeCompressedData(quality, seqlen);
-		
+
 		pw.println("DNA : " + contigname);
-		
+
 		writeDNA(dna, pw);
-		
+
 		pw.println();
 
 		pw.println("BaseQuality : " + contigname);
-		
+
 		writeQuality(quality, pw);
-		
+
 		pw.println();
-	
+
 		for (i = 0; i < readids.length; i++) {
 			int rc = writeRead(readids[i], seqids[i], pw);
-			
+
 			if (rc != OK)
 				return rc;
 		}
 
 		return OK;
 	}
-	
+
 	private int writeRead(int readid, int seqid, PrintWriter pw)
-		throws SQLException, DataFormatException {
+			throws SQLException, DataFormatException {
 		pstmtReadBasicData.setInt(1, readid);
-		
+
 		ResultSet rs = pstmtReadBasicData.executeQuery();
-		
+
 		if (!rs.next()) {
 			rs.close();
 			return READ_BASIC_DATA_NOT_FOUND;
 		}
-		
+
 		String readname = rs.getString(1);
 		java.util.Date asped = rs.getDate(2);
 		String strand = rs.getString(3);
@@ -222,32 +235,32 @@ public class ContigCAFWriter {
 		String chemistry = rs.getString(5);
 		String basecaller = rs.getString(6);
 		String status = rs.getString(7);
-		
+
 		rs.close();
 
 		pstmtReadCloneData.setInt(1, readid);
-		
+
 		rs = pstmtReadCloneData.executeQuery();
-		
+
 		if (!rs.next()) {
 			rs.close();
 			return READ_CLONE_DATA_NOT_FOUND;
 		}
-		
+
 		String template = rs.getString(1);
 		String ligation = rs.getString(2);
 		int silow = rs.getInt(3);
 		int sihigh = rs.getInt(4);
 		String clone = rs.getString(5);
-		
+
 		rs.close();
-		
+
 		pw.println("Sequence : " + readname);
 		pw.println("Is_read");
 		pw.println("Unpadded");
-		
+
 		pw.println("SCF_File " + readname + "SCF");
-		
+
 		pw.println("Template " + template);
 		pw.println("Insert_size " + silow + " " + sihigh);
 		pw.println("Ligation_no " + ligation);
@@ -256,102 +269,104 @@ public class ContigCAFWriter {
 		pw.println("Dye " + chemistry);
 		pw.println("Clone " + clone);
 		pw.println("Status " + status);
-		pw.println("Asped " +dateformat.format(asped));
+		pw.println("Asped " + dateformat.format(asped));
 		pw.println("Base_caller " + basecaller);
-		
+
 		pstmtQualityClipping.setInt(1, seqid);
-		
+
 		rs = pstmtQualityClipping.executeQuery();
-		
+
 		while (rs.next()) {
 			int qleft = rs.getInt(1);
 			int qright = rs.getInt(2);
 			pw.println("Clipping QUAL " + qleft + " " + qright);
 		}
-		
+
 		rs.close();
-		
+
 		pstmtSequenceVector.setInt(1, seqid);
-		
+
 		rs = pstmtSequenceVector.executeQuery();
-		
+
 		while (rs.next()) {
 			int svleft = rs.getInt(1);
 			int svright = rs.getInt(2);
 			String svname = rs.getString(3);
-			pw.println("Seq_vec SVEC " + svleft + " " + svright + " \"" + svname + "\"");
+			pw.println("Seq_vec SVEC " + svleft + " " + svright + " \""
+					+ svname + "\"");
 		}
-		
+
 		rs.close();
-		
+
 		pstmtCloningVector.setInt(1, seqid);
-		
+
 		rs = pstmtCloningVector.executeQuery();
-		
+
 		while (rs.next()) {
 			int cvleft = rs.getInt(1);
 			int cvright = rs.getInt(2);
 			String cvname = rs.getString(3);
-			pw.println("Clone_vec CVEC " + cvleft + " " + cvright + " \"" + cvname + "\"");
+			pw.println("Clone_vec CVEC " + cvleft + " " + cvright + " \""
+					+ cvname + "\"");
 		}
-		
+
 		rs.close();
-		
+
 		pstmtReadTag.setInt(1, seqid);
-		
+
 		rs = pstmtReadTag.executeQuery();
-		
+
 		while (rs.next()) {
 			String tagtype = rs.getString(1);
 			int tagstart = rs.getInt(2);
 			int tagfinish = rs.getInt(3);
 			String tagcomment = rs.getString(4);
-			
+
 			pw.print("TAG " + tagtype + " " + tagstart + " " + tagfinish);
 			if (tagcomment != null)
 				pw.print(" \"" + tagcomment + "\"");
 			pw.println();
 		}
-		
+
 		rs.close();
-		
+
 		pstmtSequence.setInt(1, seqid);
-		
+
 		rs = pstmtSequence.executeQuery();
-		
+
 		if (!rs.next()) {
 			rs.close();
 			return NO_SEQUENCE_DATA;
 		}
-		
-		
+
 		byte[] dna = rs.getBytes(1);
 		byte[] quality = rs.getBytes(2);
 		int seqlen = rs.getInt(3);
-		
+
 		pw.println("Align_to_SCF 1 " + seqlen + " 1 " + seqlen);
-		
+
 		pw.println();
-		
+
 		dna = decodeCompressedData(dna, seqlen);
 		quality = decodeCompressedData(quality, seqlen);
-		
+
 		pw.println("DNA : " + readname);
-		
+
 		writeDNA(dna, pw);
-		
+
 		pw.println();
 
 		pw.println("BaseQuality : " + readname);
-		
+
 		writeQuality(quality, pw);
 
 		pw.println();
 
 		return OK;
 	}
-	
-	private byte[] decodeCompressedData(byte[] compressed, int length) throws DataFormatException {
+
+	private byte[] decodeCompressedData(byte[] compressed, int length)
+			throws DataFormatException {
 		byte[] buffer = new byte[length];
 
 		decompresser.setInput(compressed, 0, compressed.length);
@@ -387,49 +402,87 @@ public class ContigCAFWriter {
 
 		pw.print(buffer.toString());
 	}
-	
+
 	public void close() throws SQLException {
 		if (conn != null)
 			conn.close();
-		
+
 		conn = null;
 	}
-	
+
 	protected void finalize() {
 		try {
 			close();
+		} catch (SQLException sqle) {
 		}
-		catch (SQLException sqle) {}
+	}
+	
+	public List<Integer> getContigIDsForProject(String project) throws SQLException {
+		pstmtContigsForProject.setString(1, project);
+		
+		ResultSet rs = pstmtContigsForProject.executeQuery();
+		
+		Vector<Integer> ids = new Vector<Integer>();
+		
+		while (rs.next()) {
+			int id = rs.getInt(1);
+			ids.add(id);
+		}
+		
+		rs.close();
+		
+		return ids;
 	}
 
 	public static void main(String[] args) {
 		try {
-			String instance = "test";
-			ArcturusInstance ai = ArcturusInstance.getInstance(instance);
-			String organism = "TESTPKN";
-			ArcturusDatabase adb = ai.findArcturusDatabase(organism);
-			
-			ContigCAFWriter ccw = new  ContigCAFWriter(adb);
+			String instance = null;
+			String organism = null;
+			String project = null;
 			
 			for (int i = 0; i < args.length; i++) {
-				int contigid = Integer.parseInt(args[i]);
+				if (args[i].equalsIgnoreCase("-instance"))
+					instance = args[++i];
 				
-				if (contigid > 0) {
-					File file = new File(organism + contigid + ".caf");
-					
-					PrintWriter pw
-					   = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-					
-					ccw.writeContigAsCAF(contigid, pw);
-					
-					pw.close();
-				}
+				if (args[i].equalsIgnoreCase("-organism"))
+					organism = args[++i];
+				
+				if (args[i].equalsIgnoreCase("-project"))
+					project = args[++i];
 			}
-		}
-		catch (Exception e) {
+			
+			if (instance == null || organism == null || project == null) {
+				showUsage(System.err);
+				System.exit(1);
+			}
+			
+			ArcturusInstance ai = ArcturusInstance.getInstance(instance);
+			ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+
+			ContigCAFWriter ccw = new ContigCAFWriter(adb);
+			
+			File file = new File(project + ".caf");
+			
+			PrintWriter pw = new PrintWriter(new BufferedWriter(
+					new FileWriter(file)));
+			
+			List<Integer> ids = ccw.getContigIDsForProject(project);
+
+			for (int contigid : ids)
+				ccw.writeContigAsCAF(contigid, pw);
+			
+			pw.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
+	}
+	
+	protected static void showUsage(PrintStream ps) {
+		ps.println("MANDATORY PARAMETERS:");
+		ps.println("\t-instance\tName of instance");
+		ps.println("\t-organism\tName of organism");
+		ps.println("\t-project\tName of project");
 	}
 }
