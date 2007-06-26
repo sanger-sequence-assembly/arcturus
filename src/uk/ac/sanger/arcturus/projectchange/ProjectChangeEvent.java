ec65934654277a3c2a27d804a95e2433578f534d
David Harper <adh@sanger.ac.uk>
2007-06-26 09:08:16 +0000

    Added toString and getTypeAsString methods.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2254 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/projectchange/ProjectChangeEvent.java
+++ src/uk/ac/sanger/arcturus/projectchange/ProjectChangeEvent.java
@@ -8,22 +8,43 @@ public class ProjectChangeEvent extends EventObject {
 	public static final int CONTIGS_CHANGED = 1;
 	public static final int LOCK_CHANGED = 2;
 	public static final int OWNER_CHANGED = 3;
-	
+
 	protected Project project;
 	protected int type;
-	
+
 	public ProjectChangeEvent(Object source, Project project, int type) {
 		super(source);
-		
+
 		this.project = project;
 		this.type = type;
 	}
-	
+
 	public Project getProject() {
 		return project;
 	}
-	
+
 	public int getType() {
 		return type;
+	}
+	
+	public String getTypeAsString() {
+		switch (type) {
+			case CONTIGS_CHANGED:
+				return "CONTIGS_CHANGED";
+				
+			case LOCK_CHANGED:
+				return "LOCK_CHANGED";
+				
+			case OWNER_CHANGED:
+				return "OWNER_CHANGED";
+				
+			default:
+				return "UNKNOWN(" + type + ")";
+		}
+	}
+
+	public String toString() {
+		return "ProjectChangeEvent[source=" + source + ", project="
+				+ (project == null ? "null" : project.getName()) + ", type=" + getTypeAsString() + "]";
 	}
 }
