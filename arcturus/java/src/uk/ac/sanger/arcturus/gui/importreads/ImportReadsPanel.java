67aa7dca013299cd57e4adde4c29b46c0358c194
David Harper <adh@sanger.ac.uk>
2007-06-26 11:08:11 +0000

    Added code to notify project change listeners of read imports and contig transfers.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2256 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/gui/importreads/ImportReadsPanel.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/gui/importreads/ImportReadsPanel.java
@@ -1,6 +1,7 @@
 package uk.ac.sanger.arcturus.gui.importreads;
 
 import uk.ac.sanger.arcturus.gui.*;
+import uk.ac.sanger.arcturus.projectchange.ProjectChangeEvent;
 import uk.ac.sanger.arcturus.utils.*;
 import uk.ac.sanger.arcturus.*;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
@@ -24,17 +25,17 @@ public class ImportReadsPanel extends MinervaPanel {
 	protected JTextArea txtMessages = new JTextArea(20, 40);
 	protected JButton btnImportReads;
 	protected JButton btnClearMessages = new JButton("Clear messages");
-	
+
 	protected ProjectListModel plm;
-	
+
 	protected MinervaAbstractAction actionImportReads;
 	protected MinervaAbstractAction actionGetReadsFromFile;
-	
+
 	protected JFileChooser fileChooser = new JFileChooser();
 
 	public ImportReadsPanel(ArcturusDatabase adb, MinervaTabbedPane parent) {
 		super(parent, adb);
-		
+
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
 		try {
@@ -44,21 +45,21 @@ public class ImportReadsPanel extends MinervaPanel {
 		}
 
 		createActions();
-		
+
 		createMenus();
-		
+
 		getPrintAction().setEnabled(false);
-	
+
 		JPanel topPanel = new JPanel();
-		
+
 		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
-		
+
 		// Set up the text area for the list of reads
-		
+
 		JScrollPane scrollpane = new JScrollPane(txtReadList,
 				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		
+
 		txtReadList.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e) {
 				// Do nothing
@@ -70,16 +71,16 @@ public class ImportReadsPanel extends MinervaPanel {
 
 			public void removeUpdate(DocumentEvent e) {
 				updateImportReadsButton();
-			}		
+			}
 		});
 
 		JPanel panel = new JPanel(new BorderLayout());
 
 		panel.add(scrollpane, BorderLayout.CENTER);
-		
+
 		JButton btnClearReads = new JButton("Clear read list");
 		panel.add(btnClearReads, BorderLayout.SOUTH);
-		
+
 		btnClearReads.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				txtReadList.setText("");
@@ -87,88 +88,89 @@ public class ImportReadsPanel extends MinervaPanel {
 		});
 
 		panel.setBorder(etchedTitledBorder("Reads to import"));
-		
+
 		topPanel.add(panel);
 
 		panel = new JPanel(new BorderLayout());
 
 		plm = new ProjectListModel(adb);
-		
+
 		lstProjects = new JList(plm);
-		
+
 		lstProjects.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
+
 		lstProjects.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
 				updateImportReadsButton();
 			}
 		});
-		
+
 		scrollpane = new JScrollPane(lstProjects);
-		
+
 		panel.add(scrollpane, BorderLayout.CENTER);
-		
+
 		btnImportReads = new JButton(actionImportReads);
-		
+
 		panel.setBorder(etchedTitledBorder("Projects"));
 
 		topPanel.add(panel);
 
 		add(topPanel);
-		
+
 		panel = new JPanel(new FlowLayout());
-		
+
 		panel.add(btnImportReads);
-		
+
 		add(panel);
-		
+
 		scrollpane = new JScrollPane(txtMessages);
 
 		panel = new JPanel(new BorderLayout());
 
 		panel.add(scrollpane, BorderLayout.CENTER);
-		
+
 		panel.add(btnClearMessages, BorderLayout.SOUTH);
-		
+
 		btnClearMessages.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				txtMessages.selectAll();
 				txtMessages.cut();
-			}			
+			}
 		});
-		
+
 		panel.setBorder(etchedTitledBorder("Information"));
 
 		add(panel);
 	}
-	
+
 	protected void createActions() {
-		actionGetReadsFromFile = new MinervaAbstractAction("Open file of read names",
-				null, "Open file of read names", new Integer(KeyEvent.VK_O),
-				KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK)) {
+		actionGetReadsFromFile = new MinervaAbstractAction(
+				"Open file of read names", null, "Open file of read names",
+				new Integer(KeyEvent.VK_O), KeyStroke.getKeyStroke(
+						KeyEvent.VK_O, ActionEvent.CTRL_MASK)) {
 			public void actionPerformed(ActionEvent e) {
 				getReadsFromFile();
 			}
 		};
 
-		actionImportReads = new MinervaAbstractAction("Import reads",
-				null, "Import reads into project", new Integer(KeyEvent.VK_I),
+		actionImportReads = new MinervaAbstractAction("Import reads", null,
+				"Import reads into project", new Integer(KeyEvent.VK_I),
 				KeyStroke.getKeyStroke(KeyEvent.VK_I, ActionEvent.CTRL_MASK)) {
 			public void actionPerformed(ActionEvent e) {
 				importReadsIntoProject();
 			}
 		};
-		
+
 		actionImportReads.setEnabled(false);
 	}
 
-	protected boolean addClassSpecificFileMenuItems(JMenu menu) {		
+	protected boolean addClassSpecificFileMenuItems(JMenu menu) {
 		menu.add(actionGetReadsFromFile);
-		
+
 		return true;
 	}
 
-	protected void addClassSpecificViewMenuItems(JMenu menu) {		
+	protected void addClassSpecificViewMenuItems(JMenu menu) {
 	}
 
 	protected void createClassSpecificMenus() {
@@ -198,71 +200,89 @@ public class ImportReadsPanel extends MinervaPanel {
 
 	protected void getReadsFromFile() {
 		int rc = fileChooser.showOpenDialog(this);
-		
+
 		if (rc == JFileChooser.APPROVE_OPTION) {
 			addReadsToList(fileChooser.getSelectedFile());
 		}
 	}
-	
+
 	protected void addReadsToList(File file) {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			
+
 			String line;
-			
+
 			while ((line = br.readLine()) != null) {
 				txtReadList.append(line);
 				txtReadList.append("\n");
 			}
-			
+
 			br.close();
-		}
-		catch (IOException ioe) {
-			Arcturus.logWarning("Error encountered whilst reading file " + file.getPath(), ioe);
+		} catch (IOException ioe) {
+			Arcturus.logWarning("Error encountered whilst reading file "
+					+ file.getPath(), ioe);
 		}
 	}
 
 	protected void importReadsIntoProject() {
-		ProjectProxy proxy = (ProjectProxy)lstProjects.getSelectedValue();
-		
+		ProjectProxy proxy = (ProjectProxy) lstProjects.getSelectedValue();
+
 		Project project = proxy.getProject();
-		
+
 		String text = txtReadList.getText();
-		
+
 		StringTokenizer st = new StringTokenizer(text);
-		
+
 		int wordcount = st.countTokens();
-		
+
 		String[] readnames = new String[wordcount];
-		
+
 		for (int i = 0; i < wordcount; i++)
 			readnames[i] = st.nextToken();
 
-		txtMessages.append("There are " + readnames.length + " read names in the list\n");
-		txtMessages.append("They will be imported into " + project.getName() + "\n\n");
-		
+		txtMessages.append("There are " + readnames.length
+				+ " read names in the list\n");
+		txtMessages.append("They will be imported into " + project.getName()
+				+ "\n\n");
+
 		try {
-			int[] rcs = importer.makeSingleReadContigs(readnames, project.getID());
-		
+			int[] rcs = importer.makeSingleReadContigs(readnames, project
+					.getID());
+
+			int ok = 0;
+
+			for (int i = 0; i < rcs.length; i++)
+				if (rcs[i] == ReadToProjectImporter.OK)
+					ok++;
+
 			for (int i = 0; i < readnames.length; i++)
-				txtMessages.append(readnames[i] + " : " +
-						importer.getErrorMessage(rcs[i]) + "\n");
-		}
-		catch (SQLException sqle) {
-			Arcturus.logWarning("An error occurred whilst making single-read contigs", sqle);
+				txtMessages.append(readnames[i] + " : "
+						+ importer.getErrorMessage(rcs[i]) + "\n");
+
+			if (ok > 0) {
+				ProjectChangeEvent event = new ProjectChangeEvent(this,
+						project, ProjectChangeEvent.CONTIGS_CHANGED);
+
+				adb.notifyProjectChangeEventListeners(event);
+			}
+		} catch (SQLException sqle) {
+			Arcturus
+					.logWarning(
+							"An error occurred whilst making single-read contigs",
+							sqle);
 		}
 	}
-	
+
 	protected void updateImportReadsButton() {
 		boolean isProjectSelected = !lstProjects.isSelectionEmpty();
 		boolean haveReadsInList = txtReadList.getDocument().getLength() > 0;
-		
+
 		actionImportReads.setEnabled(isProjectSelected && haveReadsInList);
 	}
-	
+
 	public boolean setSelectedProject(String name) {
 		ProjectProxy proxy = plm.getProjectProxyByName(name);
-		
+
 		if (proxy == null)
 			return false;
 		else {
@@ -270,7 +290,7 @@ public class ImportReadsPanel extends MinervaPanel {
 			return true;
 		}
 	}
-	
+
 	class ProjectListModel extends AbstractListModel {
 		ProjectProxy[] projects;
 		ArcturusDatabase adb;
@@ -283,22 +303,21 @@ public class ImportReadsPanel extends MinervaPanel {
 		public void refresh() {
 			try {
 				Set projectset = adb.getAllProjects();
-				
+
 				projects = new ProjectProxy[projectset.size()];
-				
+
 				int i = 0;
-				
+
 				for (Iterator iter = projectset.iterator(); iter.hasNext(); i++) {
-					Project project = (Project)iter.next();
+					Project project = (Project) iter.next();
 					projects[i] = new ProjectProxy(project);
 				}
-				
+
 				Arrays.sort(projects);
 				fireContentsChanged(this, 0, projects.length);
+			} catch (SQLException sqle) {
+
 			}
-			catch (SQLException sqle) {
-				
-			}	
 		}
 
 		public Object getElementAt(int index) {
@@ -313,7 +332,7 @@ public class ImportReadsPanel extends MinervaPanel {
 			for (int i = 0; i < projects.length; i++)
 				if (projects[i].toString().equalsIgnoreCase(name))
 					return projects[i];
-			
+
 			return null;
 		}
 	}
@@ -334,8 +353,9 @@ public class ImportReadsPanel extends MinervaPanel {
 		}
 
 		public int compareTo(Object o) {
-			ProjectProxy that = (ProjectProxy)o;
-			return project.getName().compareToIgnoreCase(that.project.getName());
+			ProjectProxy that = (ProjectProxy) o;
+			return project.getName()
+					.compareToIgnoreCase(that.project.getName());
 		}
 	}
 
