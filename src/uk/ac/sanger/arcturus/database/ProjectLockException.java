9e6a349b79c7c78c0b96d29f53b641e5b84263ce
David Harper <adh@sanger.ac.uk>
2007-06-27 10:09:48 +0000

    Created a generic exception superclass for Arcturus database operations, and a specfic subclass for project lock/unlock operations.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2268 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/database/ProjectLockException.java
@@ -0,0 +1,52 @@
+package uk.ac.sanger.arcturus.database;
+
+public class ProjectLockException extends ArcturusDatabaseException {
+	public static final int OK = 0;
+	public static final int PROJECT_IS_LOCKED = 1;
+	public static final int PROJECT_IS_UNLOCKED = 2;
+	public static final int OPERATION_NOT_PERMITTED = 3;
+	public static final int PROJECT_HAS_NO_OWNER = 4;
+	
+	protected int type = OK;
+	
+	public ProjectLockException(String message, int type) {
+		super(message);
+		this.type = type;
+	}
+	
+	public ProjectLockException(int type) {
+		this(null, type);
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public String getTypeAsString() {
+		switch (type) {
+			case OK:
+				return "OK";
+				
+			case PROJECT_IS_LOCKED:
+				return "The project is already locked";
+				
+			case PROJECT_IS_UNLOCKED:
+				return "The project is already unlocked";
+				
+			case OPERATION_NOT_PERMITTED:
+				return "The operation is not permitted";
+				
+			case PROJECT_HAS_NO_OWNER:
+				return "The project has no owner";
+				
+			default:
+				return "(Unknown type code: " + type + ")";
+		}
+	}
+	
+	public String getMessage() {
+		String message = super.getMessage();
+		
+		return message == null ? getTypeAsString() : message;
+	}
+}
