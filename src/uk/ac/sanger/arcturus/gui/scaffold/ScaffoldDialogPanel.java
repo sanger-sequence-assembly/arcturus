8a67a36588feca71bf4f3f2d69ce373e98ea65df
David Harper <adh@sanger.ac.uk>
2007-07-20 12:55:40 +0000

    Added the remaining sub-panels.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2347 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/gui/scaffold/ScaffoldDialogPanel.java
+++ src/uk/ac/sanger/arcturus/gui/scaffold/ScaffoldDialogPanel.java
@@ -15,9 +15,20 @@ public class ScaffoldDialogPanel extends JPanel {
 	String[] projects = {"BIN", "PKN1", "PKN2", "PKN3", "PKN4" };
 	private JComboBox cbxProjectList = new JComboBox(projects);
 	
-	private JRadioButton btnIncludeFromSeedProject = new JRadioButton("From seed project only");
-	private JRadioButton btnIncludeFromSeedProjectAndBin  = new JRadioButton("From seed project and BIN");
-	private JRadioButton btnIncludeFromAll = new JRadioButton("From all projects");
+	private JRadioButton btnIncludeFromSeedProject = new JRadioButton("Seed project only");
+	private JRadioButton btnIncludeFromSeedProjectAndBin  = new JRadioButton("Seed project and BIN");
+	private JRadioButton btnIncludeFromAll = new JRadioButton("All projects");
+	
+	private JRadioButton btnAnyLength = new JRadioButton("Any length");
+	private JRadioButton btnAtLeast2kb = new JRadioButton("At least 2kb");
+	private JRadioButton btnAtLeast5kb = new JRadioButton("At least 5kb");
+	
+	private JRadioButton btnAnyNumber = new JRadioButton("Any number");
+	private JRadioButton btnAtLeast2 = new JRadioButton("At least 2");
+	
+	private JRadioButton btnOnlyBestBridge = new JRadioButton("Only the best bridge");
+	private JRadioButton btnBestTwoBridges = new JRadioButton("The best two bridges");
+	private JRadioButton btnAllBridges = new JRadioButton("All bridges");
 		
 	public ScaffoldDialogPanel() {
 		super(null);
@@ -26,13 +37,13 @@ public class ScaffoldDialogPanel extends JPanel {
 		
 		add(createContigSelectionPanel());
 		
-		add(createIncludeContigsPanel());
+		add(createIncludeProjectsPanel());
 		
-		//add(createLengthCriterionPanel());
+		add(createLengthCriterionPanel());
 		
-		//add(createSubCloneCriterionPanel());
+		add(createSubCloneCriterionPanel());
 		
-		//add(createBridgeCriterionPanel());
+		add(createBridgeCriterionPanel());
 	}
 	
 	private JPanel createContigSelectionPanel() {
@@ -80,7 +91,7 @@ public class ScaffoldDialogPanel extends JPanel {
 		return panel;
 	}
 	
-	private JPanel createIncludeContigsPanel() {
+	private JPanel createIncludeProjectsPanel() {
 		JPanel panel = new JPanel(new GridLayout(0,1));
 
 		ButtonGroup group = new ButtonGroup();
@@ -95,13 +106,78 @@ public class ScaffoldDialogPanel extends JPanel {
 		btnIncludeFromSeedProject.setSelected(true);
 		
 		Border border = BorderFactory.createTitledBorder(
-			       loweredetched, "Which contigs to include?");
+			       loweredetched, "Which projects to include?");
+		
+		panel.setBorder(border);
+		
+		return panel;
+	}
+	
+	private JPanel createLengthCriterionPanel() {
+		JPanel panel = new JPanel(new GridLayout(0,1));
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(btnAnyLength);
+		group.add(btnAtLeast2kb);
+		group.add(btnAtLeast5kb);
+		
+		panel.add(btnAnyLength);
+		panel.add(btnAtLeast2kb);
+		panel.add(btnAtLeast5kb);
+		
+		btnAtLeast2kb.setSelected(true);
+		
+		Border border = BorderFactory.createTitledBorder(
+			       loweredetched, "Minimum length for contigs to include?");
 		
 		panel.setBorder(border);
 		
 		return panel;
 	}
 
+	private JPanel createSubCloneCriterionPanel() {
+		JPanel panel = new JPanel(new GridLayout(0,1));
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(btnAnyNumber);
+		group.add(btnAtLeast2);
+		
+		panel.add(btnAnyNumber);
+		panel.add(btnAtLeast2);
+	
+		btnAtLeast2.setSelected(true);
+		
+		Border border = BorderFactory.createTitledBorder(
+			       loweredetched, "How many sub-clones to validate a bridge?");
+		
+		panel.setBorder(border);
+		
+		return panel;
+	}
+	
+	private JPanel createBridgeCriterionPanel() {
+		JPanel panel = new JPanel(new GridLayout(0,1));
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(btnOnlyBestBridge);
+		group.add(btnBestTwoBridges);
+		group.add(btnAllBridges);
+		
+		panel.add(btnOnlyBestBridge);
+		panel.add(btnBestTwoBridges);
+		panel.add(btnAllBridges);
+		
+		btnBestTwoBridges.setSelected(true);
+		
+		Border border = BorderFactory.createTitledBorder(
+			       loweredetched, "Which bridges to include?");
+		
+		panel.setBorder(border);
+		
+		return panel;
+		
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
