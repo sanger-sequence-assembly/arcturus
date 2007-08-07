649de51591b40fc2d99f6405e6b740266905288d
David Harper <adh@sanger.ac.uk>
2007-08-07 11:18:24 +0000

    Added an SQL fragment which lists contigs which were current as at the date specified in @cutoff.
    Replaced free reads query using temporary tables with a nifty one-liner.
    
    
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2367 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/sql/freereads.sql
+++ arcturus/sql/freereads.sql
@@ -1,16 +1,6 @@
-create temporary table CURCTG as
-  select CONTIG.contig_id from CONTIG left join C2CMAPPING
-  on CONTIG.contig_id = C2CMAPPING.parent_id
-  where C2CMAPPING.parent_id is null;
-
-create temporary table CURSEQ
-  (seq_id integer not null, contig_id integer not null, key (contig_id)) as
-  select seq_id,CURCTG.contig_id from CURCTG left join MAPPING using(contig_id);
-
-create temporary table CURREAD
-  (read_id integer not null, seq_id integer not null, contig_id integer not null, key (read_id)) as
-  select read_id,SEQ2READ.seq_id,contig_id from CURSEQ left join SEQ2READ using(seq_id);
-
-create temporary table FREEREAD as
-  select READINFO.read_id from READINFO left join CURREAD using(read_id)
-  where seq_id is null;
+select READINFO.read_id,asped
+  from READINFO left join (SEQ2READ, MAPPING, CURRENTCONTIGS)
+  on (READINFO.read_id = SEQ2READ.read_id
+  and SEQ2READ.seq_id = MAPPING.seq_id
+  and MAPPING.contig_id = CURRENTCONTIGS.contig_id)
+  where CURRENTCONTIGS.contig_id is null;
