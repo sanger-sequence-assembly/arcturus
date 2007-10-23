f459d9cff149d6ed918f3f28a943a7df62cc0e3a
David Harper <adh@sanger.ac.uk>
2007-10-23 09:11:47 +0000

    Added a no-argument constructor which uses the job placer properties in the global properties file.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2519 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/jobrunner/JobPlacer.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/jobrunner/JobPlacer.java
@@ -2,11 +2,33 @@ package uk.ac.sanger.arcturus.jobrunner;
 
 import java.io.*;
 
+import uk.ac.sanger.arcturus.Arcturus;
+
 public class JobPlacer {
 	private String[] command = new String[3];
+	
+	public JobPlacer() throws Exception {
+		String prefix = Arcturus.getProperty("jobplacer.prefix");
+		
+		if (prefix == null)
+			throw new Exception("Unable to find jobplacer.prefix in Arcturus properties");
+		
+		String host = Arcturus.getProperty("jobplacer.host");
+		
+		if (prefix == null)
+			throw new Exception("Unable to find jobplacer.host in Arcturus properties");
+		
+		String cmd = Arcturus.getProperty("jobplacer.command");
+		
+		if (prefix == null)
+			throw new Exception("Unable to find jobplacer.command in Arcturus properties");
+		
+		command[0] = prefix;
+		command[1] = host;
+		command[2] = cmd;		
+	}
 
 	public JobPlacer(String prefix, String host, String cmd) {
-
 		command[0] = prefix;
 		command[1] = host;
 		command[2] = cmd;
