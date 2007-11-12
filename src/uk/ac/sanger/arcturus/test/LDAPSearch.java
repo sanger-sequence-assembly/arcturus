f375a44f4b3e02969d47794b70575123b151367a
David Harper <adh@sanger.ac.uk>
2007-11-12 12:50:51 +0000

    Updated code to use proper command-line arguments and to display the details of any MySQL data source objects that are found.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2550 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/test/LDAPSearch.java
+++ src/uk/ac/sanger/arcturus/test/LDAPSearch.java
@@ -4,6 +4,8 @@ import java.util.*;
 import javax.naming.*;
 import javax.naming.directory.*;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 public class LDAPSearch {
 	private DirContext rootctx;
 	
@@ -31,29 +33,53 @@ public class LDAPSearch {
 			
 			String name = res.getName();
 			
-			System.out.println("Name:\t" + name);
+			System.out.println("Name:\t\t" + name);
 			System.out.println("Classname:\t" + classname);
-			if (obj != null) {
-				System.out.println(obj);
+			System.out.println("isRelative:\t" + (res.isRelative() ? "yes" : "no"));
+			 
+			if (obj != null && obj instanceof MysqlDataSource) {
+				MysqlDataSource ds = (MysqlDataSource)obj;
+				
+				System.out.println("Host:\t\t" + ds.getServerName());
+				System.out.println("Port:\t\t" + ds.getPortNumber());
+				System.out.println("Database:\t" + ds.getDatabaseName());
 			}
+			
 			System.out.println();
 		}
 	}
 	
 	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.err.println("Usage: LDAPSearch context-name key");
-			System.exit(1);
+		String ctxname = null;
+		String key = null;
+		String url = null;
+		
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("-name"))
+				ctxname = args[++i];
+			else if (args[i].equalsIgnoreCase("-key"))
+				key = args[++i];
+			else if (args[i].equalsIgnoreCase("-url"))
+				url = args[++i];
+			else
+				System.err.println("Unknown option: \"" + args[i] + "\"");
 		}
 		
-		String ctxname = args[0];
-		String key = args[1];
+		if (ctxname == null)
+			ctxname = "";
+		
+		if (url == null)
+			url = "ldap://ldap.internal.sanger.ac.uk/cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk";
+		
+		if (key == null) {
+			System.err.println("Usage: LDAPSearch -key key [-name ctxname] [-url url]");
+			System.exit(1);
+		}
 		
 		Properties props = new Properties();
 		
 		props.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		props.put(Context.PROVIDER_URL, "ldap://ldap.internal.sanger.ac.uk/cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk");
-
+		props.put(Context.PROVIDER_URL, url);
 		
 		try {
 			LDAPSearch ls = new LDAPSearch(props);
