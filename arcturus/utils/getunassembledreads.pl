6c353253561c838ffc8b0d439ca9981d62a4e28b
Ed Zuiderwijk <ejz@sanger.ac.uk>
2007-11-14 16:08:04 +0000

    Added test code to trace free reads inconsistency due to failed reads
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2556 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/utils/getunassembledreads.pl
+++ arcturus/utils/getunassembledreads.pl
@@ -8,6 +8,8 @@ use FileHandle;
 use Logging;
 use PathogenRepository;
 
+use DBI;
+
 #----------------------------------------------------------------
 # ingest command line parameters
 #----------------------------------------------------------------
@@ -31,6 +33,7 @@ my $addtag;
 my $namelike;
 my $namenotlike;
 my $excludelist;
+my $status;
 
 my $threshold;  # only with clipmethod
 my $clipmethod;
@@ -39,12 +42,13 @@ my $minimumrange;
 my $logfile;            # default STDERR
 my $loglevel;           # default log warnings and errors only
 my $debug;
+my $test;
 
 my $validKeys  = "organism|instance|assembly|caf|aspedbefore|ab|aspedafter|aa|"
                . "nosingleton|ns|blocksize|bs|selectmethod|sm|namelike|nl|"
-               . "namenotlike|nnl|excludelist|el|mask|tags|all|fasta|"
-               . "clipmethod|cm|threshold|th|minimumqualityrange|mqr|"
-               . "info|verbose|debug|log|help";
+               . "namenotlike|nnl|excludelist|el|mask|tags|all|fasta|nofile|"
+               . "clipmethod|cm|threshold|th|minimumqualityrange|mqr|status|nostatus|"
+               . "test|info|verbose|debug|log|help";
 
 
 while (my $nextword = shift @ARGV) {
@@ -85,6 +89,7 @@ while (my $nextword = shift @ARGV) {
     $fasta            = 0            if ($nextword eq '-caf');
     $outputFileName   = shift @ARGV  if ($nextword eq '-fasta');
     $fasta            = 1            if ($nextword eq '-fasta');
+    $test             = 1            if ($nextword eq '-nofile');
 
     $blocksize        = shift @ARGV  if ($nextword eq '-blocksize');
     $blocksize        = shift @ARGV  if ($nextword eq '-bs');
@@ -103,13 +108,18 @@ while (my $nextword = shift @ARGV) {
     $nosingleton      = 1            if ($nextword eq '-nosingleton');
     $nosingleton      = 1            if ($nextword eq '-ns');
 
+    $status           = 0            if ($nextword eq '-nostatus');
+    $status           = shift @ARGV  if ($nextword eq '-status');
+
     $addtag           = 1            if ($nextword eq '-tags');
 
     $loglevel         = 1            if ($nextword eq '-verbose'); 
-    $loglevel         = 1            if ($nextword eq '-info'); 
+    $loglevel         = 2            if ($nextword eq '-info'); 
     $loglevel         = 1            if ($nextword eq '-debug');
 
     $debug            = 1            if ($nextword eq '-debug'); 
+
+    $test             = 2            if ($nextword eq '-test'); 
 
     $number           = shift @ARGV  if ($nextword eq '-all');
 
@@ -136,7 +146,7 @@ $logger->setBlock('debug',unblock=>1) if $debug;
 
 &showUsage("Missing database instance") unless $instance;
 
-&showUsage("Missing caf or fasta filename") unless defined $outputFileName;
+&showUsage("Missing caf or fasta filename") unless ($test || defined($outputFileName));
 
 my $adb = new ArcturusDatabase (-instance => $instance,
 			        -organism => $organism);
@@ -167,7 +177,7 @@ if ($fasta) {
     }
     $QLT = *STDOUT unless $QLT;
 }
-else {
+elsif (defined($outputFileName)) {
 # caf output
     if ($outputFileName) {
         $outputFileName .= '.caf' unless ($outputFileName =~ /\.caf/);
@@ -181,6 +191,7 @@ my %options;
 $options{nosingleton} = 1 if $nosingleton;
 $options{aspedbefore} = $aspedbefore if $aspedbefore;
 $options{aspedafter}  = $aspedafter  if $aspedafter;
+$options{status}      = $status      if defined($status);
 
 if (defined($selectmethod)) {
     $options{method} = $selectmethod;
@@ -222,10 +233,92 @@ if ($number) {
 }
 else {
 # standard mode
+    $options{test} = 1 if $test;
     $readids = $adb->getIDsForUnassembledReads(%options);
+
+    if ($test && $test == 2) {
+        $logger->info("found ".scalar(@$readids)." reads");
+        $logger->info("testing individual reads");
+        @$readids = sort {$a <=> $b} @$readids;
+# assembled reads ?
+        my $notfree = 0;
+        foreach my $readid (@$readids) {
+            my $isfree = $adb->isUnassembledRead('read_id',$readid);
+            next if $isfree;
+            $logger->info("read $readid is not free");
+            $notfree++;
+	}
+        $logger->warning("$notfree assembled reads found") if $notfree;
+# multiple read IDs?
+        $logger->info("testing for duplicates");
+        for (my $i = 1 ; $i < scalar(@$readids) ; $i++) {
+            next unless ($readids->[$i] == $readids->[$i-1]);
+            $logger->error("duplicate read ID $readids->[$i]");
+	}
+# testing against FREEREADS table
+        $logger->info("testing against FREEREADS view");
+        my $freereads = $adb->getFreeReads('read_id');
+        $logger->info("found ".scalar(@$freereads)." reads");
+        my $inventory = {};
+        foreach my $readid (@$readids) {
+            $inventory->{$readid} = [] unless $inventory->{$readid};
+            $inventory->{$readid}->[0] = 1;
+        }
+        foreach my $readid (@$freereads) {
+            $inventory->{$readid} = [] unless $inventory->{$readid};
+            $inventory->{$readid}->[1] = 1;
+        }
+        my $stragglers = [];
+        foreach my $readid (sort {$a <=> $b} keys %$inventory) {
+            my $occurs = $inventory->{$readid};
+            next if ($occurs->[0] && $occurs->[1]);
+            $logger->warning("read $readid occurs in U but not in F") if $occurs->[0]; 
+            $logger->info("read $readid occurs in F but not in U") if $occurs->[1];
+            push @$stragglers,$readid;
+	}
+        if (@$stragglers) {
+            my $notfree = 0;
+            $logger->info("testing ".scalar(@$stragglers)." discordant reads");
+            foreach my $readid (@$stragglers) {
+                my $isfree = $adb->isUnassembledRead('read_id',$readid);
+                next if $isfree;
+                $logger->info("read $readid is not free");
+                $notfree++;
+            }
+            $logger->warning("$notfree assembled reads found") if $notfree;
+            $logger->warning("all reads are free reads")  unless  $notfree;
+# do some tests on the temporary tables
+            $logger->info("testing TEMPORARY tables");
+            unless ($selectmethod && $selectmethod == 2) {
+                $options{method} = 'intemporarytable';
+                $adb->getIDsForUnassembledReads(%options);
+	    }
+# test the temporaryu tables
+            my $dbh = $adb->getConnection();
+            foreach my $table ('CURCTG','CURSEQ','CURREAD','FREEREAD') {
+                my $query = "select count(*) from $table";
+                my $sth = $dbh->prepare($query);
+		$sth->execute() || $logger->error("Failed query '$query': ");
+                $logger->error("MySQL error: $DBI::err ($DBI::errstr)") if ($DBI::err);
+                my $count = $sth->fetchrow_array() || 'unknown number of';
+		$logger->info("temporary table $table has $count rows");
+                $sth->finish();
+	    }
+	}
+        for (my $i = 1 ; $i <= 10 ; $i++) {
+	    my $query = $adb->logQuery($i);
+	    next unless $query;
+            $logger->warning("query $i:\n$query");
+	}
+
+        $adb->disconnect();
+	exit 0;
+    }
 }
 
 $logger->info("Retrieving ".scalar(@$readids)." Reads");
+
+undef @$readids unless $outputFileName; # else no export
 
 my $discarded = 0;
 my $excluded  = 0;
@@ -274,8 +367,8 @@ while (my $remainder = scalar(@$readids)) {
                 next;
 	    }
         }
-        $read->writeToCaf($CAF,qualitymask=>$mask) unless $fasta;
-        $read->writeToFasta($FAS,$QLT,qualitymask=>$mask) if $fasta;
+        $read->writeToCaf($CAF,qualitymask=>$mask)        if $CAF;
+        $read->writeToFasta($FAS,$QLT,qualitymask=>$mask) if $FAS;
     }
     undef @$reads;
 }
@@ -342,6 +435,9 @@ sub showUsage {
     print STDOUT "-aspedbefore\tAsped date upper bound (inclusive)\n";
     print STDOUT "-namelike\t(include) readname with wildcard or a pattern\n";
     print STDOUT "-namenotlike\t(exclude) readname with wildcard or a pattern\n";
+    print STDOUT "\n";
+    print STDOUT "-nostatus\tdo not select on read status; default select 'PASS'\n";
+    print STDOUT "-status\t\tspecify status explicitly\n";
     print STDOUT "\n";
     print STDOUT "-all\t\toverrides all other selection (except excludelist &\n";
     print STDOUT "\t\tstatus); exports reads 1 to number provided \n";
