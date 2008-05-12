61115be22f6586f37d96f4ffb95b0395ae198555
David Harper <adh@sanger.ac.uk>
2008-05-12 10:08:54 +0000

    Added test program to try to figure out the BindException errors that have been occurring.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2828 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/test/JMXServerTest.java
@@ -0,0 +1,44 @@
+package uk.ac.sanger.arcturus.test;
+
+import java.net.InetAddress;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import javax.management.MBeanServer;
+import javax.management.remote.*;
+
+public class JMXServerTest {
+	public static void main(String[] args) {
+		JMXServerTest tester = new JMXServerTest();
+
+		try {
+			tester.run();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+		System.exit(0);
+	}
+
+	public void run() throws IOException {
+		String hostname = InetAddress.getLocalHost().getHostName();
+		
+		System.err.println("InetAddress.getLocalHost().getHostName() returned " + hostname);
+		
+		String url = "service:jmx:jmxmp://" + hostname + "/";
+		
+		System.err.println("URL is " + url);
+
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+
+		JMXServiceURL jurl = new JMXServiceURL(url);
+
+		JMXConnectorServer server = JMXConnectorServerFactory
+				.newJMXConnectorServer(jurl, null, mbs);
+
+		server.start();
+
+		jurl = server.getAddress();
+
+		System.err.println("JMX URL is " + jurl);
+	}
+}
