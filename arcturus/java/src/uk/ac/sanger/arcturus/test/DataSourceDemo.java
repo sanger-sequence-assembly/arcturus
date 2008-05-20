9f354cc3586bd90eff00122cfce7a0ee049a1ae1
David Harper <adh@sanger.ac.uk>
2008-05-20 11:16:04 +0000

    Updated data source demo to include PostgreSQL data sources, and changed code to read JNDI parameters from a properties file. Added the necessary properties file.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@2853 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/java/src/uk/ac/sanger/arcturus/test/DataSourceDemo.java
+++ arcturus/java/src/uk/ac/sanger/arcturus/test/DataSourceDemo.java
@@ -2,87 +2,113 @@ package uk.ac.sanger.arcturus.test;
 
 import java.sql.Connection;
 import javax.sql.DataSource;
+
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
+
 import java.util.Properties;
 
-// Notice that the URL of the LDAP server is *not* hard-coded in the Java
-// client. Instead, it is specified as a run-time property which can be
-// set in a wrapper script:
-//
-// set JNDI_FACTORY=com.sun.jndi.ldap.LdapCtxFactory
-// set JNDI_URL="ldap://ldap/cn=test,cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk"
-// ...
-// set JNDI_OPTS="-Djava.naming.factory.initial=$JNDI_FACTORY -Djava.naming.provider.url=$JNDI_URL"
-// ...
-// java $JNDI_OPTS ... DataSourceDemo args ...
-
 public class DataSourceDemo {
-	public static void main(String args) {
+	public static void main(String[] args) {
+		DataSourceDemo demo = new DataSourceDemo();
 		try {
-			// 0. Create an LDAP context using parameters specified in the
-			// run-time
-			// properties.
-
-			Properties env = System.getProperties();
-
-			Context ctx = new InitialContext(env);
-
-			// 1. Create and bind a data source
-
-			boolean useMysql = Boolean.getBoolean("mysql");
-
-			String alias;
-			DataSource ds1;
-
-			if (useMysql) {
-				com.mysql.jdbc.jdbc2.optional.MysqlDataSource mysqlds = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
-
-				mysqlds.setServerName("pcs3");
-				mysqlds.setDatabaseName("EIMER");
-				mysqlds.setPort(14642);
-				mysqlds.setUser("arcturus");
-				mysqlds.setPassword("myPassword");
-
-				alias = "cn=EIMER";
-				ds1 = mysqlds;
-			} else {
-				oracle.jdbc.pool.OracleDataSource oracleds = new oracle.jdbc.pool.OracleDataSource();
-
-				oracleds.setServerName("ocs2");
-				oracleds.setDatabaseName("wgs");
-				oracleds.setPortNumber(1522);
-				oracleds.setUser("pathlook");
-				oracleds.setPassword("pathlook");
-				oracleds.setDriverType("thin");
-
-				alias = "cn=WGS";
-				ds1 = oracleds;
-			}
-
-			ctx.bind(alias, ds1);
-
-			// 2. Lookup the data source
-
-			DataSource ds2 = (DataSource) ctx.lookup(alias);
-
-			// 3. Establish a connection to the database
-
-			Connection conn1 = ds2.getConnection();
-
-			// 4. Establish a second connection, over-riding the default
-			// username and
-			// password
-
-			Connection conn2 = ds2.getConnection("dba", "adminpassword");
-
-			// 5. Close the connections.
-
-			conn1.close();
-			conn2.close();
+			demo.run();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(1);
+		}
+		System.exit(0);
+	}
+
+	private Properties props = new Properties();
+
+	public void run() throws Exception {
+		// 0. Create an LDAP context using parameters specified in the
+		// properties file.
+		
+		loadProperties();
+
+		Context ctx = new InitialContext(props);
+
+		// 1. Create and bind a data source
+
+		String dstype = System.getProperty("type", "mysql");
+		
+		String alias = null;
+		DataSource ds1 = null;
+
+		if (dstype.equalsIgnoreCase("mysql")) {
+			com.mysql.jdbc.jdbc2.optional.MysqlDataSource mysqlds = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
+
+			mysqlds.setServerName("mcs3a");
+			mysqlds.setDatabaseName("EIMER");
+			mysqlds.setPort(15001);
+			mysqlds.setUser("arcturus");
+			mysqlds.setPassword("***REMOVED***");
+
+			alias = "cn=EIMER";
+			ds1 = mysqlds;
+		} else if (dstype.equalsIgnoreCase("oracle")){
+			oracle.jdbc.pool.OracleDataSource oracleds = new oracle.jdbc.pool.OracleDataSource();
+
+			oracleds.setServerName("ocs2");
+			oracleds.setDatabaseName("wgs");
+			oracleds.setPortNumber(1522);
+			oracleds.setUser("pathlook");
+			oracleds.setPassword("pathlook");
+			oracleds.setDriverType("thin");
+
+			alias = "cn=WGS";
+			ds1 = oracleds;
+		} else if (dstype.equalsIgnoreCase("postgresql")) {
+			org.postgresql.ds.PGSimpleDataSource pgds = new org.postgresql.ds.PGSimpleDataSource();
+			
+			pgds.setServerName("pcs4d");
+			pgds.setPortNumber(15003);
+			pgds.setDatabaseName("test");
+			pgds.setUser("arcturus");
+			pgds.setPassword("***REMOVED***");
+			
+			alias = "cn=pgtest";
+			ds1 = pgds;
+		} else
+			throw new Exception("Unknown datasource type: " + dstype);
+
+		ctx.bind(alias, ds1);
+
+		// 2. Lookup the data source
+
+		DataSource ds2 = (DataSource) ctx.lookup(alias);
+
+		// 3. Establish a connection to the database
+
+		Connection conn1 = ds2.getConnection();
+
+		// 4. Establish a second connection, over-riding the default
+		// user-name and password
+
+		Connection conn2 = ds2.getConnection("dba", "adminpassword");
+
+		// 5. Close the connections.
+
+		conn1.close();
+		conn2.close();
+	}
+
+	protected void loadProperties() throws IOException {
+		InputStream is = getClass().getResourceAsStream("datasourcedemo.props");
+		props.load(is);
+		is.close();
+		
+		if (!props.containsKey(Context.SECURITY_CREDENTIALS)) {
+			Console console = System.console();
+			
+			char[] password = console.readPassword("Enter password>");
+			
+			props.setProperty(Context.SECURITY_CREDENTIALS, new String(password));
 		}
 	}
 }
