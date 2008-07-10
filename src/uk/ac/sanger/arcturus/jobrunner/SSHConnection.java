a6483e82d81822a42c0fe7f6ff86c44b5c68a8a1
David Harper <adh@sanger.ac.uk>
2008-07-10 12:27:31 +0000

    Factored out the code to create and authenticate an SSH connection into a separate class. Added debugging output to trace the problems under OS X and Windows.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@2925 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ src/uk/ac/sanger/arcturus/jobrunner/SSHConnection.java
@@ -0,0 +1,34 @@
+package uk.ac.sanger.arcturus.jobrunner;
+
+import com.trilead.ssh2.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SSHConnection {
+	public static Connection getConnection(String hostname) throws IOException {
+		Connection conn = new Connection(hostname);
+
+		conn.connect();
+
+		boolean isAuthenticated = false;
+
+		File home = new File(System.getProperty("user.home"));
+		File sshdir = new File(home, ".ssh");
+		
+		String keyfilePass = null;
+		String username = System.getProperty("user.name");
+		
+		String[] pkfiles = { "id_dsa", "id_rsa" };
+		
+		for (int i = 0; i < pkfiles.length && !isAuthenticated; i++) {
+			File keyfile = new File(sshdir, pkfiles[i]);
+			isAuthenticated = conn.authenticateWithPublicKey(username, keyfile,
+					keyfilePass);
+		}
+
+		if (isAuthenticated == false)
+			throw new IOException("Authentication failed.");
+		
+		return conn;
+	}
+}
