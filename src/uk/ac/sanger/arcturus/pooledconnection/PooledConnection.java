502f78301c04504de75fa4603a17abc53fd0add1
David Harper <adh@sanger.ac.uk>
2008-08-14 13:40:11 +0000

    Restored the JDBC 4 methods for Java 6 compliance.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@3004 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/pooledconnection/PooledConnection.java
+++ src/uk/ac/sanger/arcturus/pooledconnection/PooledConnection.java
@@ -329,59 +329,59 @@ public class PooledConnection implements Connection, PooledConnectionMBean {
 		stmt.close();
 	}
 
-	//public Array createArrayOf(String typeName, Object[] elements)
-	//		throws SQLException {
-	//	return conn.createArrayOf(typeName, elements);
-	//}
+	public Array createArrayOf(String typeName, Object[] elements)
+			throws SQLException {
+		return conn.createArrayOf(typeName, elements);
+	}
 
-	//public Blob createBlob() throws SQLException {
-	//	return conn.createBlob();
-	//}
+	public Blob createBlob() throws SQLException {
+		return conn.createBlob();
+	}
 
-	//public Clob createClob() throws SQLException {
-	//	return conn.createClob();
-	//}
+	public Clob createClob() throws SQLException {
+		return conn.createClob();
+	}
 
-	//public NClob createNClob() throws SQLException {
-	//	return conn.createNClob();
-	//}
+	public NClob createNClob() throws SQLException {
+		return conn.createNClob();
+	}
 
-	//public SQLXML createSQLXML() throws SQLException {
-	//	return conn.createSQLXML();
-	//}
+	public SQLXML createSQLXML() throws SQLException {
+		return conn.createSQLXML();
+	}
 
-	//public Struct createStruct(String typeName, Object[] attributes)
-	//		throws SQLException {
-	//	return conn.createStruct(typeName, attributes);
-	//}
+	public Struct createStruct(String typeName, Object[] attributes)
+			throws SQLException {
+		return conn.createStruct(typeName, attributes);
+	}
 
-	//public Properties getClientInfo() throws SQLException {
-	//	return conn.getClientInfo();
-	//}
+	public Properties getClientInfo() throws SQLException {
+		return conn.getClientInfo();
+	}
 
-	//public String getClientInfo(String name) throws SQLException {
-	//	return conn.getClientInfo(name);
-	//}
+	public String getClientInfo(String name) throws SQLException {
+		return conn.getClientInfo(name);
+	}
 
-	//public boolean isValid(int timeout) throws SQLException {
-	//	return conn.isValid(timeout);
-	//}
+	public boolean isValid(int timeout) throws SQLException {
+		return conn.isValid(timeout);
+	}
 
-	//public void setClientInfo(Properties properties)
-	//		throws SQLClientInfoException {
-	//	conn.setClientInfo(properties);
-	//}
+	public void setClientInfo(Properties properties)
+			throws SQLClientInfoException {
+		conn.setClientInfo(properties);
+	}
 
-	//public void setClientInfo(String name, String value)
-	//		throws SQLClientInfoException {
-	//	conn.setClientInfo(name, value);
-	//}
+	public void setClientInfo(String name, String value)
+			throws SQLClientInfoException {
+		conn.setClientInfo(name, value);
+	}
 
-	//public boolean isWrapperFor(Class<?> iface) throws SQLException {
-	//	return conn.isWrapperFor(iface);
-	//}
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return conn.isWrapperFor(iface);
+	}
 
-	//public <T> T unwrap(Class<T> iface) throws SQLException {
-	//	return conn.unwrap(iface);
-	//}
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return conn.unwrap(iface);
+	}
 }
