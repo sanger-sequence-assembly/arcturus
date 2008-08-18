62f42f517a32153d36bfe59285be8f3448030502
David Harper <adh@sanger.ac.uk>
2008-08-18 13:35:22 +0000

    Added code to copy bridge info to the clipboard.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/java@3017 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/gui/scaffold/BridgeInfoPanel.java
+++ src/uk/ac/sanger/arcturus/gui/scaffold/BridgeInfoPanel.java
@@ -5,13 +5,12 @@ import java.awt.*;
 import uk.ac.sanger.arcturus.data.*;
 import uk.ac.sanger.arcturus.scaffold.*;
 import uk.ac.sanger.arcturus.gui.genericdisplay.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
 
-public class BridgeInfoPanel extends GenericInfoPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -1878138391298888882L;
-
+public class BridgeInfoPanel extends GenericInfoPanel implements ClipboardOwner {
 	public BridgeInfoPanel(PopupManager myparent) {
 		super(myparent);
 		labels = null;
@@ -65,5 +64,32 @@ public class BridgeInfoPanel extends GenericInfoPanel {
 
 		for (int k = 0; k < templates.length; k++)
 			lines[k + 3] = templates[k].getName();
+		
+		copyToClipboard(bf);
 	}
+	
+	protected void copyToClipboard(BridgeFeature bf) {
+		StringBuffer sb = new StringBuffer();
+		
+		int leftContigID = ((Contig)bf.getLeftContigFeature().getClientObject()).getID();
+		int rightContigID = ((Contig)bf.getRightContigFeature().getClientObject()).getID();
+		
+		sb.append("BRIDGE between contig " + leftContigID + " and " + rightContigID + "\n");
+		
+		for (int i = 1; i < lines.length; i++) {
+			sb.append(lines[i]);
+			sb.append("\n");
+		}
+		
+		String str = sb.toString();
+		
+		System.err.println("Copying to clipboard:\n" + str);
+		
+		StringSelection contents = new StringSelection(str);
+
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(contents,
+				this);
+	}
+	
+	public void lostOwnership(Clipboard clipboard, Transferable contents) {};
 }
