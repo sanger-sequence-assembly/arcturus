54a6c9153d3dcfb32c10a04dad64c6434f2fffaf
(no author) <(no author)>
2008-08-20 13:29:41 +0000

    This commit was manufactured by cvs2svn to create tag 'Before_Major_Changes_To_User_Management'.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/tags/Before_Major_Changes_To_User_Management@3021 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/sql/current-contigs-to-cutoff-date.sql
+++
@@ -1,5 +0,0 @@
-select distinct CA.contig_id,CA.gap4name,CA.nreads,CA.ncntgs,CA.length,CA.created,CA.updated
-   from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
-   on (CA.contig_id = C2CMAPPING.parent_id and C2CMAPPING.contig_id = CB.contig_id)
-   where CA.created < @cutoff
-     and (C2CMAPPING.parent_id is null or CB.created > @cutoff)
