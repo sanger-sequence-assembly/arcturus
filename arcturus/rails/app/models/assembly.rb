21a4b833bfca6af6b6a667e625acabe666d82b2f
Ed Zuiderwijk <ejz@sanger.ac.uk>
2009-06-15 13:26:20 +0000

    first version of Assembly class with db queries
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3339 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/rails/app/models/assembly.rb
+++ arcturus/rails/app/models/assembly.rb
@@ -1,10 +1,45 @@
 class Assembly < ArcturusDatabase
-  has_many :projects
+  has_many :project
 
   set_table_name 'ASSEMBLY'
   self.primary_key = "assembly_id"
 
-  # def to_s
-  #   self.name
-  # end
+  def projects
+      @query = "select project_id from PROJECT where assembly_id = #{assembly_id} order by project_id"
+  end
+
+  def projects_summary
+      @query = "select count(*) as project_count from PROJECT where assembly_id = #{assembly_id}"
+      connection.select_all(@query).first
+# total = Project.find(:assembly_id).sum(&:1) ???
+  end
+
+  def current_contigs_summary
+      @query = "select sum(length) as total_length, count(*) as contig_count, " +
+               "sum(nreads) as read_count from CURRENTCONTIGS,PROJECT,ASSEMBLY " +
+               "where CURRENTCONTIGS.project_id = PROJECT.project_id " + 
+               "and PROJECT.assembly_id = #{assembly_id}"
+      connection.select_all(@query).first
+  end
+
+  def to_s
+      self.name
+  end
+
+  def to_puts
+    puts "status of Assembly instance #{self}"
+    puts "name          #{self.name}"
+    puts "chromosome    #{self.chromosome}"
+    puts "size          #{self.size}"
+    puts "progress      #{self.progress}"
+    puts "comment      '#{self.comment}'"    
+    puts "created       #{self.created}"
+    puts "creator       #{self.creator}"
+    puts "updated       #{self.updated}"
+  end
+
+  def before_create
+      self.created ||= Time.now
+  end 
+
 end
