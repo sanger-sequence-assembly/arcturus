b6e455dca7c9b329effc361f5554be433d3efe66
David Harper <adh@sanger.ac.uk>
2009-07-06 13:47:35 +0000

    Added preliminary versions of unit tests.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3406 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/rails/test/unit/project_test.rb
+++ arcturus/rails/test/unit/project_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  set_fixture_class :ASSEMBLY => Assembly,
+                    :PROJECT  => Project
+
+  fixtures :ASSEMBLY, :PROJECT
+
+  def setup
+    @default_assembly = ASSEMBLY(:default_assembly)
+  end
+
+  test "find default project" do
+    assert_not_nil(Project.find_by_name('default_project'))
+  end
+
+  test "unique project name" do
+    project = Project.new(:name => 'default_project',
+                          :assembly => @default_assembly,
+                          :creator => 'ejz',
+                          :created => Time.now)
+
+    assert_raise(ActiveRecord::StatementInvalid) {
+      project.save
+    }
+  end
+
+  test "valid assembly id" do
+    project = Project.new(:name => 'new_project',
+                          :assembly_id => 0,
+                          :creator => 'ejz',
+                          :created => Time.now)
+
+    assert !project.save
   end
 end
