0303d5e83fc05518c34fd00957f2e438d52231c6
David Harper <adh@sanger.ac.uk>
2009-07-06 14:14:21 +0000

    Added skeletons for contig tag testing.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3407 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/rails/test/unit/contig_tag_test.rb
+++ arcturus/rails/test/unit/contig_tag_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class ContigTagTest < ActiveSupport::TestCase
+  set_fixture_class :CONTIGTAG   => ContigTag,
+                    :TAG2CONTIG  => TagMapping,
+                    :ASSEMBLY    => Assembly,
+                    :PROJECT     => Project,
+                    :CONTIG      => Contig
+
+  fixtures :CONTIG, :CONTIGTAG, :TAG2CONTIG
+
   # Replace this with your real tests.
   test "the truth" do
     assert true
