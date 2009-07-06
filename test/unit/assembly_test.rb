344fb4cab0637ec9cc889ae1f64594c94ee821e9
David Harper <adh@sanger.ac.uk>
2009-07-06 13:47:35 +0000

    Added preliminary versions of unit tests.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/arcturus/rails@3406 69345dfe-1762-0410-a7d4-b9dc18918749

--- test/unit/assembly_test.rb
+++ test/unit/assembly_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class AssemblyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  set_fixture_class :ASSEMBLY => Assembly
+  fixtures :ASSEMBLY
+
+  test "find default assembly" do
+    assert_not_nil(Assembly.find_by_name('default_assembly'))
+  end
+
+  test "unique assembly name" do
+    assembly = Assembly.new(:name => 'default_assembly',
+                            :creator => 'ejz',
+                            :created => Time.now)
+
+    assert_raise(ActiveRecord::StatementInvalid) {
+      assembly.save
+    }
   end
 end
--- test/unit/contig_test.rb
+++ test/unit/contig_test.rb
@@ -1,8 +1,46 @@
 require 'test_helper'
 
 class ContigTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  set_fixture_class :ASSEMBLY => Assembly,
+                    :PROJECT  => Project,
+                    :CONTIG   => Contig
+
+  fixtures :ASSEMBLY, :PROJECT, :CONTIG
+
+  def setup
+    @default_project  = PROJECT(:default_project)
+    @bin_project      = PROJECT(:bin)
+  end
+
+  test "find default contig" do
+    contig = Contig.find_by_gap4name('default_contig')
+    assert_not_nil(contig)
+    assert_equal contig.project_id, @default_project.project_id
+  end
+
+  test "valid project id" do
+    contig = Contig.new(:gap4name   => 'new_contig',
+                        :project_id => 0,
+                        :created    => Time.now)
+
+    assert !contig.save
+  end
+
+  test "set project to bin" do
+    contig = Contig.find_by_gap4name('default_contig')
+    assert_not_nil(contig)
+    contig.project = @bin_project
+    assert_equal contig.project_id, @bin_project.project_id
+    assert contig.save
+    contig.reload
+    assert_equal contig.project_id, @bin_project.project_id
+  end
+
+  test "cannot delete parent project" do
+    contig = Contig.find_by_gap4name('default_contig')
+    assert_not_nil(contig)
+    assert_raise(ActiveRecord::StatementInvalid) {
+      contig.project.delete
+    }
   end
 end
