007135bc64691f6b78e0b39b3d1875336fd5214a
David Harper <adh@sanger.ac.uk>
2009-07-06 15:15:59 +0000

    Added tests and fixture data for contig tags and mappings.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3408 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/rails/test/unit/tag_mapping_test.rb
+++ arcturus/rails/test/unit/tag_mapping_test.rb
@@ -8,8 +8,22 @@ class TagMappingTest < ActiveSupport::TestCase
                     :CONTIG      => Contig
 
   fixtures :CONTIG, :CONTIGTAG, :TAG2CONTIG
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    @default_contig  = CONTIG(:default_contig)
+    @default_tag     = CONTIGTAG(:default_tag)
+    @test_tag_2      = CONTIGTAG(:test_tag_2)
+    @test_tag_3      = CONTIGTAG(:test_tag_3)
+  end
+
+  test "create tag mapping" do
+    mapping = TagMapping.new
+    mapping.contig = @default_contig
+    mapping.tag    = @default_tag
+    mapping.cstart = 1
+    mapping.cfinal = 100
+    mapping.strand = "F"
+
+    assert mapping.save
   end
 end
