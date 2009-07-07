c1121b96906a961a17788723d9751ae0f656f6cd
David Harper <adh@sanger.ac.uk>
2009-07-07 15:46:50 +0000

    Added contig tag as resource in routes table. Added "show" for tag mapping. Added preliminary functional tests for he project controller.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3413 69345dfe-1762-0410-a7d4-b9dc18918749

--- arcturus/rails/test/functional/projects_controller_test.rb
+++ arcturus/rails/test/functional/projects_controller_test.rb
@@ -7,45 +7,49 @@ class ProjectsControllerTest < ActionController::TestCase
 
   fixtures :ASSEMBLY, :PROJECT, :CONTIG
 
+  def setup
+    @request_parameters = {:instance => 'testing', :organism => 'TESTDB_ADH'}
+  end
+
   test "should get index" do
-    get :index
+    get :index, @request_parameters
     assert_response :success
     assert_not_nil assigns(:projects)
   end
 
   test "should get new" do
-    get :new
+    get :new, @request_parameters
     assert_response :success
   end
 
-  test "should create project" do
-    assert_difference('Project.count') do
-      post :create, :project => { }
-    end
+#  test "should create project" do
+#    assert_difference('Project.count') do
+#      post :create, :project => { }
+#    end
 
-    assert_redirected_to project_path(assigns(:project))
-  end
+#    assert_redirected_to project_path(assigns(:project))
+#  end
 
-  test "should show project" do
-    get :show, :id => projects(:one).to_param
-    assert_response :success
-  end
+#  test "should show project" do
+#    get :show, :id => projects(:one).to_param
+#    assert_response :success
+#  end
 
-  test "should get edit" do
-    get :edit, :id => projects(:one).to_param
-    assert_response :success
-  end
+#  test "should get edit" do
+#    get :edit, :id => projects(:one).to_param
+#    assert_response :success
+#  end
 
-  test "should update project" do
-    put :update, :id => projects(:one).to_param, :project => { }
-    assert_redirected_to project_path(assigns(:project))
-  end
+#  test "should update project" do
+#    put :update, :id => projects(:one).to_param, :project => { }
+#    assert_redirected_to project_path(assigns(:project))
+#  end
 
-  test "should destroy project" do
-    assert_difference('Project.count', -1) do
-      delete :destroy, :id => projects(:one).to_param
-    end
+#  test "should destroy project" do
+#    assert_difference('Project.count', -1) do
+#      delete :destroy, :id => projects(:one).to_param
+#    end
 
-    assert_redirected_to projects_path
-  end
+#    assert_redirected_to projects_path
+#  end
 end
