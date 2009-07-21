2b3db7306073a0b0fc39abd08b6e69c2550bfa0b
David Harper <adh@sanger.ac.uk>
2009-07-21 06:19:24 +0000

    Moved class-specific initialisations from environment.rb to class files and added class-specific YML configuration files.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/adh@3472 69345dfe-1762-0410-a7d4-b9dc18918749

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,12 +1,20 @@
+require 'net/ldap'
+
 class User < ActiveRecord::Base
   set_table_name 'USER'
   self.primary_key = "username"
 
+  config = YAML::load(File.open("#{RAILS_ROOT}/config/ldap/user.yml"))
+
+  @@ldap = Net::LDAP.new(:host => config['host'], :port => config['port'], :base => config['base'])
+
+  @@base = config['base']
+
   def full_name
     if @my_full_name.nil?
-      @filter = Net::LDAP::Filter.eq("uid", username)
+      filter = Net::LDAP::Filter.eq("uid", username)
 
-      @entry =  LDAP.search(:base => LDAP_PEOPLE_BASE, :filter => @filter).first
+      @entry =  @@ldap.search(:base => @@base, :filter => filter).first
 
       if @entry.nil?
         @my_full_name = "(Name unknown)"
