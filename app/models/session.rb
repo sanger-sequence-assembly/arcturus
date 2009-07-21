df0c0def0c961e0ac74463e784b5ddabfabd97f9
David Harper <adh@sanger.ac.uk>
2009-07-21 11:22:13 +0000

    Merged r3480 from branches/adh into trunk
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/rails@3481 69345dfe-1762-0410-a7d4-b9dc18918749

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,8 @@
 class Session < ActiveRecord::Base
-  Session.establish_connection(DatabaseConnectionManager.get_database_parameters('test', 'arcturus'))
+  config = YAML::load(File.open("#{RAILS_ROOT}/config/models/session.yml"))[RAILS_ENV]
+  Session.establish_connection(
+    DatabaseConnectionManager.get_database_parameters(config['instance'], config['database'], false))
+
   set_table_name 'USER'
   self.primary_key = "username"
 end
