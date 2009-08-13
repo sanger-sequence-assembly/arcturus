630f8c14ff3636427d86543f849071c0d3023acd
David Harper <adh@sanger.ac.uk>
2009-08-13 08:54:54 +0000

    Fixed non-existent attribute bug for type=pool and added -terse option to suppress output of sequence and trace info.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3536 69345dfe-1762-0410-a7d4-b9dc18918749

--- test/ITSdump.pl
+++ test/ITSdump.pl
@@ -3,6 +3,48 @@
 use strict;
 use TraceServer;
 
+my $terse = 0;
+
+if (scalar(@ARGV) > 0 && $ARGV[0] eq '-terse') {
+    shift @ARGV;
+    $terse = 1;
+}
+
+# Connect
+my $ts;
+eval {
+    $ts = TraceServer->new(TS_DIRECT, TS_READ_ONLY, "");
+};
+die if ($@);
+
+$| = 1; # autoflush
+
+my $nargs = scalar(@ARGV);
+
+# Iterate through arguments
+
+foreach (@ARGV) {
+    seq_dump($ts, $_);
+}
+
+# Iterate over names read from stdin, unless names were given on
+# command line
+
+if ($nargs == 0) {
+    while (my $line = <STDIN>) {
+	my ($readname) = $line =~ /\s*(\S+)\s*/;
+	next unless defined($readname);
+	seq_dump($ts, $readname);
+    }
+}
+
+# Disconnect
+undef $ts;
+
+exit(0);
+
+# -----------------------------------------------------------------------------
+
 # Dumps out all pertinent information about a trace
 sub seq_dump {
     my ($ts, $name) = @_;
@@ -72,18 +114,25 @@ sub seq_dump {
 	} else {
 	    print "$s    Parent     = (none)\n";
 	}
-	if ($src->get_type() eq "clone") {
+
+	my $type = $src->get_type();
+
+	if ($type eq "clone") {
 	    print "$s    - l.name   = ", $src->get_library_name(), "\n";
 	    print "$s    - size     = ", $src->get_library_size(), "\n";
 	    print "$s    - stddev   = ", $src->get_library_stddev(), "\n";
 	    print "$s    - vector   = ", $src->get_library_vector(), "\n";
 	    print "$s    - is_trans = ", $src->get_library_is_transposon(), "\n";
-	} else {
+	} elsif ($type eq 'pcr') {
 	    print "$s    - p.name   = ", $src->get_pcr_name(), "\n";
 	    print "$s    - desc     = ", $src->get_pcr_description(), "\n";
 	    print "$s    - est.size = ", $src->get_pcr_size(), "\n";
 	    print "$s    - fwd      = ", $src->get_pcr_fwd_primer(), "\n";
 	    print "$s    - rev      = ", $src->get_pcr_rev_primer(), "\n";
+	} elsif ($type eq 'pool') {
+	    print "$s    - DO NOT KNOW WHAT ATTRIBUTES TO SHOW FOR TYPE $type\n";
+	} else {
+	    print "$s    - DO NOT KNOW WHAT ATTRIBUTES TO SHOW FOR TYPE $type\n";	    
 	}
 
 	$src = $parent;
@@ -135,23 +184,27 @@ sub seq_dump {
 	    print "        start = ", $c->get_start(), "\n";
 	    print "        end   = ", $c->get_end(), "\n";
 	}
-    
-	if (defined($seq)) {
-	    print "    Sequence   = ", $seq->get_dna, "\n";
-	} else {
-	    print "    Sequence   = (undef)\n";
-	}
-	if (defined($conf)) {
-	    my @pcon = unpack("C*", $conf->get_phred());
-	    print "    Confidence = @pcon\n";
-	} else {
-	    print "    Confidence = (undef)\n";
-	}
-	if (defined($pos)) {
-	    my $values = $pos->get_positions();
-	    print "    Positions  = @$values\n";
-	} else {
-	    print "    Positions  = (undef)\n";
+
+	unless ($terse) {
+	    if (defined($seq)) {
+		print "    Sequence   = ", $seq->get_dna, "\n";
+	    } else {
+		print "    Sequence   = (undef)\n";
+	    }
+
+	    if (defined($conf)) {
+		my @pcon = unpack("C*", $conf->get_phred());
+		print "    Confidence = @pcon\n";
+	    } else {
+		print "    Confidence = (undef)\n";
+	    }
+
+	    if (defined($pos)) {
+		my $values = $pos->get_positions();
+		print "    Positions  = @$values\n";
+	    } else {
+		print "    Positions  = (undef)\n";
+	    }
 	}
     }
     
@@ -163,42 +216,17 @@ sub seq_dump {
 	print "    Trace obj  = $trace\n";
 	print "    Trace type = $type\n";
 	print "    Trace fmt  = ", $trace->get_format(), "\n";
-	
-	my $exp = $trace->get_data("EXP");
-	if (defined($exp))  {
-	    print "    Trace data = (defined)\n";
-	    print "    EXP File   = \n$exp\n";
-	} else {
-	    print "    Trace data = (undef)\n";
+
+	unless ($terse) {
+	    my $exp = $trace->get_data("EXP");
+	    if (defined($exp))  {
+		print "    Trace data = (defined)\n";
+		print "    EXP File   = \n$exp\n";
+	    } else {
+		print "    Trace data = (undef)\n";
+	    }
 	}
     }
     
     undef $read;
 }
-
-# -----------------------------------------------------------------------------
-
-# Connect
-my $ts;
-eval {
-    $ts = TraceServer->new(TS_DIRECT, TS_READ_ONLY, "");
-};
-die if ($@);
-
-$| = 1; # autoflush
-
-# Iterate through arguments
-foreach (@ARGV) {
-    seq_dump($ts, $_);
-}
-
-# Iterate over names read from stdin
-while (my $line = <STDIN>) {
-    my ($readname) = $line =~ /\s*(\S+)\s*/;
-    next unless defined($readname);
-    seq_dump($ts, $readname);
-}
-
-# Disconnect
-undef $ts;
-
