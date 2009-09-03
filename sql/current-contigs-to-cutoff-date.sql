c32e7168009b7dabb75fb0fbe39936f3127a83ed
David Harper <adh@sanger.ac.uk>
2009-09-03 09:49:36 +0000

    Added an SQL script to add tables to store padding information.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3564 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/current-contigs-to-cutoff-date.sql
+++ sql/current-contigs-to-cutoff-date.sql
@@ -1,5 +1,9 @@
-select distinct CA.contig_id,CA.gap4name,CA.nreads,CA.ncntgs,CA.length,CA.created,CA.updated
-   from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
-   on (CA.contig_id = C2CMAPPING.parent_id and C2CMAPPING.contig_id = CB.contig_id)
-   where CA.created < @cutoff
-     and (C2CMAPPING.parent_id is null or CB.created > @cutoff)
+select CA.contig_id,PA.name,CA.nreads,CA.length,CA.created,CB.contig_id,PB.name,CB.nreads,CB.length,CB.created
+   from CONTIG as CA left join (C2CMAPPING M,CONTIG as CB, PROJECT PA,PROJECT PB)
+   on (CA.contig_id = M.parent_id and M.contig_id = CB.contig_id)
+   where CA.created < '2009-08-21'
+   and (M.parent_id is null or CB.created > '2009-08-21')
+   and CA.project_id=PA.project_id
+   and CB.project_id=PB.project_id
+   and PA.name like 'BIG2P_'
+   order by PA.name asc,CA.length desc,CB.length desc
