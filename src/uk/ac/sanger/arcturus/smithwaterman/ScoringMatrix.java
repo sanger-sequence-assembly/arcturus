eff86dade78e141106e1fa8892e2319b5d70e6e8
David Harper <adh@sanger.ac.uk>
2009-10-01 11:19:11 +0000

    Added constants to define standard scores/penalties.  Renamed penalty getter methods.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/java@3605 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/smithwaterman/ScoringMatrix.java
+++ src/uk/ac/sanger/arcturus/smithwaterman/ScoringMatrix.java
@@ -1,27 +1,42 @@
 package uk.ac.sanger.arcturus.smithwaterman;
 
 public class ScoringMatrix {
-    protected final int scoreMatch;
-    protected final int scoreMismatch;
-    protected final int scoreGapInit;
-    protected final int scoreGapExtend;
+	public static final int DEFAULT_MATCH_SCORE = 1;
+	public static final int DEFAULT_MISMATCH_PENALTY = -2;
+	public static final int DEFAULT_GAP_INIT_PENALTY = -4;
+	public static final int DEFAULT_GAP_EXTEND_PENALTY = -3;
 
-    public ScoringMatrix(int scoreMatch, int scoreMismatch, int scoreGapInit, int scoreGapExtend) {
-	this.scoreMatch = scoreMatch;
-	this.scoreMismatch = scoreMismatch;
-	this.scoreGapInit = scoreGapInit;
-	this.scoreGapExtend = scoreGapExtend;
-    }
+	protected final int scoreMatch;
+	protected final int penaltyMismatch;
+	protected final int penaltyGapInit;
+	protected final int penaltyGapExtend;
 
-    public ScoringMatrix() {
-	this(1, -2, -4, -3);
-    }
+	public ScoringMatrix(int scoreMatch, int penaltyMismatch, int penaltyGapInit,
+			int penaltyGapExtend) {
+		this.scoreMatch = scoreMatch;
+		this.penaltyMismatch = penaltyMismatch;
+		this.penaltyGapInit = penaltyGapInit;
+		this.penaltyGapExtend = penaltyGapExtend;
+	}
 
-    public int getMatchScore() { return scoreMatch; }
+	public ScoringMatrix() {
+		this(DEFAULT_MATCH_SCORE, DEFAULT_MISMATCH_PENALTY,
+				DEFAULT_GAP_INIT_PENALTY, DEFAULT_GAP_EXTEND_PENALTY);
+	}
 
-    public int getMismatchScore() { return scoreMismatch; }
+	public int getMatchScore() {
+		return scoreMatch;
+	}
 
-    public int getGapInitScore() { return scoreGapInit; }
+	public int getMismatchPenalty() {
+		return penaltyMismatch;
+	}
 
-    public int getGapExtendScore() { return scoreGapExtend; }
+	public int getGapInitPenalty() {
+		return penaltyGapInit;
+	}
+
+	public int getGapExtendPenalty() {
+		return penaltyGapExtend;
+	}
 }
