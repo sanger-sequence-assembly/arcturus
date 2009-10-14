84e3df97beb882ad68c7ad8579705493cffe440c
David Harper <adh@sanger.ac.uk>
2009-10-14 15:22:30 +0000

    Removed the loop and added code to generate a chained exception.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/java@3630 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/logging/LogTester.java
+++ src/uk/ac/sanger/arcturus/logging/LogTester.java
@@ -5,38 +5,95 @@ import java.util.logging.*;
 
 public class LogTester {
 	public static void main(String[] args) {
+		// Explicitly set the testing flag to prevent the logging system
+		// from instantiating a MailHandler.
+		System.setProperty("testing", "true");
+
 		Arcturus.logWarning("Starting");
 
-		for (int j = 0; j < 3; j++) {
-			try {
-				Thread.sleep(3000L);
-			} catch (InterruptedException ie) {
-				Arcturus.logWarning("Sleep interrupted");
-			}
-
-			Arcturus.logWarning("This is a warning");
-
-			try {
-				Thread.sleep(3000L);
-			} catch (InterruptedException ie) {
-				Arcturus.logWarning("Sleep interrupted");
-			}
-
-			try {
-				doSomething(0);
-			} catch (Exception e) {
-				Arcturus.log(Level.SEVERE, "exception", e);
-			}
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException ie) {
+			Arcturus.logWarning("Sleep interrupted");
 		}
-		
+
+		Arcturus.logWarning("This is a warning");
+
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException ie) {
+			Arcturus.logWarning("Sleep interrupted");
+		}
+
+		try {
+			doSomething(0);
+		} catch (Exception e) {
+			Arcturus.log(Level.SEVERE, "exception", e);
+		}
+
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException ie) {
+			Arcturus.logWarning("Sleep interrupted");
+		}
+
+		try {
+			createNestedExceptions();
+		} catch (HighLevelException e) {
+			Arcturus.logSevere(e);
+		}
+
 		System.exit(0);
 	}
-	
+
+	static void createNestedExceptions() throws HighLevelException {
+		try {
+			b();
+		} catch (MidLevelException e) {
+			throw new HighLevelException(e);
+		}
+	}
+
+	static void b() throws MidLevelException {
+		c();
+	}
+
+	static void c() throws MidLevelException {
+		try {
+			d();
+		} catch (LowLevelException e) {
+			throw new MidLevelException(e);
+		}
+	}
+
+	static void d() throws LowLevelException {
+		e();
+	}
+
+	static void e() throws LowLevelException {
+		throw new LowLevelException();
+	}
+
 	public static void doSomething(int i) throws Exception {
 		if (i < 5)
-			doSomething(i+1);
+			doSomething(i + 1);
 		else
 			throw new Exception("Something bad happened");
 	}
 
+}
+
+class HighLevelException extends Exception {
+	HighLevelException(Throwable cause) {
+		super(cause);
+	}
+}
+
+class MidLevelException extends Exception {
+	MidLevelException(Throwable cause) {
+		super(cause);
+	}
+}
+
+class LowLevelException extends Exception {
 }
