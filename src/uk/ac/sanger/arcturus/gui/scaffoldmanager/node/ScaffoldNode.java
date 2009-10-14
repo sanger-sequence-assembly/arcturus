b417d800b1d4aafa5f038c2e5b3cd0db448682a7
David Harper <adh@sanger.ac.uk>
2009-10-14 09:42:30 +0000

    Updated code to display the projects represented by each scaffold, with the total consensus length of each.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk/java@3623 69345dfe-1762-0410-a7d4-b9dc18918749

--- src/uk/ac/sanger/arcturus/gui/scaffoldmanager/node/ScaffoldNode.java
+++ src/uk/ac/sanger/arcturus/gui/scaffoldmanager/node/ScaffoldNode.java
@@ -1,14 +1,26 @@
 package uk.ac.sanger.arcturus.gui.scaffoldmanager.node;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
+
+import uk.ac.sanger.arcturus.data.Contig;
+import uk.ac.sanger.arcturus.data.Project;
 
 public class ScaffoldNode extends DefaultMutableTreeNode {
 	private MutableTreeNode lastNode = null;
 	private int length = 0;
 	private int contigs = 0;
+	private int reads = 0;
 	private boolean forward;
 	
+	private Map<Project, Integer> projectWeightByReads = new HashMap<Project, Integer>();
+	private Map<Project, Integer> projectWeightByLength = new HashMap<Project, Integer>();
+
 	public ScaffoldNode(boolean forward) {
 		this.forward = forward;
 	}
@@ -16,13 +28,12 @@ public class ScaffoldNode extends DefaultMutableTreeNode {
 	public void add(MutableTreeNode node) {
 		if (node instanceof ContigNode) {
 			if (lastNode == null || lastNode instanceof GapNode) {
-				length += ((ContigNode)node).getContig().getLength();
-				contigs++;
+				addContigNode((ContigNode)node);
 			} else
 				throw new IllegalArgumentException("Cannot add a ContigNode at this point.");
 		} else if (node instanceof GapNode) {
 			if (lastNode != null && lastNode instanceof ContigNode) {
-				length += ((GapNode)node).length();
+				addGapNode((GapNode)node);
 			} else
 				throw new IllegalArgumentException("Cannot add a GapNode at this point.");
 		} else
@@ -31,6 +42,34 @@ public class ScaffoldNode extends DefaultMutableTreeNode {
 		super.add(node);
 		
 		lastNode = node;
+	}
+	
+	private void addContigNode(ContigNode node) {
+		Contig contig = node.getContig();
+		Project project = contig.getProject();
+		
+		int contigLength = contig.getLength();
+		int contigReads = contig.getReadCount();
+		
+		length += contigLength;
+		reads += contigReads;
+		contigs++;
+		
+		incrementMapEntry(projectWeightByReads, project, contigReads);
+		
+		incrementMapEntry(projectWeightByLength, project, contigLength);
+	}
+	
+	private void incrementMapEntry(Map<Project, Integer> map, Project project, int delta) {
+		Integer oldValue = map.get(project);
+		
+		int newValue = (oldValue == null) ? delta : oldValue + delta;
+		
+		map.put(project, newValue);
+	}
+	
+	private void addGapNode(GapNode node) {
+		length += node.length();
 	}
 	
 	public int length() {
@@ -45,7 +84,94 @@ public class ScaffoldNode extends DefaultMutableTreeNode {
 		return forward;
 	}
 	
+	public Map<Project, Float> getProjectWeightsByReads() {
+		return getProjectWeights(projectWeightByReads);
+	}
+	
+	public Map<Project, Float> getProjectWeightsByLength() {
+		return getProjectWeights(projectWeightByLength);
+	}
+	
+	private Map<Project, Float> getProjectWeights(Map<Project, Integer> map) {
+		int total = 0;
+		
+		for (Map.Entry<Project, Integer> entry : map.entrySet())
+			total += entry.getValue();
+
+		float ftotal = (float)total;
+		
+		Map<Project, Float> result = new HashMap<Project, Float>();
+		
+		for (Map.Entry<Project, Integer> entry : map.entrySet()) {
+			float frac = (float)entry.getValue() / ftotal;
+			
+			result.put(entry.getKey(), frac);
+		}
+		
+		return result;
+	}
+	
+	class ProjectAndValue implements Comparable<ProjectAndValue> {
+		private Project project;
+		private int value;
+		
+		public ProjectAndValue(Map.Entry<Project, Integer> entry) {
+			this.project = entry.getKey();
+			this.value = entry.getValue();
+		}
+
+		public int compareTo(ProjectAndValue that) {
+			return that.value - this.value;
+		}
+		
+		public Project getProject() {
+			return project;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+	}
+	
+	private String getProjectWeightsString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(" [");
+		
+		ProjectAndValue[] array;
+		
+		synchronized (projectWeightByLength) {
+			array = new ProjectAndValue[projectWeightByLength.size()];
+		
+			int i = 0;
+		
+			for (Map.Entry<Project, Integer> entry : projectWeightByLength.entrySet())
+				array[i++] = new ProjectAndValue(entry);
+		}
+			
+		Arrays.sort(array);
+		
+		for (int i = 0; i < array.length; i++) {
+			Project project = array[i].getProject();
+			int value = array[i].getValue();
+			
+			if (i > 0)
+				sb.append(", ");
+			
+			sb.append(project.getName() + " : " + value + " bp");
+		}
+		
+		sb.append("]");
+		
+		return sb.toString();
+	}
+	
+	private String cachedToString = null;
+
 	public String toString() {
-		return "Scaffold of " + contigs + " contigs, " + length + " bp";
+		if (cachedToString == null)
+			cachedToString =  "Scaffold of " + contigs + " contigs, " + length + " bp" + getProjectWeightsString();
+		
+		return cachedToString;
 	}
 }
