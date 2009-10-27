38b1cbf7beabdc5994cefabe98339e5f73280d54
David Harper <adh@sanger.ac.uk>
2009-10-27 17:06:42 +0000

    Added option to specify LDAP URL on the command line.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3657 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/test/TestLDAPSocketFactory.java
+++ java/src/uk/ac/sanger/arcturus/test/TestLDAPSocketFactory.java
@@ -13,6 +13,11 @@ public class TestLDAPSocketFactory {
 	public static void main(String[] args) {
 		Properties props = Arcturus.getProperties();
 		
+		String serverName = System.getProperty("java.naming.provider.url",
+				"ldap://ldapfoo.internal.sanger.ac.uk/cn=jdbc,ou=arcturus,ou=projects,dc=sanger,dc=ac,dc=uk");
+		
+		props.put("java.naming.provider.url",serverName);
+
 		props.put("java.naming.ldap.factory.socket", "uk.ac.sanger.arcturus.utils.LDAPSocketFactory");
 		
 		LDAPSocketFactory factory = (LDAPSocketFactory) LDAPSocketFactory.getDefault();		
