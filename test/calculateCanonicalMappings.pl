79f5f12fd5c74d6e3ec862740b6dacfa96fd7b29
David Harper <adh@sanger.ac.uk>
2009-12-01 14:16:59 +0000

    Added preliminary code to populate the new canonical mappings tables
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3696 69345dfe-1762-0410-a7d4-b9dc18918749

--- test/calculateCanonicalMappings.pl
+++ test/calculateCanonicalMappings.pl
@@ -4,7 +4,7 @@ use strict;
 
 use DBI;
 use Compress::Zlib;
-use Digest::MD5 qw(md5_hex);
+use Digest::MD5 qw(md5 md5_hex);
 
 use DataSource;
 
@@ -18,8 +18,6 @@ while (my $nextword = shift @ARGV) {
     $instance      = shift @ARGV if ($nextword eq '-instance');
     $organism      = shift @ARGV if ($nextword eq '-organism');
 
-    $seqid         = shift @ARGV if ($nextword eq '-seqid');
-
     $limit         = shift @ARGV if ($nextword eq '-limit');
 
     if ($nextword eq '-help') {
@@ -28,14 +26,14 @@ while (my $nextword = shift @ARGV) {
     }
 }
 
-unless (defined($instance) && defined($organism) && defined($seqid)) {
+unless (defined($instance) && defined($organism)) {
     &showUsage("One or more mandatory parameters missing");
     exit(1);
 }
 
 my $ds = new DataSource(-instance => $instance, -organism => $organism);
 
-my $dbh = $ds->getConnection();
+my $dbh = $ds->getConnection(-options => { RaiseError => 1, PrintError => 1});
 
 unless (defined($dbh)) {
     print STDERR "Failed to connect to DataSource(instance=$instance, organism=$organism)\n";
@@ -44,7 +42,7 @@ unless (defined($dbh)) {
     die "getConnection failed";
 }
 
-my $query = "select contig_id,mapping_id,direction from MAPPING where seq_id = ? order by contig_id asc";
+my $query = "select contig_id,seq_id,mapping_id,direction from MAPPING order by contig_id asc";
 
 $query .= " limit $limit" if defined($limit);
 
@@ -56,12 +54,34 @@ $query = "select cstart,rstart,length from SEGMENT where mapping_id = ? order by
 my $sth_get_segments =  $dbh->prepare($query);
 &db_die("prepare($query) failed");
 
-$sth_get_mappings->execute($seqid);
+$query = "select mapping_id from CANONICALMAPPING where checksum = ?";
 
-while (my ($contigid,$mappingid,$direction) = $sth_get_mappings->fetchrow_array()) {
-    print "Contig $contigid (mapping $mappingid) $direction\n\n";
+my $sth_find_canonical_mapping =  $dbh->prepare($query);
+&db_die("prepare($query) failed");
+
+$query = "insert into SEQ2CONTIG(contig_id,seq_id,mapping_id,direction,coffset,roffset) value (?,?,?,?,?,?)";
+
+my $sth_insert_seq_to_contig =  $dbh->prepare($query);
+&db_die("prepare($query) failed");
+
+$query = "insert into CANONICALMAPPING(cspan,rspan,checksum) values (?,?,?)";
+
+my $sth_insert_canonical_mapping =  $dbh->prepare($query);
+&db_die("prepare($query) failed");
+
+$query = "insert into CANONICALSEGMENT(mapping_id,cstart,rstart,length) values (?,?,?,?)";
+
+my $sth_insert_canonical_segment =  $dbh->prepare($query);
+&db_die("prepare($query) failed");
+
+$sth_get_mappings->execute();
+
+while (my ($contigid,$seqid,$mappingid,$direction) = $sth_get_mappings->fetchrow_array()) {
+    #print "Contig $contigid (mapping $mappingid) $direction\n\n";
 
     my $forward = $direction eq 'Forward';
+
+    my $dirn = $forward ? 'F' : 'R';
 
     $sth_get_segments->execute($mappingid);
 
@@ -70,32 +90,32 @@ while (my ($contigid,$mappingid,$direction) = $sth_get_mappings->fetchrow_array(
 
     my @segments;
 
-    print "OLD SCHEMA\n\n";
+    #print "OLD SCHEMA\n\n";
 
-    printf "%8s %8s %8s\n","cstart","rstart","length";
-    print "-------- -------- --------\n";
+    #printf "%8s %8s %8s\n","cstart","rstart","length";
+    #print "-------- -------- --------\n";
 
     while (my ($cstart,$rstart,$seglen) = $sth_get_segments->fetchrow_array()) {
-	printf "%8d %8d %8d\n", $cstart, $rstart, $seglen;
+	#printf "%8d %8d %8d\n", $cstart, $rstart, $seglen;
 
 	($cstart,$rstart) = ($cstart+$seglen-1, $rstart-$seglen+1) unless $forward;	
 
 	push @segments, [$cstart,$rstart,$seglen];
     }
 
-    print "\n";
+    #print "\n";
 
     my ($cs0, $rs0, $dummy) = @{$segments[0]};
 
     my $coffset = $forward ? $cs0 - 1 : $cs0 + 1;
     my $roffset = $rs0 - 1;
 
-    print "NEW SCHEMA\n\n";
+    #print "NEW SCHEMA\n\n";
 
-    print "coffset = $coffset\nroffset = $roffset\n\n";
+    #print "coffset = $coffset\nroffset = $roffset\n\n";
 
-    printf "%8s %8s %8s\n","cstart","rstart","length";
-    print "-------- -------- --------\n";
+    #printf "%8s %8s %8s\n","cstart","rstart","length";
+    #print "-------- -------- --------\n";
 
     my $signature = '';
 
@@ -105,15 +125,45 @@ while (my ($contigid,$mappingid,$direction) = $sth_get_mappings->fetchrow_array(
 	my $czero = $forward ? $cstart - $coffset : $coffset - $cstart;
 	my $rzero = $rstart - $roffset;
 
-	printf "%8d %8d %8d\n", $czero, $rzero, $seglen;
+	#printf "%8d %8d %8d\n", $czero, $rzero, $seglen;
 
 	$signature .= ':' if ($cstart != $cs0);
 
 	$signature .= "$czero,$rzero,$seglen";
     }
 
-    print "\nHash is ",md5_hex($signature),"\n";
-    print "\n-----------------------------------------\n\n";
+    my $cspan = 0;
+    my $rspan = 0;
+
+    my $sighash_hex = md5_hex($signature);
+
+    #print "\nHash is $sighash\n";
+    #print "\n-----------------------------------------\n\n";
+
+    printf "%8d %8d %8d %8d %1s %32s %s\n",$contigid,$seqid,$coffset,$roffset,$dirn,$sighash_hex,$signature;
+
+    my $sighash = md5($signature);
+
+    $dbh->begin_work();
+
+    $sth_find_canonical_mapping->execute($sighash);
+
+    my ($new_mapping_id) = $sth_find_canonical_mapping->fetchrow_array();
+
+    unless (defined($new_mapping_id)) {
+	$sth_insert_canonical_mapping->execute($cspan, $rspan, $sighash);
+
+	$new_mapping_id = $dbh->{'mysql_insertid'};
+
+	foreach my $segment (@segments) {
+	    my ($cstart,$rstart,$seglen) = @{$segment};
+	    $sth_insert_canonical_segment->execute($new_mapping_id, $cstart, $rstart, $seglen );
+	}
+    }
+
+    $sth_insert_seq_to_contig->execute($contigid, $seqid, $new_mapping_id, $direction, $coffset, $roffset);
+
+    $dbh->commit();
 }
 
 $sth_get_mappings->finish();
@@ -135,7 +185,6 @@ sub showUsage {
     print STDERR "\n";
     print STDERR "-instance\tName of instance\n";
     print STDERR "-organism\tName of organism\n";
-    print STDERR "-seqid\t\tSequence ID\n";
     print STDERR "\n";
     print STDERR "OPTIONAL PARAMETERS:\n";
     print STDERR "\n";
