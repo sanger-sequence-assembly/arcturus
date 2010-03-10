3835363ed4bb9b0e076bb119d58df1015ba0697f
David Harper <adh@sanger.ac.uk>
2010-03-10 13:04:44 +0000

    Added code to print the project, length and read count for contigs exported in FASTA format.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3834 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/gui/contigtable/ContigTable.java
+++ java/src/uk/ac/sanger/arcturus/gui/contigtable/ContigTable.java
@@ -12,6 +12,7 @@ import uk.ac.sanger.arcturus.test.CAFWriter;
 import uk.ac.sanger.arcturus.Arcturus;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
 import uk.ac.sanger.arcturus.data.Contig;
+import uk.ac.sanger.arcturus.data.Project;
 
 public class ContigTable extends SortableTable {
 	public final static int BY_ROW_NUMBER = 1;
@@ -106,7 +107,10 @@ public class ContigTable extends SortableTable {
 					if (i > 0)
 						ps.print('\n');
 					
-					ps.println(">contig" + df.format(contig.getID()));
+					Project project = contig.getProject();
+					
+					ps.println(">contig" + df.format(contig.getID()) + (project != null ? " project=" + project.getName() : "") +
+							" length=" + contig.getLength() + " reads=" + contig.getReadCount());
 					
 					for (int j = 0; j < dna.length; j += 50) {
 						int sublen = (j + 50 < dna.length) ? 50 : dna.length - j;
