fb7f899be9af8472111dc319bee9a646800c9a14
David Harper <adh@sanger.ac.uk>
2010-04-26 13:51:57 +0000

    Added preliminary version of a class which finds free capillary reads which are siblings (from the same sub-clone) as reads in current contigs in a specified project.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3917 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ java/src/uk/ac/sanger/arcturus/siblingreadfinder/SiblingReadFinder.java
@@ -0,0 +1,141 @@
+package uk.ac.sanger.arcturus.siblingreadfinder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import uk.ac.sanger.arcturus.Arcturus;
+import uk.ac.sanger.arcturus.data.Project;
+import uk.ac.sanger.arcturus.database.ArcturusDatabase;
+import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
+
+public class SiblingReadFinder {
+	protected ArcturusDatabase adb;
+	protected Connection conn;
+	
+	private final int CONNECTION_VALIDATION_TIMEOUT = 10;
+	
+	private static final String SQL_LIST_TEMPLATES_FOR_PROJECT =
+		"select distinct RI.template_id" +
+    	" from ((CURRENTCONTIGS CC left join MAPPING M using(contig_id))" +
+    	" left join SEQ2READ using (seq_id)) left join READINFO RI using (read_id)" +
+    	" where CC.project_id = ? and RI.asped is not null and RI.template_id > 0";
+	
+	private static final String SQL_LIST_READS_FOR_TEMPLATE =
+		"select readname from READINFO where template_id = ?";
+	
+	private static final String SQL_CURRENT_CONTIG_FOR_READ =
+		"select CC.contig_id from READINFO RI left join" +
+		" (SEQ2READ SR, MAPPING M, CURRENTCONTIGS CC) using (read_id)" +
+		" where RI.readname = ? and SR.seq_id=M.seq_id and M.contig_id=CC.contig_id";
+
+	protected PreparedStatement pstmtListTemplatesForProject;
+	protected PreparedStatement pstmtListReadsForTemplate;
+	protected PreparedStatement pstmtCurrentContigForRead;
+	
+	private void checkConnection() throws SQLException, ArcturusDatabaseException {
+		if (conn != null && conn.isValid(CONNECTION_VALIDATION_TIMEOUT))
+			return;
+		
+		if (conn != null) {
+			Arcturus.logInfo("ReadFinder: connection was invalid, obtaining a new one");
+			conn.close();
+		}
+		
+		prepareConnection();
+	}
+
+	private void prepareConnection() throws SQLException, ArcturusDatabaseException {
+		conn = adb.getPooledConnection(this);
+
+		pstmtListTemplatesForProject = conn.prepareStatement(SQL_LIST_TEMPLATES_FOR_PROJECT);
+
+		pstmtListReadsForTemplate = conn.prepareStatement(SQL_LIST_READS_FOR_TEMPLATE);
+
+		pstmtCurrentContigForRead = conn.prepareStatement(SQL_CURRENT_CONTIG_FOR_READ);
+	}
+
+	public void close() throws ArcturusDatabaseException {
+		if (conn != null)
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new ArcturusDatabaseException(e,
+						"An error occurred when trying to close the ReadFinder's database connection", conn, adb);
+			}
+
+		conn = null;
+	}
+
+	protected void finalize() {
+		try {
+			close();
+		} catch (ArcturusDatabaseException e) {
+			Arcturus.logWarning("An error occurred when finalizing the ReadFinder", e);
+		}
+	}
+
+	public Set<String> getSiblingReadnames(Project project) throws ArcturusDatabaseException {
+		if (project == null)
+			return null;
+		
+		Set<String> names = new HashSet<String>();
+		
+		try {
+			checkConnection();
+			
+			Set<Integer> templateIDs = new HashSet<Integer>();
+			
+			pstmtListTemplatesForProject.setInt(1, project.getID());
+			
+			ResultSet rs = pstmtListTemplatesForProject.executeQuery();
+			
+			while (rs.next()) {
+				templateIDs.add(rs.getInt(1));
+			}
+			
+			rs.close();
+			
+			Set<String> readnames = new HashSet<String>();
+			
+			for (int template_id : templateIDs) {
+				readnames.clear();
+				
+				pstmtListReadsForTemplate.setInt(1, template_id);
+				
+				rs = pstmtListReadsForTemplate.executeQuery();
+				
+				while (rs.next())
+					readnames.add(rs.getString(1));
+				
+				rs.close();
+				
+				for (String readname : readnames) {
+					if (isFree(readname))
+						names.add(readname);
+				}
+			}
+		}
+		catch (SQLException e) {
+			throw new ArcturusDatabaseException(e, conn);
+		}
+		
+		return names;
+	}
+	
+	private boolean isFree(String readname) throws SQLException {
+		pstmtCurrentContigForRead.setString(1, readname);
+		
+		ResultSet rs = pstmtCurrentContigForRead.executeQuery();
+		
+		boolean free = rs.next();
+		
+		rs.close();
+		
+		return free;
+	}
+}
