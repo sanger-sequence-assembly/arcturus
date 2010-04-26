b7a03f347c3f893acdfc9626143f2ffb8c0de063
Lars Jorgensen <lj3@sanger.ac.uk>
2010-04-26 17:26:41 +0000

    Added JUnit test task to build and skeleton test suite
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3918 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ java/src/uk/ac/sanger/arcturus/ArcturusTestSuite.java
@@ -0,0 +1,12 @@
+package uk.ac.sanger.arcturus;
+
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+public class ArcturusTestSuite extends TestSuite {
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		// TODO: Add tests
+		return suite;
+	}
+}
