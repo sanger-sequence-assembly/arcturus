49eed43ffb4f846d91680c92e1682762b0fe7865
Lars Jorgensen <lj3@sanger.ac.uk>
2010-04-27 23:48:26 +0000

    Moved test to src root and updated build.xml
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@3922 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/test/ArcturusTestSuite.java
+++ java/src/test/ArcturusTestSuite.java
@@ -1,12 +1,11 @@
-package uk.ac.sanger.arcturus;
+package test;
 
+import junit.framework.JUnit4TestAdapter;
 import junit.framework.TestSuite;
 import junit.framework.Test;
 
 public class ArcturusTestSuite extends TestSuite {
 	public static Test suite() {
-		TestSuite suite = new TestSuite();
-		// TODO: Add tests
-		return suite;
+         return new JUnit4TestAdapter(PersonTest.class);
 	}
 }
---
+++ java/src/test/PersonTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import junit.framework.JUnit4TestAdapter;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import uk.ac.sanger.arcturus.people.Person;
+
+
+public class PersonTest {
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(PersonTest.class);
+    }
+
+
+    @Test
+    public void testEqual() {
+        Person person1 = new Person("23");
+        Person person2 = new Person("24");
+        assertEquals(false, person1.equals(person2));
+    }
+}
