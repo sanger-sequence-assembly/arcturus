c1d4d4c163c63707762dcd10b810884509b1545b
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-05-07 15:26:56 +0000

    Rewrite as sub class of Alignment
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@3969 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/data/AssembledFrom.java
+++ java/src/uk/ac/sanger/arcturus/data/AssembledFrom.java
@@ -2,95 +2,30 @@ package uk.ac.sanger.arcturus.data;
 
 import uk.ac.sanger.arcturus.data.ReadToContigMapping.Direction;
 
-public class AssembledFrom implements Comparable<AssembledFrom> {
-	protected Range readRange;
-	protected Range contigRange;
+public class AssembledFrom extends Alignment {
 	
 	public AssembledFrom(Range contigRange, Range readRange) throws IllegalArgumentException {
+        super(contigRange,readRange);
 		if (readRange == null)
-			throw new IllegalArgumentException("Read range cannot be null");
+			throw new IllegalArgumentException("read range cannot be null");
 		
 		if (contigRange == null)
-			throw new IllegalArgumentException("Contig range cannot be null");
+			throw new IllegalArgumentException("contig  range cannot be null");
 		
 		if (readRange.getDirection() == Direction.REVERSE)
-			throw new IllegalArgumentException("Read range cannot be reveresed");
-		
-		this.readRange = readRange;
-		this.contigRange = contigRange;
+			throw new IllegalArgumentException("read range cannot be reversed");
 	}
 	
 	public Range getReadRange() {
-		return readRange;
+		return getSubjectRange();
 	}
 	
 	public Range getContigRange() {
-		return contigRange;
+		return getReferenceRange();
 	}
 
-	public int compareTo(AssembledFrom that) {
-		return this.readRange.getStart() - that.readRange.getStart();
+	public String toString() {
+	    return "Assembled_from " + getContigRange().toString() + " " + getReadRange().toString();
 	}
-	
-	public static Direction getDirection(AssembledFrom[] afdata) {
-		// If no segments, we can't infer a direction.
-		if (afdata == null || afdata.length == 0)
-			return Direction.UNKNOWN;
-		
-		// Try to infer direction from one of the segments.
-		for (int i = 0; i < afdata.length; i++) {
-			if (afdata[i] != null) {
-				Direction dir = afdata[i].getDirection();
-				if (dir != Direction.UNKNOWN)
-					return dir;
-			}
-		}
-		
-		// All of the segments are apparently single-base or null.
-		// Try to find two non-null segments and infer the direction
-		// from them.
-		AssembledFrom seg1 = null;
-		AssembledFrom seg2 = null;
-		
-		for (int i = 0; i < afdata.length; i++) {
-			if (afdata[i] != null) {
-				if (seg1 == null)
-					seg1 = afdata[i];
-				else if (seg2 == null) {
-					seg2 = afdata[i];
-					break;
-				}
-			}
-		}
-		
-		// We failed to find two non-null segments.
-		if (seg1 == null || seg2 == null)
-			return Direction.UNKNOWN;
-		
-		int readDelta = seg1.getReadRange().getStart() - seg2.getReadRange().getStart();
-		int contigDelta = seg1.getContigRange().getStart() - seg2.getContigRange().getStart();
-		
-		int signum = readDelta * contigDelta;
-		
-		if (signum > 0)
-			return Direction.FORWARD;
-		else if (signum < 0)
-			return Direction.REVERSE;
-		else
-			return Direction.UNKNOWN;
-	}
-	
-	public Direction getDirection() {
-		if (readRange == null || contigRange == null)
-			return Direction.UNKNOWN;
-		
-		Direction readDirection = readRange.getDirection();
-		
-		Direction contigDirection = contigRange.getDirection();
-	
-		if (readDirection == Direction.UNKNOWN || contigDirection == Direction.UNKNOWN)
-			return Direction.UNKNOWN;
-		
-		return readDirection == contigDirection ? Direction.FORWARD : Direction.REVERSE;
-	}
-}
+}	
+
