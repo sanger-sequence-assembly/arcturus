a96b9609a4375709dd129c8c98c0e064fbf753ea
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-05-07 15:34:01 +0000

    Heavily canabalized version, soon to be deleted.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@3972 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/data/CanonicalReadToContigMapping.java
+++ java/src/uk/ac/sanger/arcturus/data/CanonicalReadToContigMapping.java
@@ -2,7 +2,10 @@ package uk.ac.sanger.arcturus.data;
 
 import java.util.Arrays;
 
-public class CanonicalReadToContigMapping implements ReadToContigMapping, Comparable<CanonicalReadToContigMapping> {
+import uk.ac.sanger.arcturus.data.Utility;
+
+
+public class CanonicalReadToContigMapping {
 	protected Contig contig;
 	protected Sequence sequence;
 	protected int contigOffset;
@@ -11,58 +14,6 @@ public class CanonicalReadToContigMapping implements ReadToContigMapping, Compar
 	protected CanonicalMapping mapping;
 	protected Range contigRange;
 
-	public CanonicalReadToContigMapping(Contig contig, Sequence sequence,
-			int contigOffset, int readOffset, Direction direction,
-			CanonicalMapping mapping) {
-		this.contig = contig;
-		this.sequence = sequence;
-		this.contigOffset = contigOffset;
-		this.readOffset = readOffset;
-		this.direction = direction;
-		this.mapping = mapping;
-		calculateContigRange();
-	}
-	
-	public CanonicalReadToContigMapping(Contig contig, Sequence sequence, AssembledFrom[] afdata) {
-		this.contig = contig;
-		this.sequence = sequence;
-
-		Arrays.sort(afdata);
-		
-		direction = AssembledFrom.getDirection(afdata);
-		
-		readOffset = afdata[0].getReadRange().getStart() - 1;
-		
-		contigOffset = afdata[0].getContigRange().getStart();
-		
-		if (direction == Direction.FORWARD)
-			contigOffset -= 1;
-		else
-			contigOffset += 1;
-		
-		CanonicalSegment[] segments = new CanonicalSegment[afdata.length];
-		
-		for (int i = 0; i < afdata.length; i++) {
-			AssembledFrom af = afdata[i];
-			
-			int rstart = af.getReadRange().getStart();
-			int cstart = af.getContigRange().getStart();
-			int length = af.getReadRange().getLength();
-			
-			rstart -= readOffset;
-			cstart -= contigOffset;
-			
-			if (direction == Direction.REVERSE)
-				cstart = -cstart;
-			
-			segments[i] = new CanonicalSegment(cstart, rstart, length);
-		}
-		
-		mapping = new CanonicalMapping(0, segments);
-
-		calculateContigRange();
-	}
-	
 	public CanonicalReadToContigMapping(Mapping mapping) {
 		this.contig = mapping.getContig();
 		this.sequence = mapping.getSequence();
@@ -102,42 +53,8 @@ public class CanonicalReadToContigMapping implements ReadToContigMapping, Compar
 	}
 
 	private void calculateContigRange() {
-		int cstart = isForward() ? contigOffset + 1 : contigOffset - mapping.getContigSpan();
-		int cfinish = isForward() ? contigOffset + mapping.getContigSpan() : contigOffset - 1;
-		
-		this.contigRange = new Range(cstart, cfinish);
 	}
 	
-	public AssembledFrom[] getAssembledFromRecords() {
-		CanonicalSegment[] segments = mapping.getSegments();
-		
-		int nSegments = segments.length;
-		
-		AssembledFrom[] afdata = new AssembledFrom[nSegments];
-		
-		boolean forward = isForward();
-		
-		for (int i = 0; i < nSegments; i++) {
-			CanonicalSegment segment = segments[i];
-			
-			int length = segment.getLength();
-			
-			int readStart = readOffset + segment.getReadStart();
-			int readFinish = readStart + length - 1;
-			
-			int contigStart = forward ? contigOffset + segment.getContigStart() : contigOffset - segment.getContigStart();
-			int contigFinish = forward ? contigStart + length - 1 : contigStart - (length - 1); 
-			
-			Range readRange = new Range(readStart, readFinish);
-			Range contigRange = new Range(contigStart, contigFinish);
-			
-			afdata[i] = new AssembledFrom(contigRange, readRange);
-		}
-		
-		Arrays.sort(afdata);
-		
-		return afdata;
-	}
 
 	public BaseWithQuality getBaseAndQualityByContigPosition(int cpos) {
 		if (!contigRange.contains(cpos) || sequence == null || sequence.getDNA() == null || sequence.getQuality() == null)
@@ -178,42 +95,11 @@ public class CanonicalReadToContigMapping implements ReadToContigMapping, Compar
 		int qvalue = (int)(quality[rpos - 1]);
 		
 		if (!isForward())
-			base = complement(base);
+			base = Utility.complement(base);
 		
 		return new BaseWithQuality(base, qvalue);
 	}
-	
-	private char complement(char base) {
-		switch (base) {
-		case 'a':
-			return 't';
-			
-		case 'A':
-			return 'T';
-			
-		case 'c':
-			return 'g';
-			
-		case 'C':
-			return 'G';
-			
-		case 'g':
-			return 'c';
-			
-		case 'G':
-			return 'C';
-			
-		case 't':
-			return 'a';
-			
-		case 'T':
-			return 'A';
-			
-		default:
-			return base;
-		}
-	}
-	
+		
 	private BaseWithQuality getPadAndQualityByReadPosition(float rpos) {
 		if (rpos < 1 || rpos > sequence.getLength() - 1)
 			return null;
@@ -233,45 +119,3 @@ public class CanonicalReadToContigMapping implements ReadToContigMapping, Compar
 		
 		return new BaseWithQuality(BaseWithQuality.STAR, qvalue);
 	}
-	
-	public int getContigStartPosition() {
-		return contigRange.getStart();
-	}
-
-	public int getContigEndPosition() {
-		return contigRange.getEnd();
-	}
-
-	public Sequence getSequence() {
-		return sequence;
-	}
-
-	public boolean isForward() {
-		return direction == Direction.FORWARD;
-	}
-	
-	public Direction getDirection() {
-		return direction;
-	}
-
-	public Contig getContig() {
-		return contig;
-	}
-	
-	public int getReadOffset() {
-		return readOffset;
-	}
-	
-	public int getContigOffset() {
-		return contigOffset;
-	}
-	
-	public CanonicalMapping getCanonicalMapping() {
-		return mapping;
-	}
-
-	public int compareTo(CanonicalReadToContigMapping that) {
-		return this.getContigStartPosition() - that.getContigStartPosition();
-	}
-
-}
