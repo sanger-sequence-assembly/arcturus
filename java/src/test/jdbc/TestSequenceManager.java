b250211b8befd1155e3036ea3676c0e9f5a1641c
David Harper <adh@sanger.ac.uk>
2010-05-14 13:36:06 +0000

    Added a class for testing the sequence manager.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4005 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ java/src/test/jdbc/TestSequenceManager.java
@@ -0,0 +1,48 @@
+package test.jdbc;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
+
+import uk.ac.sanger.arcturus.data.Clipping;
+import uk.ac.sanger.arcturus.data.Sequence;
+
+import uk.ac.sanger.arcturus.database.ArcturusDatabase;
+import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
+
+public class TestSequenceManager extends TestBase {
+	@Test
+	public void storeSequence() throws ArcturusDatabaseException, UnsupportedEncodingException {
+		Sequence sequence = new Sequence(0, null);
+		
+		String dnastr = "ACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGT";
+		
+		byte[] dna = dnastr.getBytes("US-ASCII");
+		
+		assumeNotNull(dna);
+		
+		sequence.setDNA(dna);
+		
+		byte[] quality = new byte[dna.length];
+		
+		for (int i = 0; i < quality.length; i++)
+			quality[i] = (byte)2;
+		
+		sequence.setQuality(quality);
+		
+		Clipping clip = new Clipping(Clipping.QUAL, 10, 40);
+		
+		sequence.setQualityClipping(clip);
+		
+		clip = new Clipping(Clipping.SVEC, "SEC1234", 1, 20);
+		
+		sequence.setSequenceVectorClippingLeft(clip);
+
+		ArcturusDatabase adb = getArcturusDatabase();		
+	
+		int seq_id = adb.putSequence(sequence);
+	}
+}
