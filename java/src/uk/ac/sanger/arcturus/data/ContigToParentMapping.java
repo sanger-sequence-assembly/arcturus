3c59bfd55c687ce6125ceee1cf1caec55e2b913c
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-06-10 07:54:19 +0000

    Contig to parent mapping, new class
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4099 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ java/src/uk/ac/sanger/arcturus/data/ContigToParentMapping.java
@@ -0,0 +1,20 @@
+package uk.ac.sanger.arcturus.data;
+
+public class ContigToParentMapping extends GenericMapping<Contig,Contig> {
+	private int readCount;
+
+	public ContigToParentMapping(Contig contig, Contig parent, Alignment[] alignments) {
+	    super(contig,parent,alignments);
+	}
+	public ContigToParentMapping(Contig contig, Contig parent) {
+	    super(contig,parent);
+	}
+	
+    public void setReadCount(int count) {
+    	this.readCount = count;
+    }
+    
+    public int getReadCount() {
+        return readCount;
+    }
+}
