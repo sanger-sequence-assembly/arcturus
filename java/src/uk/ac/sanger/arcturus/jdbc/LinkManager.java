36b481b25da6dad9253afacb27841bedc0ca425a
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-06-10 14:36:20 +0000

    Refactored and added blocked retrieval; re-wrote queries
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4102 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
+++ java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
@@ -6,23 +6,33 @@ import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
 
 import uk.ac.sanger.arcturus.data.*;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
-//import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
 
 import java.sql.*;
 import java.util.*;
 
 public class LinkManager extends AbstractManager {
-	private HashMap<Checksum, Integer> cacheByReadName;
+	private Map<String, Integer> cacheByReadName;
 
 	protected PreparedStatement pstmtSelectReadNamesForCurrentContigs = null;
 	protected PreparedStatement pstmtSelectReadNamesForCurrentContigsAndProject = null;
 	protected PreparedStatement pstmtSelectCurrentContigForReadName = null;
-
+	
+    private static final int BLOCK_LIMIT = 100000;
+	
+	private static final String LINK_COLUMNS = "readname,flags,read_id,SEQ2CONTIG.contig_id";
+	private static final String LINK_TABLES = "(READNAME join " 
+	                                        +    "(SEQ2READ join "
+	                                        +       "(SEQ2CONTIG join CURRENTCONTIGS using (contig_id))"
+                                            +    " using (seq_id))"
+                                            + " using (read_id))";
+	private static final String SELECT_ALL = "select " + LINK_COLUMNS + " from " + LINK_TABLES;
+	private static final String RESTRICT_PROJECT = "project_id = ?";
+	private static final String RESTRICT_READ_ID = "read_id > ? order by read_id limit " + BLOCK_LIMIT;
 	
 	public LinkManager(ArcturusDatabase adb) throws ArcturusDatabaseException {
 		super(adb);
 
-		cacheByReadName = new HashMap<Checksum,Integer>();
+		cacheByReadName = new HashMap<String,Integer>();
 
 		try {
 			setConnection(adb.getDefaultConnection());
@@ -37,75 +47,84 @@ public class LinkManager extends AbstractManager {
 	}
 
 	protected void prepareConnection() throws SQLException {
-		String query;
-		
-		String subQuery = "select contig_id from CURRENTCONTIGS";
-		
-		query = "select readname,flags,contig_id"
-			  + "  from READNAME,SEQ2READ,SEQ2CONTIG"
-              + " where READNAME.read_id = SEQ2READ.read_id"
-              + "   and SEQ2READ.seq_id = SEQ2CONTIG.seq_id"
-              + "   and contig_id in (" + subQuery + ")";
-		pstmtSelectReadNamesForCurrentContigs = conn.prepareStatement(query);
-		
-		subQuery += " and project_id in ?";
-
-		query = "select readname,flags,contig_id"
-			  + "  from READNAME,SEQ2READ,SEQ2CONTIG"
-            + " where READNAME.read_id = SEQ2READ.read_id"
-            + "   and SEQ2READ.seq_id = SEQ2CONTIG.seq_id"
-            + "   and contig_id in (" + subQuery + ")";
-		pstmtSelectReadNamesForCurrentContigsAndProject = conn.prepareStatement(query);
-		
-        query = "select readname,flags,contig_id"	
-        	  + "  from (READNAME join SEQ2READ using (read_id)) join SEQ2CONTIG"; // FIX
-        pstmtSelectCurrentContigForReadName = conn.prepareStatement(query);
+		pstmtSelectReadNamesForCurrentContigs = 
+			conn.prepareStatement(SELECT_ALL + " where " + RESTRICT_READ_ID);
+		pstmtSelectReadNamesForCurrentContigsAndProject = 
+			conn.prepareStatement(SELECT_ALL + " where " + RESTRICT_PROJECT + " and " + RESTRICT_READ_ID);
+        pstmtSelectCurrentContigForReadName = 
+        	conn.prepareStatement(SELECT_ALL + " where readname = ?");
     }
 	
 	static final int FLAG_MASK= 128 + 64 + 1;
 
 	public void preload() throws ArcturusDatabaseException {
-	    preload(null);
+		clearCache();
+		int lastReadID = 0;
+
+		while (lastReadID >= 0) {
+//System.out.println("loading block starting at " + lastReadID);
+			try {
+	            pstmtSelectReadNamesForCurrentContigs.setInt(1,lastReadID);
+
+	            ResultSet rs = pstmtSelectReadNamesForCurrentContigs.executeQuery();
+
+				lastReadID = -1; // preset end loop
+	            while (rs.next()) {
+	        	    Read read = new Read(rs.getString(1),rs.getInt(2));
+	        	    lastReadID = rs.getInt(3); // activate one further pass through loop
+	        	    String readName = read.getUniqueName();
+	 		   	    cacheByReadName.put(readName,rs.getInt(4));
+			    }
+			    rs.close();				
+			}
+		    catch (SQLException e) {
+		        adb.handleSQLException(e,"Failed to build the read-contig cache", conn, adb);
+		        
+		    }
+		}
 	}
 		
 	public void preload(Project project) throws ArcturusDatabaseException {
 		clearCache();
-	    try {
-	    	ResultSet rs;
-	    	if (project == null) 
- 	            rs = pstmtSelectReadNamesForCurrentContigs.executeQuery();
-	    	else {
+		int lastReadID = 0;
+		while (lastReadID >= 0) {
+			try {
 	    		pstmtSelectReadNamesForCurrentContigsAndProject.setInt(1,project.getID());
-	    		rs = pstmtSelectReadNamesForCurrentContigsAndProject.executeQuery();
-	    	}
-		    
-            while (rs.next()) {
-        	    int flags = rs.getInt(2) & FLAG_MASK;
-        	    Read read = new Read(rs.getString(1),flags);
- 		   	    cacheByReadName.put(new Checksum(read.getUniqueName()),rs.getInt(3));
+	            pstmtSelectReadNamesForCurrentContigsAndProject.setInt(2,lastReadID);
+
+	            ResultSet rs = pstmtSelectReadNamesForCurrentContigsAndProject.executeQuery();
+
+				lastReadID = -1; // preset end loop
+	            while (rs.next()) {
+	        	    Read read = new Read(rs.getString(1),rs.getInt(2));
+	        	    lastReadID = rs.getInt(3);
+	        	    String readName = read.getUniqueName();
+	 		   	    cacheByReadName.put(readName,rs.getInt(4));
+			    }
+			    rs.close();				
+			}
+		    catch (SQLException e) {
+		        adb.handleSQLException(e,"Failed to build the read-contig cache", conn, adb);
+		        
 		    }
-		    rs.close();
-	    }
-	    catch (SQLException e) {
-	        adb.handleSQLException(e,"Failed to build the read-contig cache", conn, adb);
-	    }
+		}
 	}
 	
 	public int getCurrentContigIDForReadName(String readName) throws ArcturusDatabaseException {
 // returns contig_id for input read name      
-		Checksum checksum = new Checksum(readName);
-		if (cacheByReadName.containsKey(checksum))
-			return cacheByReadName.get(checksum);
+		if (cacheByReadName.containsKey(readName))
+			return cacheByReadName.get(readName);
+
 		else { // probe the database
 			try {
 		  	    pstmtSelectCurrentContigForReadName.setString(1,readName);
 			    ResultSet rs = pstmtSelectCurrentContigForReadName.executeQuery();
 			    
 			    if (rs.next()) {
-	        	    int flags = rs.getInt(2) & FLAG_MASK;
-	        	    Read read = new Read(rs.getString(1),flags);
+	        	    Read read = new Read(rs.getString(1),rs.getInt(2));
 	        	    int contig_id = rs.getInt(3);
-			   	    cacheByReadName.put(new Checksum(read.getUniqueName()),contig_id);
+	           	    String newReadName = read.getUniqueName();
+			   	    cacheByReadName.put(newReadName,contig_id);
 			   	    return contig_id;
 			    }
 			}
@@ -118,45 +137,10 @@ public class LinkManager extends AbstractManager {
 	}
 	
 	public String getCacheStatistics() {
-		return "ByID: " + cacheByReadName.size();
+		return "CurrentContigIDsByReadName: " + cacheByReadName.size();
 	}
-	
-/**
-* internal class provides hash key code for (long) readname-contig ID cache from read name
-*/
-		
-	class Checksum {
-		
-		byte[] data;
-		String name;
-			
-		Checksum(String readName) {
-			// constructor stores both readname and a hash of up to 16 bytes; 
-			this.name = readName;
-			if (readName.length() >= 16)
-			    this.data = Utility.calculateMD5Hash(readName);
-			else
-		        this.data = readName.getBytes();
-	    }
-			
-		public int hashCode() {	
-			if (data == null)
-				return 0;
-			int hashcode = 0;
-			for (int i = 1 ; i <= 4 ; i++) {
-				hashcode += data[i];
-				hashcode = hashcode << 3;
-			}
-			return hashcode;
-		}
-			
-		public boolean equals(Checksum that) {
-			if (this.data == null || that == null)
-				return false;
-			else if (this.name == that.name)
-				return true;
-			else 
-				return false;
-		}
+
+	public Set<String> getCacheKeys() {
+		return cacheByReadName.keySet();
 	}
 }
