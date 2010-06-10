2b174ed2e379650794f1d67f3d790f492f0265c6
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-06-10 14:24:19 +0000

    Added FLAG masking to getUniqueName method
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4100 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/data/Read.java
+++ java/src/uk/ac/sanger/arcturus/data/Read.java
@@ -32,8 +32,11 @@ public class Read extends Core {
 	public int getFlags() {
 		return flags;
 	}
+
+	static final int FLAG_MASK= 128 + 64 + 1;
 	
 	public String getUniqueName() {
-		return flags == 0 ? name : name + "/" + flags;
+		int masked_flags = flags & FLAG_MASK;
+		return masked_flags == 0 ? name : name + "/" + masked_flags;
 	}
 }
