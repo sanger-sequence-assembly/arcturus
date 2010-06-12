300ee02b371462e0fe4d8f001ed4330ee43d8330
Ed Zuiderwijk <ejz@sanger.ac.uk>
2010-06-12 15:47:44 +0000

    Made read preload optional (to be used when importing new assembly)
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4111 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/apps/ContigLoader.java
+++ java/src/uk/ac/sanger/arcturus/apps/ContigLoader.java
@@ -5,6 +5,7 @@ import java.io.*;
 import uk.ac.sanger.arcturus.data.*;
 import uk.ac.sanger.arcturus.samtools.BAMContigLoader;
 import uk.ac.sanger.arcturus.samtools.BAMReadLoader;
+import uk.ac.sanger.arcturus.Arcturus;
 import uk.ac.sanger.arcturus.ArcturusInstance;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
 //import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
@@ -15,10 +16,11 @@ public class ContigLoader {
 
 	public static void main (String[] args) {
 		File file = null;
-		String projectname = null;
-		String assemblyname = null;
+		String projectName = null;
+		String assemblyName = null;
 		String instance = null;
-		String organism = null; 
+		String organism = null;
+		boolean preloadReads = false;
 		
 	    for (int i = 0; i < args.length; i++) {
             if (args[i].equalsIgnoreCase("-in"))
@@ -28,65 +30,83 @@ public class ContigLoader {
             else if (args[i].equalsIgnoreCase("-organism"))
                 organism = args[++i];
             else if (args[i].equalsIgnoreCase("-projectname"))
-                projectname = args[++i];
+                projectName = args[++i];
             else if (args[i].equalsIgnoreCase("-assemblyname"))
-                assemblyname = args[++i];
+                assemblyName = args[++i];
+            else if (args[i].equalsIgnoreCase("-preload"))
+                preloadReads = true;
             else {
             	System.err.println("Invalid parameter " + args[i]);
     		    showUsage(System.err);
     		    System.exit(1);
-           }
+            }
  	    }
 
-	    if (file == null || instance == null || organism == null || projectname == null) {
+	    if (file == null || instance == null || organism == null) {
 		    showUsage(System.err);
 		    System.exit(1);
 		}
 	    
 	    try {
+System.out.println("Trying to get DB handle"); 	    	
  	        ArcturusInstance ai = ArcturusInstance.getInstance(instance);
  			ArcturusDatabase adb = ai.findArcturusDatabase(organism);
+if (adb != null)
+	System.out.println("DB opened");
  			
  // get the project, if specified
  			
  			Project project = null;
- 			if (projectname != null) {
+ 			if (projectName != null) {
  				Assembly assembly = null;
- 				if (assemblyname != null)
- 				    assembly = adb.getAssemblyByName(assemblyname);
-  			    project = adb.getProjectByName(assembly,projectname);
+ 				if (assemblyName != null)
+ 				    assembly = adb.getAssemblyByName(assemblyName);
+  			    project = adb.getProjectByName(assembly,projectName);
  			}
  			
  // open SAM file reader and test it
+ 			
+ System.out.println("Try to open BAM file " + file); 	    	
 
  			SAMFileReader.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);			
  			SAMFileReader reader = new SAMFileReader(file);
  	    	if (reader.isBinary() == false || reader.hasIndex() == false)
- 	    		throw new IllegalArgumentException("The input file is not indexed: " + file.toString());
+ 	    		throw new IllegalArgumentException("The input file is not indexed: " + file);
  	    	
 // set up read loader/tester and process the reads
 
  	    	BAMReadLoader brl = new BAMReadLoader(adb);
- 			brl.processFile(reader);
- 			
-// set up the contig loader and iterate through the contigs
-
- 			BAMContigLoader bcl = new BAMContigLoader(adb);	        
-	        bcl.processFile(reader,project);
+// in preload mode, load (missing) reads first, then iterate through contigs
+ 	    	if (preloadReads) {
+ 	    		
+ 			    brl.processFile(reader); 	    		    	
+ 			    BAMContigLoader bcl = new BAMContigLoader(adb);	        
+	            bcl.processFile(reader,project);
+ 	    	}
+ 	    	else {
+// load missing reads as and when they are identified by the contig loader
+	    	
+ 			    BAMContigLoader bcl = new BAMContigLoader(adb,brl);	        
+	            bcl.processFile(reader,project);
+ 	    	}
 	    }
 	    catch(Exception e) {
-	        System.err.println("Failed to initialise or execute the contig loader" + e);
+	    	Arcturus.logWarning("Failed to initialise or execute the contig loader", e);
+	    	System.exit(1);
 	    }
+    	System.exit(0);
 	}
 	
 	public static void showUsage(PrintStream ps) {
+		ps.println("Invalid or missing input parameters");
+		ps.println();
 		ps.println("MANDATORY PARAMETERS:");
 		ps.println("\t-instance\tName of instance");
 		ps.println("\t-organism\tName of organism");
 		ps.println("\t-bam\tName of ordered and indexed bam file");
 		ps.println();
 		ps.println("OPTIONAL PARAMETERS:");
-		ps.println("\t-project\tName of project (default: BIN)");		
-		ps.println("\t-lock\tAcquire the lock on the project");		
+		ps.println("\t-project\tName of project");		
+		ps.println("\t-preload\tDo a read import first");		
 	}
 }
