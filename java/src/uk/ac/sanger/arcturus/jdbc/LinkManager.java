54056e06956bcebd4996659746dd7a78c0e8ea55
David Harper <adh@sanger.ac.uk>
2010-06-15 11:48:25 +0000

    Re-factored the preload methods to remove duplicated code and clarify the logic.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4117 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
+++ java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
@@ -21,19 +21,31 @@ public class LinkManager extends AbstractManager {
     private static final int BLOCK_LIMIT = 100000;
 	
 	private static final String LINK_COLUMNS = "readname,flags,read_id,SEQ2CONTIG.contig_id";
+	
 	private static final String LINK_TABLES = "(READNAME join " 
 	                                        +    "(SEQ2READ left join "
 	                                        +       "(SEQ2CONTIG left join CURRENTCONTIGS using (contig_id))"
                                             +    " using (seq_id))"
                                             + " using (read_id))";
 	private static final String SELECT_ALL = "select " + LINK_COLUMNS + " from " + LINK_TABLES;
+	
 	private static final String RESTRICT_PROJECT = "project_id = ?";
+	
 	private static final String RESTRICT_READ_ID = "read_id > ? order by read_id limit " + BLOCK_LIMIT;
+	
+	private static final String GET_READ_NAMES_FOR_ALL_CURRENT_CONTIGS =
+		SELECT_ALL + " where " + RESTRICT_READ_ID;
+	
+	private static final String GET_READ_NAMES_FOR_CURRENT_CONTIGS_IN_PROJECT =
+		SELECT_ALL + " where " + RESTRICT_PROJECT + " and " + RESTRICT_READ_ID;
+	
+	private static final String GET_CURRENT_CONTIG_FOR_READ_NAME =
+		SELECT_ALL + " where readname = ?";
 	
 	public LinkManager(ArcturusDatabase adb) throws ArcturusDatabaseException {
 		super(adb);
 
-		cacheByReadName = new HashMap<String,Integer>();
+		cacheByReadName = new HashMap<String, Integer>();
 
 		try {
 			setConnection(adb.getDefaultConnection());
@@ -49,79 +61,67 @@ public class LinkManager extends AbstractManager {
 
 	protected void prepareConnection() throws SQLException {
 		pstmtSelectReadNamesForCurrentContigs = 
-			conn.prepareStatement(SELECT_ALL + " where " + RESTRICT_READ_ID);
+			conn.prepareStatement(GET_READ_NAMES_FOR_ALL_CURRENT_CONTIGS);
+		
 		pstmtSelectReadNamesForCurrentContigsAndProject = 
-			conn.prepareStatement(SELECT_ALL + " where " + RESTRICT_PROJECT + " and " + RESTRICT_READ_ID);
+			conn.prepareStatement(GET_READ_NAMES_FOR_CURRENT_CONTIGS_IN_PROJECT);
+		
         pstmtSelectCurrentContigForReadNameAndFlags = 
-        	conn.prepareStatement(SELECT_ALL + " where readname = ? and flags = ?");
+        	conn.prepareStatement(GET_CURRENT_CONTIG_FOR_READ_NAME);
     }
 
 	public void preload() throws ArcturusDatabaseException {
-		clearCache();
-		int lastReadID = 0;
-
-		while (lastReadID >= 0) {
-System.out.println("loading block starting at " + lastReadID);
-			try {
-	            pstmtSelectReadNamesForCurrentContigs.setInt(1,lastReadID);
-
-	            ResultSet rs = pstmtSelectReadNamesForCurrentContigs.executeQuery();
-
-				lastReadID = -1; // preset end outer loop
-	            while (rs.next()) {
-	        	    String readName = rs.getString(1);
-	    		    int maskedFlags = Utility.maskReadFlags(rs.getInt(2));
-	        	    Read read = new Read(readName,maskedFlags);
-	        	    lastReadID = rs.getInt(3); // activate one further pass through outer loop
-	 		   	    cacheByReadName.put(read.getUniqueName(),rs.getInt(4));
-//System.out.println("read " + readName + " flag " + maskedFlags + " last " + lastReadID + " contigID " + rs.getInt(4));	   	    
-			    }
-			    rs.close();				
-			}
-		    catch (SQLException e) {
-		        adb.handleSQLException(e,"Failed to build the LinkManager cache", conn, adb);
-		    }
-		}
+		preload(null);
 	}
 		
 	public void preload(Project project) throws ArcturusDatabaseException {
-
 		clearCache();
+		
+		PreparedStatement pstmt = project == null ? pstmtSelectReadNamesForCurrentContigs :
+			pstmtSelectReadNamesForCurrentContigsAndProject;
+		
+		boolean done = false;
 		int lastReadID = 0;
-		while (lastReadID >= 0) {
+		
+		while (!done) {
 			try {
-	    		pstmtSelectReadNamesForCurrentContigsAndProject.setInt(1,project.getID());
-	            pstmtSelectReadNamesForCurrentContigsAndProject.setInt(2,lastReadID);
+				if (project == null) {
+					pstmt.setInt(1, lastReadID);
+				} else {
+					pstmt.setInt(1, project.getID());
+	            	pstmt.setInt(2, lastReadID);
+				}
+			
+	            ResultSet rs = pstmt.executeQuery();
 
-	            ResultSet rs = pstmtSelectReadNamesForCurrentContigsAndProject.executeQuery();
+	            int count = 0;
 
-				lastReadID = -1; // preset end loop
 	            while (rs.next()) {
 	            	String readName = rs.getString(1);
 	    		    int maskedFlags = Utility.maskReadFlags(rs.getInt(2));
-	        	    Read read = new Read(readName,maskedFlags);
+	        	    Read read = new Read(readName, maskedFlags);
 	        	    lastReadID = rs.getInt(3);
-	 		   	    cacheByReadName.put(read.getUniqueName(),rs.getInt(4));
-//System.out.println("read " + readName + " flag " + maskedFlags + " last " + lastReadID + " contigID " + rs.getInt(4));	   	    
+	 		   	    cacheByReadName.put(read.getUniqueName(), rs.getInt(4));
+	 		   	    count++;
 			    }
+	            
 			    rs.close();				
+			    
+			    done = count == 0;
 			}
 		    catch (SQLException e) {
 		        adb.handleSQLException(e,"Failed to build the read-contig cache", conn, adb);    
 		    }
-		}
-		 
+		}		 
 	}
 	
+
 	public int getCurrentContigIDForRead(Read read) throws ArcturusDatabaseException {
-// returns contig_id for input read
-		
 		String uniqueReadName = read.getUniqueName();
+
 		if (cacheByReadName.containsKey(uniqueReadName))
 			return cacheByReadName.get(uniqueReadName);
-		
 		else {		
-//System.out.println("TRYing database again " + read.getName() + " flag " + read.getFlags());
 			try {
 		  	    pstmtSelectCurrentContigForReadNameAndFlags.setString(1,read.getName());
 		  	    pstmtSelectCurrentContigForReadNameAndFlags.setInt(2,read.getFlags());
@@ -139,7 +139,6 @@ System.out.println("loading block starting at " + lastReadID);
 				adb.handleSQLException(e,"Failed to test readname in database", conn, adb);
 			}
 		}
-// the read is not in the database
 		return -1;
 	}
 	
