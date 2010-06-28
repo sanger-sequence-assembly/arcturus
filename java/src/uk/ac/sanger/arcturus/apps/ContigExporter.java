fef16d948c50150c7e2d51d1d7df5b2852e2d54d
David Harper <adh@sanger.ac.uk>
2010-06-28 12:59:20 +0000

    Added option to export a set of contigs identified by their IDs.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4170 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/apps/ContigExporter.java
+++ java/src/uk/ac/sanger/arcturus/apps/ContigExporter.java
@@ -3,11 +3,14 @@ package uk.ac.sanger.arcturus.apps;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.naming.NamingException;
 
 import uk.ac.sanger.arcturus.ArcturusInstance;
 import uk.ac.sanger.arcturus.data.Assembly;
+import uk.ac.sanger.arcturus.data.Contig;
 import uk.ac.sanger.arcturus.data.Project;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
 import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
@@ -22,6 +25,7 @@ public class ContigExporter implements SAMContigExporterEventListener {
 		String projectName = null;
 		String outputFileName = null;
 		String assemblyName = null;
+		String contigList = null;
 		
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].equalsIgnoreCase("-instance"))
@@ -32,30 +36,56 @@ public class ContigExporter implements SAMContigExporterEventListener {
 				assemblyName = args[++i];
 			else if (args[i].equalsIgnoreCase("-project"))
 				projectName = args[++i];
+			else if (args[i].equalsIgnoreCase("-contigs"))
+				contigList = args[++i];
 			else if (args[i].equalsIgnoreCase("-out"))
 				outputFileName = args[++i];
 		}
 		
-		if (instanceName == null || organismName == null || projectName == null || outputFileName == null) {
+		if (instanceName == null 
+				|| organismName == null 
+				|| (projectName == null && contigList == null)
+				|| outputFileName == null) {
 			printUsage(System.err);
+			System.exit(1);
+		}
+		
+		if (projectName != null && contigList != null) {
+			System.err.println("You should only specify one of -project and -contigs");
 			System.exit(1);
 		}
 		
 	    try { 	
  	        ArcturusInstance ai = ArcturusInstance.getInstance(instanceName);
  			ArcturusDatabase adb = ai.findArcturusDatabase(organismName);
- 			
-  			Assembly assembly = assemblyName == null ? null : adb.getAssemblyByName(assemblyName);
-  			
-  			Project project = adb.getProjectByName(assembly,projectName);
-			
-			PrintWriter pw = new PrintWriter(outputFileName);
 			
 			SAMContigExporter exporter = new SAMContigExporter(adb);
 			
-			ContigExporter ce = new ContigExporter();
+			PrintWriter pw = new PrintWriter(outputFileName);
 			
-			ce.run(exporter, project, pw);
+			ContigExporter ce = new ContigExporter();
+		
+			if (projectName != null) {
+				Assembly assembly = assemblyName == null ? null : adb.getAssemblyByName(assemblyName);
+  			
+				Project project = adb.getProjectByName(assembly, projectName);
+			
+				ce.run(exporter, project, pw);
+			} else {
+				String[] words = contigList.split(",");
+				
+				Set<Contig> contigSet = new HashSet<Contig>(words.length);
+				
+				for (String word : words) {
+					int contig_id = Integer.parseInt(word);
+					
+					Contig contig = adb.getContigByID(contig_id, ArcturusDatabase.CONTIG_BASIC_DATA);
+					
+					contigSet.add(contig);
+				}
+				
+				ce.run(exporter, contigSet, pw);
+			}
 			
 			pw.close();
 		}
@@ -80,12 +110,22 @@ public class ContigExporter implements SAMContigExporterEventListener {
 		exporter.exportContigsForProject(project, pw);
 	}
 	
+	public void run(SAMContigExporter exporter, Set<Contig> contigSet, PrintWriter pw)
+		throws ArcturusDatabaseException {		
+		exporter.setSAMContigExporterEventListener(this);
+		
+		exporter.exportContigSet(contigSet, pw);
+	}
+
 	private static void printUsage(PrintStream ps) {
 		ps.println("MANDATORY PARAMETERS");
 		ps.println("\t-instance\tThe name of the instance");
 		ps.println("\t-organism\tThe name of the organism");
-		ps.println("\t-project\tThe name of the project to be exported");
 		ps.println("\t-out\t\tThe name of the SAM file to be written");
+		ps.println();
+		ps.println("MANDATORY EXCLUSIVE PARAMETERS");
+		ps.println("\t-project\tThe name of the project to be exported");
+		ps.println("\t-contigs\tA comma-separated list of contig IDs to be exported");
 		ps.println();
 		ps.println("OPTIONAL PARAMETERS");
 		ps.println("\t-assembly\tThe name of the assembly, to disambiguate projects");
