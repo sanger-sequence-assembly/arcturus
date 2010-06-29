dfce6ed8ef7cd636d64c9ed5f0d2bdcc48e05baf
David Harper <adh@sanger.ac.uk>
2010-06-29 14:09:56 +0000

    Defined a better SQL query for enumerating the read names in current contigs in a specific project, and modified the code to use the new query.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4175 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
+++ java/src/uk/ac/sanger/arcturus/jdbc/LinkManager.java
@@ -20,27 +20,30 @@ public class LinkManager extends AbstractManager {
 	
     private static final int BLOCK_LIMIT = 100000;
 	
-	private static final String LINK_COLUMNS = "readname,flags,read_id,SEQ2CONTIG.contig_id";
+	private static final String COLUMNS = "RN.readname,RN.flags,RN.read_id,SC.contig_id";
 	
-	private static final String LINK_TABLES = "(READNAME join " 
-	                                        +    "(SEQ2READ left join "
-	                                        +       "(SEQ2CONTIG left join CURRENTCONTIGS using (contig_id))"
+	private static final String TABLES_BY_READ = "(READNAME RN join " 
+	                                        +    "(SEQ2READ SR left join "
+	                                        +       "(SEQ2CONTIG SC left join CURRENTCONTIGS CC using (contig_id))"
                                             +    " using (seq_id))"
                                             + " using (read_id))";
-	private static final String SELECT_ALL = "select " + LINK_COLUMNS + " from " + LINK_TABLES;
 	
-	private static final String RESTRICT_PROJECT = "project_id = ?";
+	private static final String TABLES_BY_PROJECT = 
+		"(CURRENTCONTIGS CC left join SEQ2CONTIG SC using (contig_id)),SEQ2READ SR,READNAME RN" +
+			" where CC.project_id = ? and SC.seq_id=SR.seq_id and SR.read_id=RN.read_id";
 	
-	private static final String RESTRICT_READ_ID = "read_id > ? order by read_id limit " + BLOCK_LIMIT;
+	private static final String SELECT_BY_READ = "select " + COLUMNS + " from " + TABLES_BY_READ;
+	
+	private static final String RESTRICT_MINIMUM_READ_ID = "RN.read_id > ? order by RN.read_id limit " + BLOCK_LIMIT;
 	
 	private static final String GET_READ_NAMES_FOR_ALL_CURRENT_CONTIGS =
-		SELECT_ALL + " where " + RESTRICT_READ_ID;
+		SELECT_BY_READ + " where " + RESTRICT_MINIMUM_READ_ID;
 	
 	private static final String GET_READ_NAMES_FOR_CURRENT_CONTIGS_IN_PROJECT =
-		SELECT_ALL + " where " + RESTRICT_PROJECT + " and " + RESTRICT_READ_ID;
+		"select " + COLUMNS + " from " + TABLES_BY_PROJECT;
 	
 	private static final String GET_CURRENT_CONTIG_FOR_READ_NAME =
-		SELECT_ALL + " where readname = ?";
+		SELECT_BY_READ + " where readname = ?";
 	
 	public LinkManager(ArcturusDatabase adb) throws ArcturusDatabaseException {
 		super(adb);
@@ -85,12 +88,10 @@ public class LinkManager extends AbstractManager {
 		
 		while (!done) {
 			try {
-				if (project == null) {
+				if (project == null)
 					pstmt.setInt(1, lastReadID);
-				} else {
+				else
 					pstmt.setInt(1, project.getID());
-	            	pstmt.setInt(2, lastReadID);
-				}
 			
 	            ResultSet rs = pstmt.executeQuery();
 
@@ -99,16 +100,21 @@ public class LinkManager extends AbstractManager {
 	            while (rs.next()) {
 	            	String readName = rs.getString(1);
 	    		    int maskedFlags = Utility.maskReadFlags(rs.getInt(2));
+	    		    
 	        	    Read read = new Read(readName, maskedFlags);
+	        	    
 	        	    lastReadID = rs.getInt(3);
+	        	    
 	        	    Contig currentContig = adb.getContigByID(rs.getInt(4));
+	        	    
 	 		   	    cacheByReadName.put(read.getUniqueName(), currentContig);
+	 		   	    
 	 		   	    count++;
 			    }
 	            
 			    rs.close();				
 			    
-			    done = count == 0;
+			    done = count == 0 || project != null;
 			}
 		    catch (SQLException e) {
 		        adb.handleSQLException(e,"Failed to build the read-contig cache", conn, adb);    
