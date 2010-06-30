63984704ef6b711005fc9cc431e08e6bb11e7543
David Harper <adh@sanger.ac.uk>
2010-06-30 10:32:39 +0000

    Implemented code to approve or reject the import of a contig based upon a basic set of criteria.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4180 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/samtools/SimpleContigImportApprover.java
+++ java/src/uk/ac/sanger/arcturus/samtools/SimpleContigImportApprover.java
@@ -1,6 +1,7 @@
 package uk.ac.sanger.arcturus.samtools;
 
 import java.io.PrintStream;
+import java.util.Set;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
@@ -14,7 +15,42 @@ public class SimpleContigImportApprover implements ContigImportApprover {
 			SimpleDirectedWeightedGraph<Contig, DefaultWeightedEdge> graph,
 			Project targetProject, PrintStream reportStream)
 			throws ArcturusDatabaseException {
-		// TODO Auto-generated method stub
+        Set<Contig> vertices = graph.vertexSet();
+        
+        for (Contig contig : vertices) {
+            if (graph.inDegreeOf(contig) > 0)
+                if (!approveParentContig(contig, targetProject, reportStream))
+                	return false;
+        }
+
+		return false;
+	}
+
+	private boolean approveParentContig(Contig contig, Project targetProject, PrintStream reportStream) {
+		Project parentProject = contig.getProject();
+		
+		if (parentProject == null) {
+			reportStream.println("Parent " + contig + " has no project: DENY");
+			return false;
+		}
+		
+		if (parentProject.equals(targetProject)) {
+			reportStream.println("Parent " + contig + " is in target project: APPROVE");
+			return true;
+		}
+		
+		if (parentProject.isLocked()) {
+			reportStream.println("Parent " + contig + " is in a locked project: DENY");
+			return false;
+		}
+		
+		if (parentProject.isUnowned()) {
+			reportStream.println("Parent " + contig + " is in an unowned project: APPROVE");
+			return true;
+		}
+		
+		reportStream.println("Parent " + contig + " default action: DENY");
+		
 		return false;
 	}
 
