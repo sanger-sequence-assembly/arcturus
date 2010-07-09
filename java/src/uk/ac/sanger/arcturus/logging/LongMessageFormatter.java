dcb0bb802e90b01c019c8259752709be5239df8c
David Harper <adh@sanger.ac.uk>
2010-07-09 09:02:02 +0000

    Altered the formatter so that it returns only the log message if there is no associated throwable.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4213 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/logging/LongMessageFormatter.java
+++ java/src/uk/ac/sanger/arcturus/logging/LongMessageFormatter.java
@@ -11,55 +11,62 @@ public class LongMessageFormatter extends AbstractFormatter {
 	private final String SEPARATOR = "\n\n########################################################################\n\n";
 	
 	public String format(LogRecord record) {
-		StringBuffer sb = new StringBuffer(16384);
-		
-		sb.append("An Arcturus Java exception has occurred\n\n");
-		
-		String hostname = null;
-		
-		try {
-			hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			hostname = "[InetAddress.getLocalHost().getHostName() failed : " + e.getMessage() + "]";
-		}
-		
-		sb.append("Hostname: " + hostname + "\n\n");
-		
-		String revision = Arcturus.getProperty(Arcturus.BUILD_VERSION_KEY, "[NOT KNOWN]");
-		sb.append("Revision: " + revision + "\n\n");
-		
-		sb.append(record.getMessage() + "\n\n");
-		
-		sb.append("The logger is " + record.getLoggerName() + "\n");
-		sb.append("The sequence number is " + record.getSequenceNumber() + "\n");
-		sb.append("The level is " + record.getLevel().intValue() + "\n");
-		sb.append("The source class name is " + record.getSourceClassName() + "\n");
-		sb.append("The source method name is " + record.getSourceMethodName() + "\n");
-		
-		Date timestamp = new Date(record.getMillis());
-		
-		sb.append("The timestamp is " + timestamp + "\n");
-		
-		Throwable thrown = getUnderlyingException(record.getThrown());
-				
-		if (thrown != null) {
-			sb.append("\n----- PRIMARY EXCEPTION -----\n");
-			
-			displayThrowable(thrown, sb);
-			
-			Throwable cause = thrown.getCause();
-			
-			while (cause != null) {
-				sb.append("\n----- CHAINED EXCEPTION -----\n");
-				displayThrowable(cause, sb);
-				
-				cause = cause.getCause();
-			}
-		}
-		
-		sb.append(SEPARATOR);
+		if (record.getThrown() != null) {
+			StringBuffer sb = new StringBuffer(16384);
 
-		return sb.toString();
+			sb.append("An Arcturus Java exception has occurred\n\n");
+
+			String hostname = null;
+
+			try {
+				hostname = InetAddress.getLocalHost().getHostName();
+			} catch (UnknownHostException e) {
+				hostname = "[InetAddress.getLocalHost().getHostName() failed : "
+						+ e.getMessage() + "]";
+			}
+
+			sb.append("Hostname: " + hostname + "\n\n");
+
+			String revision = Arcturus.getProperty(Arcturus.BUILD_VERSION_KEY,
+					"[NOT KNOWN]");
+			sb.append("Revision: " + revision + "\n\n");
+
+			sb.append(record.getMessage() + "\n\n");
+
+			sb.append("The logger is " + record.getLoggerName() + "\n");
+			sb.append("The sequence number is " + record.getSequenceNumber()
+					+ "\n");
+			sb.append("The level is " + record.getLevel().intValue() + "\n");
+			sb.append("The source class name is " + record.getSourceClassName()
+					+ "\n");
+			sb.append("The source method name is "
+					+ record.getSourceMethodName() + "\n");
+
+			Date timestamp = new Date(record.getMillis());
+
+			sb.append("The timestamp is " + timestamp + "\n");
+
+			Throwable thrown = getUnderlyingException(record.getThrown());
+
+			if (thrown != null) {
+				sb.append("\n----- PRIMARY EXCEPTION -----\n");
+
+				displayThrowable(thrown, sb);
+
+				Throwable cause = thrown.getCause();
+
+				while (cause != null) {
+					sb.append("\n----- CHAINED EXCEPTION -----\n");
+					displayThrowable(cause, sb);
+
+					cause = cause.getCause();
+				}
+			}
+
+			sb.append(SEPARATOR);
+			return sb.toString();
+		} else
+			return record.getMessage() + "\n\n";
 	}
 
 }
