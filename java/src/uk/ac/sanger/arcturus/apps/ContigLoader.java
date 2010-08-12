7f08dd070dd8e45b6fbab61ac62fb433d408207a
David Harper <adh@sanger.ac.uk>
2010-08-12 14:51:26 +0000

    Added code to import contig consensus sequences from a FASTQ file.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4277 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/apps/ContigLoader.java
+++ java/src/uk/ac/sanger/arcturus/apps/ContigLoader.java
@@ -8,24 +8,35 @@ import uk.ac.sanger.arcturus.samtools.BAMReadLoader;
 import uk.ac.sanger.arcturus.Arcturus;
 import uk.ac.sanger.arcturus.ArcturusInstance;
 import uk.ac.sanger.arcturus.database.ArcturusDatabase;
+import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
+import uk.ac.sanger.arcturus.fasta.FastqFileReader;
+import uk.ac.sanger.arcturus.fasta.SequenceProcessor;
 //import uk.ac.sanger.arcturus.database.ArcturusDatabaseException;
 
 import net.sf.samtools.*;
 
 public class ContigLoader extends AbstractLoader {
-
 	public static void main (String[] args) {
-		File file = null;
+		ContigLoader loader = new ContigLoader();
+		
+		int rc = loader.run(args);
+		
+		System.exit(rc);
+	}
+	
+	public int run(String[] args) {
+		File bamFile = null;
 		String projectName = null;
 		String assemblyName = null;
 		String instance = null;
 		String organism = null;
 		boolean preloadReads = false;
 		String contigName = null;
+		File consensusFile = null;
 		
 	    for (int i = 0; i < args.length; i++) {
             if (args[i].equalsIgnoreCase("-in"))
-                file = new File(args[++i]);
+                bamFile = new File(args[++i]);
             else if (args[i].equalsIgnoreCase("-instance"))
                 instance = args[++i];
             else if (args[i].equalsIgnoreCase("-organism"))
@@ -38,24 +49,24 @@ public class ContigLoader extends AbstractLoader {
             	contigName = args[++i];
             else if (args[i].equalsIgnoreCase("-preloadreads"))
                 preloadReads = true;
+            else if (args[i].equalsIgnoreCase("-consensus"))
+            	consensusFile = new File(args[++i]);
             else {
             	System.err.println("Invalid parameter " + args[i]);
     		    showUsage(System.err);
-    		    System.exit(1);
+    		    return 1;
             }
  	    }
 
-	    if (file == null || instance == null || organism == null) {
+	    if (instance == null || organism == null || (bamFile == null && consensusFile == null)) {
 		    showUsage(System.err);
-		    System.exit(1);
+		    return 1;
 		}
 	    
-	    try {
-	    	
+	    try {	    	
  	        ArcturusInstance ai = ArcturusInstance.getInstance(instance);
  			ArcturusDatabase adb = ai.findArcturusDatabase(organism);
-
- 			
+			
  			Project project = null;
  			
  			if (projectName != null) {
@@ -68,36 +79,84 @@ public class ContigLoader extends AbstractLoader {
  			}
 
  
- 			SAMFileReader.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);	
- 			
- 			SAMFileReader reader = new SAMFileReader(file);
- 			
- 	    	if (reader.isBinary() == false || reader.hasIndex() == false)
- 	    		throw new IllegalArgumentException("The input file is not indexed: " + file);
+ 			if (bamFile != null) {
+				SAMFileReader
+						.setDefaultValidationStringency(SAMFileReader.ValidationStringency.SILENT);
 
- 	    	BAMReadLoader brl = createBAMReadLoader(adb);
- 	    	
- 	    	if (preloadReads)
-  			    brl.processFile(reader);
-  			    
-			BAMContigLoader bcl = new BAMContigLoader(adb,brl);	   
-			
-	        bcl.processFile(reader, project, contigName);
+				SAMFileReader reader = new SAMFileReader(bamFile);
+
+				if (reader.isBinary() == false || reader.hasIndex() == false)
+					throw new IllegalArgumentException(
+							"The input file is not indexed: " + bamFile);
+
+				BAMReadLoader brl = createBAMReadLoader(adb);
+
+				if (preloadReads)
+					brl.processFile(reader);
+
+				BAMContigLoader bcl = new BAMContigLoader(adb, brl);
+
+				bcl.processFile(reader, project, contigName);
+ 			}
+ 			
+ 			if (consensusFile != null) {
+ 				if (consensusFile.exists() && consensusFile.canRead()) {
+					ConsensusFileProcessor processor = new ConsensusFileProcessor(adb);
+
+					FastqFileReader reader = new FastqFileReader();
+
+					reader.processFile(consensusFile, processor);
+ 				} else
+ 					Arcturus.logWarning("The consensus file " + consensusFile + " does not exist or cannot be read");
+ 			}
 	    }
 	    catch(Exception e) {
 	    	Arcturus.logWarning("Failed to initialise or execute the contig loader", e);
-	    	System.exit(1);
+	    	return 1;
 	    }
-    	System.exit(0);
+	    
+    	return 0;
 	}
 	
-	public static void showUsage(PrintStream ps) {
+	class ConsensusFileProcessor implements SequenceProcessor {
+		private ArcturusDatabase adb;
+		
+		public ConsensusFileProcessor(ArcturusDatabase adb) {
+			this.adb = adb;
+		}
+		
+		public void processSequence(String name, byte[] dna, byte[] quality) {
+			System.out.println(name + " " + (dna == null ? 0 : dna.length) +
+					" " + (quality == null ? 0 : quality.length));
+			
+			try {
+				Contig contig = adb.getContigByName(name);
+				
+				if (contig == null) {
+					Arcturus.logWarning("Failed to find a contig with name \"" + name + "\"");
+				} else {
+					contig.setConsensus(dna, quality);
+					
+					adb.putContigConsensus(contig);
+					
+					contig.setConsensus(null, null);
+				}
+			} catch (ArcturusDatabaseException e) {
+				Arcturus.logWarning("Failed to store consensus for contig with name \"" + name + "\"", e);
+			}
+		}		
+	}
+	
+	public void showUsage(PrintStream ps) {
 		ps.println("Invalid or missing input parameters");
 		ps.println();
 		ps.println("MANDATORY PARAMETERS:");
 		ps.println("\t-instance\tName of instance");
 		ps.println("\t-organism\tName of organism");
+		ps.println();
+		ps.println("AT LEAST ONE INPUT FILE MUST BE SPECIFIED:");
 		ps.println("\t-in\t\tName of ordered and indexed BAM file to be imported");
+		ps.println("\t-consensus\tName of FASTQ file containing the contig consensus sequences");
 		ps.println();
 		ps.println("OPTIONAL PARAMETERS:");
 		ps.println("\t-project\tName of project");		
