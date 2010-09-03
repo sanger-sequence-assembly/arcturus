d14fc0d09824ad976ec4e174403905ed379284a6
Arcturus Account <arcturus@sanger.ac.uk>
2010-09-03 13:55:51 +0000

    Changed the failure notification email address from adh to arcturus-help.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@4303 69345dfe-1762-0410-a7d4-b9dc18918749

--- cron-scripts/scaffolder.sh
+++ cron-scripts/scaffolder.sh
@@ -3,8 +3,7 @@
 ARCTURUS=/software/arcturus
 SCAFFOLDER=${ARCTURUS}/test/scaffolder
 OPTIONS='-instance pathogen -out /dev/null -xml DATABASE -puclimit 15000 -shownames'
-#NOTIFY_TO=arcturus-help@sanger.ac.uk
-NOTIFY_TO=adh@sanger.ac.uk
+NOTIFY_TO=arcturus-help@sanger.ac.uk
 
 let failed=0
 
