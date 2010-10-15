75b0d5a8919f7e282e90e123b4db168de5a5ea19
Kate Taylor <kt6@sanger.ac.uk>
2010-10-15 15:18:14 +0000

    Added transaction and retry points and three example contigs
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@4342 69345dfe-1762-0410-a7d4-b9dc18918749

--- test/testRetry.pm
+++ test/testRetry.pm
@@ -1,5 +1,40 @@
 #!/user/local/bin/perl5
 
+# shisto 1 fails on both
+# shisto 2 imports the read mapping but fails on the contig mapping
+# shisto 3 imports successfully
+
+@contigs = ("shisto1", "shisto2", "shisto3");
+
+foreach $contig (@contigs) {
+	print "\nStarting work on contig $contig\n";
+	print "\tMaking a savepoint before starting read mappings\n";
+  print "\tSTART TRANSACTION: insert metatdata for  $contig\n";
+	if (putMappingsForContig($contig, "read")) {
+	  if (putMappingsForContig($contig, "contig")) {
+		  print "\tSuccessfully imported $contig\n";
+		}
+		else {
+		  # have done the read mapping but unable to do the contig mapping after three retries 
+		  print "\tReverting to savepoint to skip import of $contig\n";
+		}
+	}
+	else {
+		# unable to do the read mapping after three retries 
+		print "\tReverting to savepoint to skip import of $contig\n";
+		next;
+	}
+	print "\tEND TRANSACTION\n";
+}
+
+print "\nRaised RT ticket to list contig(s) not imported\n";
+
+exit;
+
+sub putMappingsForContig {
+  my $contig = shift;
+  my $option = shift;
+
 #$retry_in_secs = 1 * 60;
 # for testing, use one hundreth of real values
 
@@ -8,19 +43,24 @@ $retry_counter = 0.25;
 $counter = 1;
 $max_retries = 4;
 
-while ($counter < ($max_retries + 1)) {
+  if ($contig eq "shisto3") {return 1};
+
+  while ($counter < ($max_retries + 1)) {
     $retry_counter = $retry_counter * 4;
 		# to mimic that the insert succeeds
 		# until ({$counter > 4}) {
-      print "Attempt $counter for the insert statement\n";
+      print "\tAttempt $counter for the insert statement for $option mapping for contig $contig\n";
+      if ($contig eq "shisto2" && $counter == 3 && $option == "read") {return 1};
       # Execute the statement
       $retry_in_secs = $retry_in_secs * $retry_counter;
 			if ($counter < $max_retries) {
-        print "Statement has failed so wait for $retry_in_secs seconds\n"; 
+        print "\tStatement has failed so wait for $retry_in_secs seconds\n"; 
         sleep($retry_in_secs);
 			}
 			$counter++;
 		#}
+  }
+  print "\tStatement has failed $counter times so give up:  some other process has locked $contig\n";
+  return 0;
 }
-print "Statement has failed $counter times so send an email\n";
 
