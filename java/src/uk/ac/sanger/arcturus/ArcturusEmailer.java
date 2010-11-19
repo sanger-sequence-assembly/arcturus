5b33311785cdbfd883954764754533364aee7433
Kate Taylor <kt6@sanger.ac.uk>
2010-11-19 14:40:37 +0000

    Added method send with extra cc parameter
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/4595239_consistency_checks@4450 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/ArcturusEmailer.java
+++ java/src/uk/ac/sanger/arcturus/ArcturusEmailer.java
@@ -56,6 +56,38 @@ public class ArcturusEmailer {
 		}
 	}
 	
+	static void send(String smtpServer, String to, String from, String cc, String subject, String body)
+	{
+		try
+		{
+			Properties props = System.getProperties();
+			// -- Attaching to default Session, or we could start a new one --
+			props.put("mail.smtp.host", smtpServer);
+			Session session = Session.getDefaultInstance(props, null);
+			// -- Create a new message --
+			Message msg = new MimeMessage(session);
+			// -- Set the FROM and TO fields --
+			msg.setFrom(new InternetAddress(from));
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(to, false));
+			// -- Set the cc recipient(s) --
+			if (cc != null)
+			msg.setRecipients(Message.RecipientType.CC,InternetAddress.parse(cc, false));
+			// -- Set the subject and body text --
+			msg.setSubject(subject);
+			msg.setText(body);
+			// -- Set some other header information --
+			msg.setHeader("X-Mailer", "LOTONtechEmail");
+			msg.setSentDate(new Date());
+			// -- Send the message --
+			Transport.send(msg);
+			System.out.println("Email message to notify inconsistencies sent OK.");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
 
 		try
@@ -63,9 +95,13 @@ public class ArcturusEmailer {
 			String smtpServer=args[0];
 			String to=args[1];
 			String from=args[2];
-			String subject=args[3];
-			String body=args[4];
+			String cc=args[3];
+			String subject=args[4];
+			String body=args[5];
+			
 			send(smtpServer, to, from, subject, body);
+			
+			send(smtpServer, to, from, cc, subject, body);
 		}
 		catch (Exception ex)
 		{
