26dbc775085747fbc6e6c2f4a0877d70a61949f2
Kate Taylor <kt6@sanger.ac.uk>
2011-01-06 17:01:55 +0000

    Populate all fields except free reads and median
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/6163487_warehouse@4506 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/populate-project-contig-history.sql
+++ sql/populate-project-contig-history.sql
@@ -6,8 +6,10 @@
 insert into PROJECT_CONTIG_HISTORY (
 project_id, 
 statsdate, 
+name,
 total_contigs, 
 total_reads,
+free_reads,
 total_contig_length,
 mean_contig_length, 
 stddev_contig_length, 
@@ -16,19 +18,29 @@ median_contig_length)
 select 
 P.project_id, 
 now(),
+P.name,
 count(*) as contigs,
 sum(C.nreads),
+99,
 sum(C.length),
 round(avg(C.length)),
 round(std(C.length)),
 max(C.length), 
 '0'
-from CONTIG as C,PROJECT as P  where C.contig_id in 
+from CONTIG as C,PROJECT as P  
+where C.contig_id in 
      (select distinct CA.contig_id from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
      on (CA.contig_id = C2CMAPPING.parent_id and C2CMAPPING.contig_id = CB.contig_id)
      where CA.created < now()  and CA.nreads > 1 and CA.length >= 0 and (C2CMAPPING.parent_id is null  or CB.created > now()-1))
     and P.name not in ('BIN','FREEASSEMBLY','TRASH')
     and P.project_id = C.project_id;
+
+-- update the free reads
+-- this query needs the project link
+
+--update PROJECT_CONTIG_HISTORY set free_reads = select count(*) from READINFO left join CURREAD using(read_id) where seq_id is null;
+
+-- update the median value
 
 -- for each project
 
