988133f12b4ee3cdb1002c34d52aa763357bfbdd
David Harper <adh@sanger.ac.uk>
2011-01-19 09:43:13 +0000

    Fixed bug which caused the formatter to encounter a null pointer when given a record which didn't have an associated throwable.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/trunk@4537 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/logging/ShortMessageFormatter.java
+++ java/src/uk/ac/sanger/arcturus/logging/ShortMessageFormatter.java
@@ -1,13 +1,39 @@
 package uk.ac.sanger.arcturus.logging;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class ShortMessageFormatter extends Formatter {
 	public String format(LogRecord record) {
 		StringBuffer sb = new StringBuffer();
+		
+		if (record.getThrown() != null)
+			formatForException(sb, record);
+		else
+			formatForMessage(sb, record);
+		
+		return sb.toString();
+	}
 
+	private void formatForMessage(StringBuffer sb, LogRecord record) {
+		Date timestamp = new Date(record.getMillis());
+		
+		sb.append(timestamp);
+		sb.append(" : ");
+		sb.append(record.getSourceClassName());
+		sb.append(" : ");
+		sb.append(record.getSourceMethodName());
+		sb.append("\n");
+		
+		sb.append(record.getLevel());
+		sb.append(" : ");
+		sb.append(record.getMessage());
+		sb.append("\n");
+	}
+
+	private void formatForException(StringBuffer sb, LogRecord record) {
 		Throwable throwable = record.getThrown();
 
 		sb.append("An error has occurred.  Please notify a developer.\n\n");
@@ -44,8 +70,6 @@ public class ShortMessageFormatter extends Formatter {
 								"uk.ac.sanger.arcturus"))
 					sb.append("  [" + i + "]: " + ste[i] + "\n");
 		}
-		
-		return sb.toString();
 	}
 
 }
