cc8b472f9a35c12575669312e725139df4f17932
Kate Taylor <kt6@sanger.ac.uk>
2011-02-25 16:39:43 +0000

    Correct reads in contigs query to match definition used in OligoFinder (OligoFinder.sql)
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/6163487_warehouse@4624 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/populate-organism-history.sql
+++ sql/populate-organism-history.sql
@@ -3,51 +3,25 @@
 
 -- for all projects in the database
 
-insert into ORGANISM_HISTORY (
-organism,
-statsdate, 
-total_reads,
-reads_in_contigs,
-free_reads,
-asped_reads,
-next_gen_reads)
-select 
-'TESTRATTI',
-date(now())-1,
-9999,
-sum(C.nreads),
-9999,
-9999,
-9999
-from CONTIG as C,PROJECT as P  
+select sum(nreads) as reads_in_contigs
+from CONTIG C
 where C.contig_id in 
      (select distinct CA.contig_id from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
      on (CA.contig_id = C2CMAPPING.parent_id and C2CMAPPING.contig_id = CB.contig_id)
-     where date(CA.created) < date(now())-1  and CA.nreads > 1 and CA.length >= 0 and (C2CMAPPING.parent_id is null  or date(CB.created) > date(now())-2))
-    and P.name not in ('BIN','FREEASSEMBLY','TRASH')
-    and P.project_id = C.project_id;
+     where date(CA.created) < date(now())-1 and (C2CMAPPING.parent_id is null  or date(CB.created) > date(now())-1));
 
 -- update the total reads
 
-update ORGANISM_HISTORY 
-set total_reads = (select count(*) from READINFO )
-where statsdate = date(now())-1;
+select count(*) as total_reads from READINFO;
 
 -- update the free reads
 
-update ORGANISM_HISTORY 
-set free_reads =  total_reads - reads_in_contigs
-where statsdate = date(now())-1;
 
 -- update the asped_reads
 
-update ORGANISM_HISTORY 
-set asped_reads =  (select count(*) from READINFO where asped is not null )
+select count(*) as asped_reads from READINFO where asped is not null )
 where statsdate = date(now())-1;
 
 -- update the next_gen_reads
 
-update ORGANISM_HISTORY 
-set next_gen_reads = total_reads - asped_reads 
-where statsdate = date(now())-1;
 
