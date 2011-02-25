607b76280789633fbe233c39af3a1a4a893b020a
Kate Taylor <kt6@sanger.ac.uk>
2011-02-25 17:08:46 +0000

    Improve labelling to avoid adding any more confusion
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/6163487_warehouse@4626 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/check-bins.sql
+++ sql/check-bins.sql
@@ -1,6 +1,6 @@
 -- check-bins.sql to find discrepancy between OligoFinder.java and the free reads overnight job populate-organism-history.pl
 select 
-	sum(C.nreads) as 'READS IN PROJECTS TRASH, FREEASSEMBLY and BIN'
+	sum(C.nreads) as 'READS IN CONTIGS WITH MORE THAN 1 READ IN PROJECTS TRASH, FREEASSEMBLY and BIN'
 	from CONTIG as C,PROJECT as P  
 	where C.contig_id in 
      (select distinct CA.contig_id from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
@@ -11,7 +11,7 @@ select
     and P.project_id = C.project_id; 
 
 select 
-	sum(C.nreads) 'READS IN PROJECTS TRASH, FREEASSEMBLY and BIN'
+	sum(C.nreads) 'READS IN CONTIGS WITH MORE THAN 1 READ IN PROJECTS TRASH, FREEASSEMBLY and BIN'
 	from CONTIG as C,PROJECT as P  
 	where C.contig_id in 
      (select distinct CA.contig_id from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
@@ -21,8 +21,17 @@ select
     and P.name not in ('BIN','FREEASSEMBLY','TRASH')
     and P.project_id = C.project_id; 
 
-select sum(nreads) as 'READS IN ALL CONTIGS' from CONTIG;
+select 
+	sum(C.nreads) 'READS IN CONTIGS IN ANY PROJECT'
+	from CONTIG as C
+	where C.contig_id in 
+     (select distinct CA.contig_id from CONTIG as CA left join (C2CMAPPING,CONTIG as CB)
+     on (CA.contig_id = C2CMAPPING.parent_id and C2CMAPPING.contig_id = CB.contig_id)
+     where CA.created < now() and CA.length >= 0 
+		 and (C2CMAPPING.parent_id is null  or CB.created > now()));
 
 select sum(nreads)  as 'READS IN CURRENT CONTIGS' from CURRENTCONTIGS;
+
+select sum(nreads) as 'READS IN ALL CONTIGS' from CONTIG;
 
 select count(*)  'READS IN READ TABLE' from READINFO;
