d16a3ed8e524995af76b6bc0d205b7d86636785a
Kate Taylor <kt6@sanger.ac.uk>
2011-05-12 10:49:36 +0000

    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/import-export@4700 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/alter-importexport.sql
+++ sql/alter-importexport.sql
@@ -1,3 +1,3 @@
-alter table IMPORTEXPORT add column starttime datetime;
+alter table IMPORTEXPORT add column starttime datetime null;
 alter table IMPORTEXPORT change date endtime datetime;
 update IMPORTEXPORT set starttime = date(endtime);
