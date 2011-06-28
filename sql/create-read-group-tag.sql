7cd4b598affb11bf90e36d5cb0c069d39a96e3d4
Kate Taylor <kt6@sanger.ac.uk>
2011-06-28 16:20:11 +0000

    New table for RT 219981 read tag groups for Minerva2
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4759 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ sql/create-read-group-tag.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `READGROUPTAG`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `READGROUPTAG` (
+   `read_group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	 `import_id` int(10) unsigned NOT NULL, 
+   `generation_id` int(10) NOT NULL,
+   `tag_name` enum('ID', 'SM', 'LB', 'DS', 'PU', 'PI', 'CN', 'DT', 'PL') NOT NULL,
+   `tag_value` char(100) NOT NULL,
+   PRIMARY KEY (`read_group_id`),
+   KEY `read_group_id` (`read_group_id`),
+	 CONSTRAINT `READGROUPTAG_ibfk_1` FOREIGN KEY (`import_id`) REFERENCES `IMPORTEXPORT` (`id`) ON DELETE CASCADE
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
