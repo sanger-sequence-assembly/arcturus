53b5f39ee48e7fd70465b8139bf327f97b953ad1
Kate Taylor <kt6@sanger.ac.uk>
2011-06-29 13:15:52 +0000

    Test data for RT219981 read groups
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4761 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ sql/create-read-group-test-data.sql
@@ -0,0 +1,33 @@
+-- import 52 loads 18 lines
+
+-- data taken from LITTLEMURIS first TRICHURIS contig
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 1, 52, 'ID', 'GE6QGXJ01.sff');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 1, 52, 'SM', 'unknown');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 1, 52, 'LB', 'GE6QGXJ01.sff');
+
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 2, 52, 'ID', 'GI1YXNO01.sff');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 2, 52, 'SM',  'unknown');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 2, 52, 'LB', 'GI1YXNO01.sff');
+
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 3, 52, 'ID', 'GWSHH1C01.sff');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 3, 52, 'SM', 'unknown');
+insert into READGROUP(read_group_line_id, import_id, tag_name, tag_value) values ( 3, 52, 'LB', 'GWSHH1C01.sff');
+
+--@RG	ID:GI1YXNO01.sff	SM:unknown	LB:GI1YXNO01.sff
+--@RG	ID:GWSHH1C01.sff	SM:unknown	LB:GWSHH1C01.sff
+--@RG	ID:GE8TV3I02.sff	SM:unknown	LB:GE8TV3I02.sff
+--@RG	ID:GIZPJ9P01.sff	SM:unknown	LB:GIZPJ9P01.sff
+--@RG	ID:GI3OZOC02.sff	SM:unknown	LB:GI3OZOC02.sff
+--@RG	ID:GI1YXNO02.sff	SM:unknown	LB:GI1YXNO02.sff
+--@RG	ID:GI3OZOC01.sff	SM:unknown	LB:GI3OZOC01.sff
+--@RG	ID:GILNHNK01.sff	SM:unknown	LB:GILNHNK01.sff
+--@RG	ID:GXJ6ZQZ01.sff	SM:unknown	LB:GXJ6ZQZ01.sff
+--@RG	ID:GWSHH1C02.sff	SM:unknown	LB:GWSHH1C02.sff
+--@RG	ID:GIZPJ9P02.sff	SM:unknown	LB:GIZPJ9P02.sff
+--@RG	ID:GIK5ICM02.sff	SM:unknown	LB:GIK5ICM02.sff
+--@RG	ID:GJA59QB02.sff	SM:unknown	LB:GJA59QB02.sff
+--@RG	ID:GXJ6ZQZ02.sff	SM:unknown	LB:GXJ6ZQZ02.sff
+--@RG	ID:GIK5ICM01.sff	SM:unknown	LB:GIK5ICM01.sff
+--@RG	ID:GILNHNK02.sff	SM:unknown	LB:GILNHNK02.sff
+--@RG	ID:GJA59QB01.sff	SM:unknown	LB:GJA59QB01.sff
+--@RG	ID:GIBXB3A02.sff	SM:unknown	LB:GIBXB3A02.sff
