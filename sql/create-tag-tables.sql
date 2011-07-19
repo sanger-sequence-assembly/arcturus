5b8fe2294ef53eab48e8053c80ae869f2b6a29e8
Kate Taylor <kt6@sanger.ac.uk>
2011-07-19 16:34:03 +0000

    Added SAMTAG table
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4804 69345dfe-1762-0410-a7d4-b9dc18918749

--- sql/create-tag-tables.sql
+++ sql/create-tag-tables.sql
@@ -5,17 +5,20 @@
 -- parent_id to previously loaded tags is the possible source of non-deletable tags, but retain structure
 -- in case policy of always importing all tags proves to be too space hungry
 
+-- GAP4 support
+
 DROP TABLE IF EXISTS `TAG2CONTIG`;
-DROP TABLE IF EXISTS `SAMCONTIGTAG`;
-CREATE TABLE `SAMCONTIGTAG` (
+DROP TABLE IF EXISTS `CONTIGTAG`;
+
+CREATE TABLE `CONTIGTAG` (
   `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `SAMtagtype` ENUM('A', 'i', 'f', 'Z', 'H', 'B'),
-  `GAPtagtype` varchar(4) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'COMM',
+  `tagtype` varchar(4) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'COMM',
+  `systematic_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
   `tag_seq_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `tagcomment` text,
   PRIMARY KEY (`tag_id`),
-  KEY `GAPtagtype` (`GAPtagtype`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1; 
+  KEY `systematic_id` (`systematic_id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `TAG2CONTIG` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -48,16 +51,39 @@ CREATE TABLE `TAGSEQUENCE` (
 -- stores GAP Zs tags
 
 DROP TABLE IF EXISTS `READTAG`;
+
 CREATE TABLE `READTAG` (
   `seq_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `SAMtagtype` ENUM('A', 'i', 'f', 'Z', 'H', 'B'),
-  `GAPtagtype` varchar(4) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'COMM',
+  `tagtype` varchar(4) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
   `tag_seq_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `pstart` int(11) NOT NULL,
   `plength` int(11) NOT NULL,
 	`strand` enum('F','R','U') DEFAULT 'U',
-  `tagcomment` text,
+  `deprecated` enum('N','Y','X') DEFAULT 'N',
+  `comment` text,
   KEY `readtag_index` (`seq_id`),
-  KEY `GAPtagtype` (`GAPtagtype`),
   CONSTRAINT `READTAG_FK_SEQUENCE` FOREIGN KEY (`seq_id`) REFERENCES `SEQUENCE` (`seq_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- GAP% support
+
+DROP TABLE IF EXISTS `SAMTAG`;
+CREATE TABLE `SAMTAG` (
+  `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `SAMtagtype` ENUM("Zc", "Zs") NOT NULL,
+  `SAMtype` ENUM('A', 'i', 'f', 'Z', 'H', 'B') NOT NULL,
+  `GAPtagtype` varchar(4) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'COMM',
+  `tagcomment` text,
+  `contig_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `start` int(11) unsigned NOT NULL DEFAULT '0',
+  `length` int(11) unsigned NOT NULL DEFAULT '0',
+  `tag_seq_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `strand` enum('F','R','U') DEFAULT 'U',
+  `comment` tinytext,
+  PRIMARY KEY (`tag_id`),
+  KEY `GAPtagtype` (`GAPtagtype`),
+  KEY `contig_id` (`contig_id`),
+  KEY `tag_seq_id` (`tag_seq_id`),
+  CONSTRAINT `SAMTAG_ibfk_1` FOREIGN KEY (`contig_id`) REFERENCES `CONTIG` (`contig_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
