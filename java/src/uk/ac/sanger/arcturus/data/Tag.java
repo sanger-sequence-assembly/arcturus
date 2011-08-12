6e101b735e54b79ab33688de1468b5159d4f4dab
Kate Taylor <kt6@sanger.ac.uk>
2011-08-12 15:38:14 +0000

    Use TAB not "|" at end of comment
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4836 69345dfe-1762-0410-a7d4-b9dc18918749

--- java/src/uk/ac/sanger/arcturus/data/Tag.java
+++ java/src/uk/ac/sanger/arcturus/data/Tag.java
@@ -82,8 +82,8 @@ public class Tag extends Core {
 	 */
 	public String toSAMString() {
 		
-		return gapTagType + ":" + samType + ":" + samTagType + "|" + start + "|" + length
-		+ (comment == null ? "" : "|" + comment + "|");
+		return samTagType + ":" + samType + ":" + gapTagType + "|" + start + "|" + length
+		+ (comment == null ? "" : "|" + comment);
 		
 	}
 	
