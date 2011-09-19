e19d2ea70271087226c70753bdceb803a91a85be
Kate Taylor <kt6@sanger.ac.uk>
2011-09-19 13:32:30 +0000

    RT 232646 script the addition of data necessary for an empty  Minerva2 database
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4868 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ sql/add_assembly_users_and_projects.sql
@@ -0,0 +1,18 @@
+
+--create a new project you must create some people and an assembly
+
+
+insert into ASSEMBLY values (1, 'TRICHURIS MURIS', 0, 'The Sanger Institute', 0, 'other', now(), now(), 'arcturus', 'Added for initial data load');
+select * from ASSEMBLY;
+
+insert into USER(username, role) values ('kt6',  'administrator');
+insert into USER(username, role) values ('sn5',  'administrator');
+insert into USER(username, role) values ('rcc',  'team leader');
+ 
+select * from USER;
+
+insert into PROJECT values(1, 1, 'BIN', now(), NULL, NULL, NULL, now(), 'arcturus', 'created via database', 'in shotgun', ':ASSEMBLY:/illumina/split/BIN');
+insert into PROJECT values(2, 1, 'PROBLEMS', now(), NULL, NULL, NULL, now(), 'arcturus', 'created via database', 'in shotgun', NULL);
+
+select * from PROJECT;
+
