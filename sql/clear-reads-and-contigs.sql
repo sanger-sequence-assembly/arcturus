f804d596d62998889cb70ffcc859a6100507fd0f
Kate Taylor <kt6@sanger.ac.uk>
2011-09-29 09:54:09 +0000

    RT 219982 A script to clear read and contig data for Minerva2
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4885 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ sql/clear-reads-and-contigs.sql
@@ -0,0 +1,10 @@
+-- clear-reads-and-contigs.sql
+
+delete from SAMTAG;
+delete from CONTIG;
+delete from CANONICALMAPPING;
+delete from SEQUENCE;
+delete from SEQ2READ;
+delete from READNAME;
+delete from SEQ2CONTIG;
+delete from SAMREADGROUPRECORD;
