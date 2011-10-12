06cdd560a4e0e0111e71f2b56961635eb7da88f9
Kate Taylor <kt6@sanger.ac.uk>
2011-10-12 16:26:06 +0000

    Gap5 meeting ideas
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/core-system-migration@4906 69345dfe-1762-0410-a7d4-b9dc18918749

--- admintools/oligo-finder.sh
+++ admintools/oligo-finder.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+ if [ $# -lt 1 ]
+ then
+   echo Usage: $0 pattern-file file-to-search
+	 echo file-to-search can be a FASTA or a SAM file
+   exit 1
+ fi
 
-FILE=/lustre/scratch101/sanger/sn5/CELERA.sam.rebuilt
+PATTERNFILE=$1
+FILE=$2
 echo Looking for the following strings in ${FILE}
-cat /lustre/scratch101/sanger/kt6/oligo-finder-strings.txt
-grep -F -f /lustre/scratch101/sanger/kt6/oligo-finder-strings.txt ${FILE} | awk '{print $1,$10}' 
+cat ${PATTERNFILE}
+echo
+echo Found the following matching lines in ${FILE}
+echo Calculate the position of the start of each line in the consensus by (line number -1 ) * 51
+grep -n --colour=auto -F -f ${PATTERNFILE} ${FILE} 
