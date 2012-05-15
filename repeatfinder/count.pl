96f72a73c7cf6ce74f5b3e919b4881b59b04caac
Kate Taylor <kt6@sanger.ac.uk>
2012-05-15 14:13:37 +0000

    RT 264157 Find the repeat size from repeats.dbs
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/arcturus/branches/repeatfinder@4993 69345dfe-1762-0410-a7d4-b9dc18918749

---
+++ repeatfinder/count.pl
@@ -0,0 +1,20 @@
+use strict;
+
+my $min = 15;
+my $max = 1000;
+my $sequence_length = 0;
+
+while (<STDIN>)
+{
+	if (/\>(.*)/) {
+			print "$sequence_length\n"  if (($sequence_length >= $min) && ($sequence_length <= $max)) ;
+			$sequence_length = 0;
+	}
+	elsif ((/\s*[atcg]+$/) || (/\s*[ATCG]+$/)) {
+		$sequence_length = $sequence_length + length $_;
+	}
+	else {
+   	print "This is not a valid fasta line:\n $_\n" 
+	}
+}
+
